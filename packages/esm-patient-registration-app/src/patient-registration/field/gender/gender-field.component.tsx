ba3cb5119dd4c70089781fd7632783eb772d68cd
Florian Rappl <florian.rappl@smapiot.com>
2021-09-01 22:36:50 +0200

    Added EoB field

--- packages/esm-patient-registration-app/src/patient-registration/field/gender/gender-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/gender/gender-field.component.tsx
@@ -5,7 +5,7 @@ import styles from '../field.scss';
 import { useTranslation } from 'react-i18next';
 import { PatientRegistrationContext } from '../../patient-registration-context';
 import { useField } from 'formik';
-import { DobField } from '../dob/dob.component';
+import { DobField, EobField } from '../dob/dob.component';
 
 export const GenderBirthField: React.FC = () => {
   const { t } = useTranslation();
@@ -21,20 +21,21 @@ export const GenderBirthField: React.FC = () => {
       <h4 className={styles.productiveHeading02Light}>{t('sexAndBirthLabel', 'Sex & Birth')}</h4>
       <div className={styles.grid}>
         <DobField />
-        <div style={{ marginBottom: '1rem' }}>
-          <p className="bx--label">{t('genderLabelText', 'Sex')}</p>
-          <RadioButtonGroup name="gender" orientation="vertical" onChange={setGender} valueSelected={field.value}>
-            <RadioButton id="male" labelText={t('maleLabelText', 'Male')} value="Male" />
-            <RadioButton id="female" labelText={t('femaleLabelText', 'Female')} value="Female" />
-            <RadioButton id="other" labelText={t('otherLabelText', 'Other')} value="Other" />
-          </RadioButtonGroup>
-          {meta.touched && meta.error && (
-            <>
-              <input type="hidden" data-invalid="true" />
-              <div className="bx--form-requirement">{t(meta.error)}</div>
-            </>
-          )}
-        </div>
+        <EobField />
+      </div>
+      <div style={{ marginBottom: '1rem' }}>
+        <p className="bx--label">{t('genderLabelText', 'Sex')}</p>
+        <RadioButtonGroup name="gender" orientation="vertical" onChange={setGender} valueSelected={field.value}>
+          <RadioButton id="male" labelText={t('maleLabelText', 'Male')} value="Male" />
+          <RadioButton id="female" labelText={t('femaleLabelText', 'Female')} value="Female" />
+          <RadioButton id="other" labelText={t('otherLabelText', 'Other')} value="Other" />
+        </RadioButtonGroup>
+        {meta.touched && meta.error && (
+          <>
+            <input type="hidden" data-invalid="true" />
+            <div className="bx--form-requirement">{t(meta.error)}</div>
+          </>
+        )}
       </div>
     </div>
   );
