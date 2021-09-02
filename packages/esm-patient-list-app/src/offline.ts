64984c4904a217580f92c3a9213621d9f98126c3
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-09-02 10:47:23 +0200

    Migrated patient-list and patient-registration to the new patient sync API and SW header API.

--- packages/esm-patient-list-app/src/offline.ts
+++ packages/esm-patient-list-app/src/offline.ts
@@ -1,23 +1,11 @@
-import {
-  fetchCurrentPatient,
-  messageOmrsServiceWorker,
-  OfflinePatientArgs,
-  registerOfflinePatientHandler,
-} from '@openmrs/esm-framework';
+import { fetchCurrentPatient, registerOfflinePatientHandler } from '@openmrs/esm-framework';
+import { cacheForOfflineHeaders } from './constants';
 
 export function setupOffline() {
   registerOfflinePatientHandler('esm-patient-list-app', {
-    displayName: 'Patient Lists',
-    onOfflinePatientAdded: cachePatientData,
+    displayName: 'Patient list',
+    async onOfflinePatientAdded({ patientUuid }) {
+      await fetchCurrentPatient(patientUuid, { headers: cacheForOfflineHeaders });
+    },
   });
-}
-
-async function cachePatientData({ patientUuid }: OfflinePatientArgs) {
-  const url = `/ws/fhir2/R4/Patient/${patientUuid}`;
-  await messageOmrsServiceWorker({
-    type: 'registerDynamicRoute',
-    pattern: `.+${url}`,
-  });
-
-  await fetchCurrentPatient(patientUuid);
 }
--- packages/esm-patient-registration-app/src/offline.resources.ts
+++ packages/esm-patient-registration-app/src/offline.resources.ts
@@ -1,9 +1,10 @@
 import React from 'react';
-import find from 'lodash/find';
-import camelCase from 'lodash/camelCase';
-import { FetchResponse, makeUrl, messageOmrsServiceWorker, openmrsFetch, SessionUser } from '@openmrs/esm-framework';
+import find from 'lodash-es/find';
+import camelCase from 'lodash-es/camelCase';
+import { FetchResponse, openmrsFetch, SessionUser } from '@openmrs/esm-framework';
 import { PatientIdentifierType, FetchedPatientIdentifierType } from './patient-registration/patient-registration-types';
 import { mockAutoGenerationOptionsResult } from '../__mocks__/autogenerationoptions.mock';
+import { cacheForOfflineHeaders } from './constants';
 
 export interface Resources {
   addressTemplate: any;
@@ -15,23 +16,26 @@ export interface Resources {
 export const ResourcesContext = React.createContext<Resources>(null);
 
 export async function fetchCurrentSession(abortController?: AbortController): Promise<FetchResponse<SessionUser>> {
-  const url = '/ws/rest/v1/session';
-  await cacheUrl(url);
-  const { data } = await openmrsFetch('/ws/rest/v1/session', { signal: abortController?.signal });
+  const { data } = await openmrsFetch('/ws/rest/v1/session', {
+    signal: abortController?.signal,
+    headers: cacheForOfflineHeaders,
+  });
   return data;
 }
 
 export async function fetchAddressTemplate(abortController?: AbortController) {
-  const url = '/ws/rest/v1/systemsetting?q=layout.address.format&v=custom:(value)';
-  await cacheUrl(url);
-  const { data } = await openmrsFetch(url, { signal: abortController?.signal });
+  const { data } = await openmrsFetch('/ws/rest/v1/systemsetting?q=layout.address.format&v=custom:(value)', {
+    signal: abortController?.signal,
+    headers: cacheForOfflineHeaders,
+  });
   return data;
 }
 
 export async function fetchAllRelationshipTypes(abortController?: AbortController) {
-  const url = '/ws/rest/v1/relationshiptype?v=default';
-  await cacheUrl(url);
-  const { data } = await openmrsFetch(url, { signal: abortController?.signal });
+  const { data } = await openmrsFetch('/ws/rest/v1/relationshiptype?v=default', {
+    signal: abortController?.signal,
+    headers: cacheForOfflineHeaders,
+  });
   return data;
 }
 
@@ -65,13 +69,15 @@ export async function fetchPatientIdentifierTypesWithSources(
 }
 
 async function fetchPrimaryIdentifierType(abortController: AbortController): Promise<FetchedPatientIdentifierType> {
-  const metadataUrl = '/ws/rest/v1/metadatamapping/termmapping?v=full&code=emr.primaryIdentifierType';
-  await cacheUrl(metadataUrl);
-  const primaryIdentifierTypeResponse = await openmrsFetch(metadataUrl, { signal: abortController?.signal });
+  const primaryIdentifierTypeResponse = await openmrsFetch(
+    '/ws/rest/v1/metadatamapping/termmapping?v=full&code=emr.primaryIdentifierType',
+    { signal: abortController?.signal, headers: cacheForOfflineHeaders },
+  );
 
-  const identifierUrl = `/ws/rest/v1/patientidentifiertype/${primaryIdentifierTypeResponse.data.results[0].metadataUuid}`;
-  await cacheUrl(identifierUrl);
-  const { data: metadata } = await openmrsFetch(identifierUrl, { signal: abortController?.signal });
+  const { data: metadata } = await openmrsFetch(
+    `/ws/rest/v1/patientidentifiertype/${primaryIdentifierTypeResponse.data.results[0].metadataUuid}`,
+    { signal: abortController?.signal, headers: cacheForOfflineHeaders },
+  );
 
   return {
     name: metadata.name,
@@ -86,22 +92,25 @@ async function fetchPrimaryIdentifierType(abortController: AbortController): Pro
 async function fetchSecondaryIdentifierTypes(
   abortController?: AbortController,
 ): Promise<Array<FetchedPatientIdentifierType>> {
-  const metadataUrl = '/ws/rest/v1/metadatamapping/termmapping?v=full&code=emr.extraPatientIdentifierTypes';
-  await cacheUrl(metadataUrl);
-  const secondaryIdentifierTypeResponse = await openmrsFetch(metadataUrl, { signal: abortController?.signal });
+  const secondaryIdentifierTypeResponse = await openmrsFetch(
+    '/ws/rest/v1/metadatamapping/termmapping?v=full&code=emr.extraPatientIdentifierTypes',
+    { signal: abortController?.signal, headers: cacheForOfflineHeaders },
+  );
 
   if (secondaryIdentifierTypeResponse.data.results) {
     const extraIdentifierTypesSetUuid = secondaryIdentifierTypeResponse.data.results[0].metadataUuid;
-    const memberMetadataUrl = `/ws/rest/v1/metadatamapping/metadataset/${extraIdentifierTypesSetUuid}/members`;
-    await cacheUrl(memberMetadataUrl);
-    const metadataResponse = await openmrsFetch(memberMetadataUrl, { signal: abortController?.signal });
+    const metadataResponse = await openmrsFetch(
+      `/ws/rest/v1/metadatamapping/metadataset/${extraIdentifierTypesSetUuid}/members`,
+      { signal: abortController?.signal, headers: cacheForOfflineHeaders },
+    );
 
     if (metadataResponse.data.results) {
       return await Promise.all(
         metadataResponse.data.results.map(async (setMember) => {
-          const identifierUrl = '/ws/rest/v1/patientidentifiertype/' + setMember.metadataUuid;
-          await cacheUrl(identifierUrl);
-          const type = await openmrsFetch(identifierUrl, { signal: abortController?.signal });
+          const type = await openmrsFetch(`/ws/rest/v1/patientidentifiertype/${setMember.metadataUuid}`, {
+            signal: abortController?.signal,
+            headers: cacheForOfflineHeaders,
+          });
 
           return {
             name: type.data.name,
@@ -120,22 +129,16 @@ async function fetchSecondaryIdentifierTypes(
 }
 
 async function fetchIdentifierSources(identifierType: string, abortController?: AbortController) {
-  const url = '/ws/rest/v1/idgen/identifiersource?v=full&identifierType=' + identifierType;
-  await cacheUrl(url);
-  return await openmrsFetch(url, { signal: abortController?.signal });
+  return await openmrsFetch(`/ws/rest/v1/idgen/identifiersource?v=full&identifierType=${identifierType}`, {
+    signal: abortController?.signal,
+    headers: cacheForOfflineHeaders,
+  });
 }
 
 function fetchAutoGenerationOptions(identifierType: string, abortController?: AbortController) {
   // return openmrsFetch('/ws/rest/v1/idgen/autogenerationoption?v=full&identifierType=' + identifierType, {
   //   signal: abortController.signal,
+  //   headers: cacheForOfflineHeaders,
   // });‚
   return Promise.resolve(mockAutoGenerationOptionsResult);
-}
-
-async function cacheUrl(url: string) {
-  const fullUrl = new URL(makeUrl(url), window.location.origin).href;
-  await messageOmrsServiceWorker({
-    type: 'registerDynamicRoute',
-    url: fullUrl,
-  });
 }
