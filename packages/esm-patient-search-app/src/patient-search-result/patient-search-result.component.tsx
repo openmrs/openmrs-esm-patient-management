a38adca00fd3c6c44b0ae7ac91aafaa9d424e6ac
Samuel Male <samuelsmalek@gmail.com>
2021-09-21 12:56:17 +0300

    Use patient banner in search results

--- packages/esm-patient-search-app/src/patient-search-result/patient-search-result.component.tsx
+++ packages/esm-patient-search-app/src/patient-search-result/patient-search-result.component.tsx
@@ -1,56 +1,42 @@
-import React from 'react';
-import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 import styles from './patient-search-result.scss';
-import { interpolateString, ExtensionSlot, useConfig, navigate } from '@openmrs/esm-framework';
+import { ExtensionSlot, useConfig, fetchCurrentPatient, interpolateString } from '@openmrs/esm-framework';
 import { SearchedPatient } from '../types/index';
 
 const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({ patients, hidePanel }) => {
+  const [fhirPatients, setFhirPatients] = useState([]);
   const config = useConfig();
 
-  function renderPatient(patient: SearchedPatient) {
-    const preferredIdentifier = patient.identifiers.find((i) => i.preferred) || patient.identifiers[0];
+  useEffect(() => {
+    const fhirPatientsBuffer = [];
+    patients.forEach((patient) => {
+      fetchCurrentPatient(patient.uuid).then((response) => {
+        fhirPatientsBuffer.push(response.data);
+        setFhirPatients([...fhirPatientsBuffer]);
+      });
+    });
+  }, [patients]);
 
+  function renderPatient(patient) {
     return (
-      <div
-        key={patient.display}
-        role="button"
-        tabIndex={0}
-        className={styles.patientChart}
-        onClick={() => {
-          navigate({
-            to: interpolateString(config.search.patientResultUrl, {
-              patientUuid: patient.uuid,
-            }),
-          });
-          hidePanel();
-        }}>
+      <div key={patient.display} className={styles.patientChart}>
         <div className={styles.container}>
-          <div className={styles.patientBanner}>
-            <div className={styles.patientAvatar}>
-              <ExtensionSlot extensionSlotName="patient-photo-slot" state={{ patientUuid: patient.uuid }} />
-            </div>
-            <div className={styles.patientInfo}>
-              <div>
-                <span className={styles.patientName}>{patient.person.display}</span>
-              </div>
-              <div className={styles.demographics}>
-                <span>{patient.person.gender === 'M' ? 'Male' : 'Female'}</span> &middot;{' '}
-                <span>
-                  {patient.person.age} {patient.person.age === 1 ? 'year' : 'years'}
-                </span>{' '}
-                &middot; <span>{dayjs(patient.person.birthdate).format('DD - MMM - YYYY')}</span>
-              </div>
-              <div>
-                <span className={styles.identifiers}>{preferredIdentifier.identifier}</span>
-              </div>
-            </div>
-          </div>
+          <ExtensionSlot
+            extensionSlotName="patient-header-slot"
+            state={{
+              patient,
+              patientUuid: patient.id,
+              patientUrl: interpolateString(config.search.patientResultUrl, {
+                patientUuid: patient.id,
+              }),
+              hidePatientSearchPanel: hidePanel,
+            }}
+          />
         </div>
       </div>
     );
   }
-
-  return <>{patients.map((patient) => renderPatient(patient))}</>;
+  return <>{fhirPatients.map((patient) => renderPatient(patient))}</>;
 };
 
 interface PatientSearchResultsProps {
