56cc822951456b3c55e569c36f3b791ca011835b
Donald Kibet <chelashawdonald@yahoo.com>
2022-01-26 22:51:25 +0300

    (enhancement) add patient-queue link to app-menu

---
+++ packages/esm-patient-queue-app/src/patient-queue-link.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ConfigurableLink } from '@openmrs/esm-framework';
+import { useTranslation } from 'react-i18next';
+import { spaBasePath } from './constants';
+
+export default function PatientQueueLink() {
+  const { t } = useTranslation();
+  return <ConfigurableLink to={spaBasePath}>{t('outpatient', 'Out Patient')}</ConfigurableLink>;
+}
