8a146bba0f202f6722230683bc8757f0d01506b2
Vineet Sharma <sharmava05@gmail.com>
2022-02-11 16:59:40 +0530

    Separated gender and birth field in configuration

--- packages/esm-patient-registration-app/src/patient-registration/field/gender/gender-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/gender/gender-field.component.tsx
@@ -4,9 +4,8 @@ import styles from '../field.scss';
 import { useTranslation } from 'react-i18next';
 import { PatientRegistrationContext } from '../../patient-registration-context';
 import { useField } from 'formik';
-import { DobField, EobField } from '../dob/dob.component';
 
-export const GenderBirthField: React.FC = () => {
+const GenderField: React.FC = () => {
   const { t } = useTranslation();
   const [field, meta] = useField('gender');
   const { setFieldValue } = useContext(PatientRegistrationContext);
@@ -17,28 +16,24 @@ export const GenderBirthField: React.FC = () => {
 
   return (
     <div className={styles.halfWidthInDesktopView}>
-      <h4 className={styles.productiveHeading02Light}>{t('sexAndBirthLabel', 'Sex & Birth')}</h4>
-      <div>
-        <div className={styles.sexField}>
-          <p className="bx--label">{t('genderLabelText', 'Sex')}</p>
-          <RadioButtonGroup name="gender" orientation="vertical" onChange={setGender} valueSelected={field.value}>
-            <RadioButton id="male" labelText={t('maleLabelText', 'Male')} value="Male" />
-            <RadioButton id="female" labelText={t('femaleLabelText', 'Female')} value="Female" />
-            <RadioButton id="other" labelText={t('otherLabelText', 'Other')} value="Other" />
-            <RadioButton id="unknown" labelText={t('unknownLabelText', 'Unknown')} value="Unknown" />
-          </RadioButtonGroup>
-          {meta.touched && meta.error && (
-            <>
-              <input type="hidden" data-invalid="true" />
-              <div className="bx--form-requirement">{t(meta.error)}</div>
-            </>
-          )}
-        </div>
-        <div className={styles.ageField}>
-          <DobField />
-          <EobField />
-        </div>
+      <h4 className={styles.productiveHeading02Light}>{t('sexFieldLabelText', 'Sex & Birth')}</h4>
+      <div className={styles.sexField}>
+        <p className="bx--label">{t('genderLabelText', 'Sex')}</p>
+        <RadioButtonGroup name="gender" orientation="vertical" onChange={setGender} valueSelected={field.value}>
+          <RadioButton id="male" labelText={t('maleLabelText', 'Male')} value="Male" />
+          <RadioButton id="female" labelText={t('femaleLabelText', 'Female')} value="Female" />
+          <RadioButton id="other" labelText={t('otherLabelText', 'Other')} value="Other" />
+          <RadioButton id="unknown" labelText={t('unknownLabelText', 'Unknown')} value="Unknown" />
+        </RadioButtonGroup>
+        {meta.touched && meta.error && (
+          <>
+            <input type="hidden" data-invalid="true" />
+            <div className="bx--form-requirement">{t(meta.error)}</div>
+          </>
+        )}
       </div>
     </div>
   );
 };
+
+export default GenderField;
