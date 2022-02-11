0959e2b082584799d59d607568d27d22e269a43f
Dennis Kigen <kigen.work@gmail.com>
2022-02-11 16:42:53 +0300

    (feat): Follow-up mods to the active visits table (#101)
    * (feat): Follow up mods to the active visits table
    
    * Update overflow menu behaviour on hover
    
    * Fix label spacing

--- packages/esm-outpatient-app/src/patient-queue-header/patient-queue-header.component.tsx
+++ packages/esm-outpatient-app/src/patient-queue-header/patient-queue-header.component.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import PatientQueueIllustration from './patient-queue-illustration.component';
 import styles from './patient-queue-header.scss';
-import { useTranslation } from 'react-i18next';
 
 const PatientQueueHeader: React.FC = () => {
   const { t } = useTranslation();
+
   return (
     <div className={styles['patient-header-container']}>
       <div className={styles['header-left']}>
         <PatientQueueIllustration />
-        <div>
+        <div className={styles.labels}>
           <p className={styles['bodyShort02']}>{t('outpatients', 'Outpatients')}</p>
           <p className={styles['productiveHeading04']}>{t('home', 'Home')}</p>
         </div>
