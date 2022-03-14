67caeac798dd7c643eecf2f33dd6116db06bf04d
Dennis Kigen <kigen.work@gmail.com>
2022-03-15 00:29:52 +0300

    (test) Add tests for the search feature (#125)

---
+++ packages/esm-outpatient-app/src/patient-search/basic-search.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import BasicSearch from './basic-search.component';
+
+describe('BasicSearch: ', () => {
+  test('renders the basic patient search in an overlay', () => {
+    renderBasicSearch();
+
+    const searchbox = screen.getByRole('searchbox', { name: /search for a patient/i });
+    const searchButton = screen.getByRole('button', { name: /^search$/i });
+    expect(searchbox).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/search for a patient name or id number/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /advanced search/i })).toBeInTheDocument();
+    expect(screen.getByRole('separator')).toBeInTheDocument();
+    expect(screen.getByText(/type the patient's name or unique id number/i)).toBeInTheDocument();
+  });
+});
+
+function renderBasicSearch() {
+  const toggleSearchType = jest.fn();
+  render(<BasicSearch toggleSearchType={toggleSearchType} />);
+}
