b023b14987c6acee95b00f3009b540dbc39f9839
Makombe <makombe>
2022-03-21 18:41:46 +0300

    (feat): Render clinic metrics services fetch from backend

--- packages/esm-outpatient-app/src/patient-queue-metrics/queue-metrics.resource.tsx
+++ packages/esm-outpatient-app/src/patient-queue-metrics/queue-metrics.resource.tsx
@@ -1,5 +1,6 @@
 import useSWR from 'swr';
-import { openmrsFetch } from '@openmrs/esm-framework';
+import useSWRImmutable from 'swr/immutable';
+import { FetchResponse, openmrsFetch } from '@openmrs/esm-framework';
 
 export function useMetrics() {
   const metrics = { scheduled_appointments: 100, average_wait_time: 28, patients_waiting_for_service: 182 };
@@ -9,5 +10,15 @@ export function useMetrics() {
     metrics: metrics,
     isError: error,
     isLoading: !data && !error,
+  };
+}
+
+export function useServices() {
+  const serviceConceptSetUuid = '330c0ec6-0ac7-4b86-9c70-29d76f0ae20a';
+  const apiUrl = `/ws/rest/v1/concept/${serviceConceptSetUuid}`;
+  const { data } = useSWRImmutable<FetchResponse>(apiUrl, openmrsFetch);
+
+  return {
+    services: data ? data?.data?.setMembers?.map((setMember) => setMember?.display) : [],
   };
 }
