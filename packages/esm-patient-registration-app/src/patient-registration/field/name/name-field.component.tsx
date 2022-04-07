42a4943b79d5d86bc5fc32aef2f27c68083d4731
Zac Butko <zac.butko@gmail.com>
2022-04-07 03:04:48 -0700

    DOB validating properly

--- packages/esm-patient-registration-app/src/patient-registration/field/name/name-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/name/name-field.component.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext } from 'react';
 import styles from '../field.scss';
 import { Input } from '../../input/basic-input/input/input.component';
 import { PatientRegistrationContext } from '../../patient-registration-context';
