9ff28d494992b4230588a59d8a77bd01e98b5d6e
Donald Kibet <chelashawdonald@yahoo.com>
2022-04-13 15:24:39 +0300

    (enhancement) Add ability to sort search results by different criteria (#168)
    * work in progress
    
    * Add ability to sort search-results

---
+++ packages/esm-outpatient-app/src/patient-search/search-results.component.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import SearchResults from './search-results.component';
+import { screen, render } from '@testing-library/react';
+import { age } from '@openmrs/esm-framework';
+
+const mockAge = age as jest.Mock;
+
+jest.mock('@openmrs/esm-framework', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-framework');
+  return {
+    ...originalModule,
+    age: jest.fn(),
+  };
+});
+
+const mockPatient = {
+  id: 'b73bee73-7bf0-4872-9657-ba9de0d93478',
+  name: [{ given: ['John', null], family: 'Smith' }],
+  person: {
+    gender: 'M',
+    age: 2,
+    birthdate: '2020-02-01T00:00:00.000+0000',
+    birthdateEstimated: true,
+    personName: {
+      display: 'John Smith',
+      uuid: '27698bff-4056-430c-824c-cb18cf9329d8',
+      givenName: 'John',
+      middleName: null,
+      familyName: 'Smith',
+      familyName2: null,
+      voided: false,
+    },
+    addresses: [
+      {
+        display: 'Bom Jesus Street',
+        uuid: '7eab94da-4b33-4da9-b352-da21a571221a',
+        preferred: true,
+        address1: 'Bom Jesus Street',
+        address2: null,
+        cityVillage: 'Recife',
+        stateProvince: 'Pernambuco',
+        country: 'Brazil',
+        postalCode: '50030-310',
+        countyDistrict: null,
+      },
+    ],
+    display: 'John Smith',
+    dead: false,
+    deathDate: null,
+  },
+  gender: 'M',
+  birthDate: '2020-02-01T00:00:00.000+0000',
+  deceasedDateTime: null,
+  identifier: [{ value: '10001F0' }],
+  address: [{ city: 'Recife', country: 'Brazil', postalCode: '50030-310', state: 'Pernambuco', use: 'home' }],
+  telecom: [],
+  patientIdentifier: { identifier: [{ value: '10001F0' }] },
+};
+
+const mockToggleSearchType = jest.fn();
+
+describe('Search Results', () => {
+  it('should search results page', () => {
+    mockAge.mockReturnValue(35);
+    renderSearchResults();
+    expect(screen.getByText(/John Smith/)).toBeInTheDocument();
+    expect(screen.getByText(/35/)).toBeInTheDocument();
+    expect(screen.getByText(/Male/)).toBeInTheDocument();
+    expect(screen.getByText(/01 — Feb — 2020/i)).toBeInTheDocument();
+  });
+});
+
+const renderSearchResults = () => {
+  render(<SearchResults patients={[mockPatient]} toggleSearchType={mockToggleSearchType} />);
+};
--- packages/esm-outpatient-app/src/patient-search/search-results.component.tsx
+++ packages/esm-outpatient-app/src/patient-search/search-results.component.tsx
@@ -1,9 +1,9 @@
 import React, { useMemo, useState } from 'react';
-import { ExtensionSlot } from '@openmrs/esm-framework';
 import styles from './search-results.scss';
 import { SearchTypes } from '../types';
-import PatientScheduledVisits from './patient-scheduled-visits.component';
 import PatientInfo from '../patient-info/patient-info.component';
+import { useTranslation } from 'react-i18next';
+import { Dropdown } from 'carbon-components-react';
 
 interface SearchResultsProps {
   patients: Array<any>;
@@ -11,7 +11,11 @@ interface SearchResultsProps {
   toggleSearchType: (searchMode: SearchTypes) => void;
 }
 
+type SortCriteria = 'firstNameFirst' | 'lastNameFirst' | 'oldest' | 'youngest';
+
 const SearchResults: React.FC<SearchResultsProps> = ({ patients, toggleSearchType }) => {
+  const { t } = useTranslation();
+  const [sortCriteria, setSortCriteria] = useState<SortCriteria>('firstNameFirst');
   const fhirPatients = useMemo(() => {
     return patients.map((patient) => {
       const preferredAddress = patient.person.addresses?.find((address) => address.preferred);
@@ -23,6 +27,7 @@ const SearchResults: React.FC<SearchResultsProps> = ({ patients, toggleSearchTyp
             family: patient.person.personName.familyName,
           },
         ],
+        person: patient.person,
         gender: patient.person.gender,
         birthDate: patient.person.birthdate,
         deceasedDateTime: patient.person.deathDate,
@@ -47,11 +52,47 @@ const SearchResults: React.FC<SearchResultsProps> = ({ patients, toggleSearchTyp
       };
     });
   }, [patients]);
-  const onClickSearchResult = () => toggleSearchType(SearchTypes.SCHEDULED_VISITS);
+
+  const sortedPatient = useMemo(() => {
+    return fhirPatients.sort((patientA, patientB) => {
+      if (sortCriteria === 'oldest') {
+        return new Date(patientA.birthDate).getTime() - new Date(patientB.birthDate).getTime();
+      }
+      if (sortCriteria === 'youngest') {
+        return new Date(patientB.birthDate).getTime() - new Date(patientA.birthDate).getTime();
+      }
+      if (sortCriteria === 'firstNameFirst') {
+        return patientA.person.personName.givenName < patientB.person.personName.givenName ? -1 : 0;
+      }
+      if (sortCriteria === 'lastNameFirst') {
+        return patientA.person.personName.familyName < patientB.person.personName.familyName ? -1 : 0;
+      }
+    });
+  }, [fhirPatients, sortCriteria]);
+
+  const sortByCriteria = [
+    { id: 'firstNameFirst', label: t('firstNameSort', 'First name (a -z)') },
+    { id: 'lastNameFirst', label: t('lastNameSort', 'Last name (a -z)') },
+    { id: 'oldest', label: t('oldest', 'Oldest first') },
+    { id: 'youngest', label: t('youngest', 'Youngest first') },
+  ];
 
   return (
     <>
-      {fhirPatients.map((patient) => (
+      <div className={styles.sortCriteria}>
+        <Dropdown
+          ariaLabel="sortBy"
+          id="sortCriteria"
+          items={sortByCriteria}
+          initialSelectedItem={sortByCriteria[0]}
+          label={t('sortBy', 'Sort by :')}
+          titleText={t('sortBy', 'Sort by')}
+          type="inline"
+          size="sm"
+          onChange={({ selectedItem }) => setSortCriteria(selectedItem.id as SortCriteria)}
+        />
+      </div>
+      {sortedPatient.map((patient) => (
         <div key={patient.id} className={styles.patientChart}>
           <div className={styles.container}>
             <PatientInfo patient={patient} />
