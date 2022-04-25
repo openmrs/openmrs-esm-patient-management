f0f5ff5281ad47a03c9d358b3026bafd90862fcb
jwnasambu <julietwamalwa@yahoo.com>
2022-04-25 18:36:43 +0300

    O3-1075:Update the patient search feature to leverage the FHIR API

--- packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
+++ packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
@@ -1,7 +1,7 @@
 import { openmrsFetch } from '@openmrs/esm-framework';
 
 export function performPatientSearch(query: string, objectVersion: string, ac: AbortController, includeDead: boolean) {
-  const url = `/ws/rest/v1/patient?q=${query}&v=${objectVersion}&includeDead=${includeDead}`;
+  const url = `/ws/fhir2/R4//Patient?name:contains=<q>&identifier=<q>`;
   return openmrsFetch(url, {
     method: 'GET',
     signal: ac.signal,
