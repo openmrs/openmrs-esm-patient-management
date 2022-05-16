dcdc5c083b918c1bf42bbbba5df7e11e8fd7eb7c
CynthiaKamau <CynthiaKamau54@gmail.com>
2022-05-16 15:32:13 +0300

    (tests) added patient scheduled visits test

---
+++ packages/esm-outpatient-app/__mocks__/patient-scheduled-visits.mock.ts
@@ -0,0 +1,33 @@
+export const mockRecentVisits = {
+  data: [
+    {
+      id: '1',
+      visit_type: 'Adult diabetes return visit',
+      clinic: 'NCD clinic',
+      visit_date: '2022-02-23T22:44:32.000+0000',
+    },
+    {
+      id: '2',
+      visit_type: 'Adult HIV return visit',
+      clinic: 'HIV clinic',
+      visit_date: '2022-02-23T22:44:32.000+0000',
+    },
+    {
+      id: '3',
+      visit_type: 'Adult HIV return visit',
+      clinic: 'HIV clinic',
+      visit_date: '2022-02-23T22:44:32.000+0000',
+    },
+  ],
+};
+
+export const mockFutureVisits = {
+  data: [
+    {
+      id: '1',
+      visit_type: 'Adult HIV return visit',
+      clinic: 'HIV clinic',
+      visit_date: '2022-02-23T22:44:32.000+0000',
+    },
+  ],
+};
