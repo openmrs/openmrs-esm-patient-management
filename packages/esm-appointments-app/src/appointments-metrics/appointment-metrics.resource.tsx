a92ab88e360353eca18abc14376d823fb1c817d0
Pius Rubangakene <piruville@gmail.com>
2022-05-23 15:02:53 +0300

    Mh appointment tabs (#216)
    * clinical appointments tabs
    
    * appointment lists

--- packages/esm-appointments-app/src/appointments-metrics/appointment-metrics.resource.tsx
+++ packages/esm-appointments-app/src/appointments-metrics/appointment-metrics.resource.tsx
@@ -3,7 +3,7 @@ import useSWRImmutable from 'swr/immutable';
 import { FetchResponse, openmrsFetch } from '@openmrs/esm-framework';
 
 export function useMetrics() {
-  const metrics = { scheduled_appointments: 100, average_wait_time: 28, patients_waiting_for_service: 182 };
+  const metrics = { scheduled_appointments: 100, missed_appointments: 28, providers_available_today: 182 };
   const { data, error } = useSWR<{ data: { results: {} } }, Error>(`/ws/rest/v1/queue?`, openmrsFetch);
 
   return {
--- packages/esm-appointments-app/src/dashboard.meta.ts
+++ packages/esm-appointments-app/src/dashboard.meta.ts
@@ -1,8 +1,8 @@
 import { Home32 } from '@carbon/icons-react';
 
-export const homeDashboardMeta = {
-  name: 'home',
-  slot: 'home-dashboard-slot',
+export const clinicalAppointmentsDashboardMeta = {
+  name: 'clinical-appointments',
+  slot: 'clinical-appointments-dashboard-slot',
   title: 'Clinical Appointments',
   renderIcon: Home32,
 };
