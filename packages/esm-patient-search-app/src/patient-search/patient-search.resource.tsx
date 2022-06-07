19156f0f49f065b30d9ad449783dea3852283e97
jwnasambu <julietwamalwa@yahoo.com>
2022-06-07 21:10:43 +0300

    update the patient search  parameter

--- packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
+++ packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
@@ -1,7 +1,7 @@
 import { openmrsFetch } from '@openmrs/esm-framework';
 
 export function performPatientSearch(query: string, objectVersion: string, ac: AbortController, includeDead: boolean) {
-  const url = `/ws/fhir2/R4//Patient?name:contains=<q>&identifier=<q>`;
+  const url = `/ws/fhir2/R4//Patient?name:contains=${query}&identifier=${query}={includeDead ? '&deceased=true,false' : ''}`;
   return openmrsFetch(url, {
     method: 'GET',
     signal: ac.signal,
