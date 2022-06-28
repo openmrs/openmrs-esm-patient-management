22b7a0d8fa0b8d20edd7f6146c808cfb9e03b6d9
Dennis Kigen <kigen.work@gmail.com>
2022-06-28 12:03:32 +0300

    (refactor) Refactor clinic metrics component
    This PR applies the following changes to the clinic metrics component:
    
      - Add improved type annotations to hooks.
      - Amend the `useServices` hook so that it obtains the `serviceConceptSetUuid` from config.
      - Add a case to the ClinicMetrics test suite that tests changing to a different service from the services list.

--- packages/esm-outpatient-app/src/patient-queue-metrics/clinic-metrics.test.tsx
+++ packages/esm-outpatient-app/src/patient-queue-metrics/clinic-metrics.test.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import { openmrsFetch, useConfig } from '@openmrs/esm-framework';
+
 import ClinicMetrics from './clinic-metrics.component';
-import { openmrsFetch } from '@openmrs/esm-framework';
+import { mockMetrics, mockServices } from '../../__mocks__/metrics.mock';
 import { waitForLoadingToFinish } from '../../../../tools/test-helpers';
-import { mockMetrics } from '../../__mocks__/metrics.mock';
 
-const mockOpenmrsFetch = openmrsFetch as jest.Mock;
+const mockedOpenmrsFetch = openmrsFetch as jest.Mock;
+const mockedUseConfig = useConfig as jest.Mock;
 
-describe('Metrics ', () => {
-  it('should render clinic metrics correctly', async () => {
-    mockOpenmrsFetch.mockReturnValueOnce({ data: mockMetrics });
+jest.mock('./queue-metrics.resource.ts', () => {
+  const originalModule = jest.requireActual('./queue-metrics.resource.ts');
+
+  return {
+    ...originalModule,
+    useServices: jest.fn().mockImplementation(() => ({
+      services: mockServices,
+    })),
+  };
+});
+
+describe('Clinic metrics', () => {
+  it('renders a dashboard outlining metrics from the outpatient clinic', async () => {
+    mockedUseConfig.mockReturnValue({
+      concepts: {
+        serviceConceptSetUuid: '330c0ec6-0ac7-4b86-9c70-29d76f0ae20a',
+      },
+    });
+    mockedOpenmrsFetch.mockReturnValueOnce({ data: mockMetrics });
+
     renderMetrics();
 
     await waitForLoadingToFinish();
 
     expect(screen.getByText(/Scheduled appts. today/i)).toBeInTheDocument();
-    expect(screen.getByText(/Waiting for:/i)).toBeInTheDocument();
-    expect(screen.getByText(/Average wait time today/i)).toBeInTheDocument();
     expect(screen.getByText(/100/i)).toBeInTheDocument();
-    expect(screen.getByText(/8/i)).toBeInTheDocument();
+    expect(screen.getByText(/Average wait time today/i)).toBeInTheDocument();
+    expect(screen.getByText(/minutes/i)).toBeInTheDocument();
     expect(screen.getByText(/28/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /see more metrics/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('button', { name: /patient list/i }).length).toEqual(3);
+
+    // Select a different service to show metrics for
+    const serviceDropdown = screen.getByRole('button', { name: /triage open menu/i });
+    userEvent.click(serviceDropdown);
+    userEvent.click(screen.getByRole('option', { name: /clinical consultation/i }));
+    expect(screen.getByRole('button', { name: /clinical consultation/i })).toBeInTheDocument();
+    expect(screen.queryByRole('button', { name: /triage/i })).not.toBeInTheDocument();
   });
 });
 
