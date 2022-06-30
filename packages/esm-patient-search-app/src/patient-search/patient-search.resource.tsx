3d3b0c1d1dbe6d8f95a86d57bcc495dc2bf2c7f8
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-06-30 17:59:11 +0200

    First iteration of offline patient search for the compact search update.

--- packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
+++ packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
@@ -1,5 +1,12 @@
 import useSWRInfinite from 'swr/infinite';
-import { FetchResponse, openmrsFetch } from '@openmrs/esm-framework';
+import {
+  fetchCurrentPatient,
+  FetchResponse,
+  getDynamicOfflineDataEntries,
+  getSynchronizationItems,
+  openmrsFetch,
+  useConnectivity,
+} from '@openmrs/esm-framework';
 import { useCallback, useMemo } from 'react';
 import { SearchedPatient } from '../types';
 
@@ -19,6 +26,8 @@ interface PatientSearchResponse {
   >;
 }
 
+type PatientFetchResponse = FetchResponse<{ results: Array<SearchedPatient>; links: Array<{ rel: 'prev' | 'next' }> }>;
+
 export function usePatientSearch(
   searchTerm: string,
   customRepresentation: string,
@@ -26,11 +35,9 @@ export function usePatientSearch(
   searching: boolean = true,
 ): PatientSearchResponse {
   const resultsToFetch = 15;
+  const isOnline = useConnectivity();
   const getUrl = useCallback(
-    (
-      page,
-      prevPageData: FetchResponse<{ results: Array<SearchedPatient>; links: Array<{ rel: 'prev' | 'next' }> }>,
-    ) => {
+    (page: number, prevPageData?: PatientFetchResponse) => {
       if (prevPageData && !prevPageData?.data?.links.some((link) => link.rel === 'next')) {
         return null;
       }
@@ -42,13 +49,13 @@ export function usePatientSearch(
     },
     [searchTerm, customRepresentation, includeDead, resultsToFetch],
   );
+  const offlineFetcher = useCallback(() => fetchOfflinePatients(searchTerm, includeDead), [searchTerm, includeDead]);
+  const { data, isValidating, setSize, error } = useSWRInfinite<PatientFetchResponse, Error>(
+    searching ? getUrl : null,
+    isOnline ? openmrsFetch : offlineFetcher,
+  );
 
-  const { data, isValidating, setSize, error } = useSWRInfinite<
-    FetchResponse<{ results: Array<SearchedPatient>; links: Array<{ rel: 'prev' | 'next' }> }>,
-    Error
-  >(searching ? getUrl : null, openmrsFetch);
-
-  const results = useMemo(
+  return useMemo(
     () => ({
       data: data ? [].concat(...data?.map((resp) => resp?.data?.results)) : null,
       isLoading: !data && !error,
@@ -59,5 +66,84 @@ export function usePatientSearch(
     }),
     [data, isValidating, error, setSize],
   );
-  return results;
+}
+
+async function fetchOfflinePatients(searchTerm: string, includeDead: boolean): Promise<PatientFetchResponse> {
+  const offlineRegisteredPatientSyncItems = await getSynchronizationItems<{ fhirPatient?: fhir.Patient }>(
+    'patient-registration',
+  );
+
+  const offlinePatientsEntries = await getDynamicOfflineDataEntries('patient');
+  const offlinePatients = await Promise.all(
+    offlinePatientsEntries.map((entry) => fetchCurrentPatient(entry.identifier)),
+  );
+  const allKnownPatients = [
+    ...offlineRegisteredPatientSyncItems.map((item) => item.fhirPatient),
+    ...offlinePatients.map((fetchPatientResult) => fetchPatientResult.data),
+  ];
+  const matchingPatients = allKnownPatients.filter(
+    (patient) =>
+      patient && patientMatchesSearchTerm(patient, searchTerm) && (includeDead ? true : !patient.deceasedBoolean),
+  );
+
+  return {
+    data: {
+      results: matchingPatients.map(fhirPatientToSearchedPatient),
+      links: [] as Array<any>,
+    },
+  } as PatientFetchResponse;
+}
+
+function patientMatchesSearchTerm(patient: fhir.Patient, searchTerm: string) {
+  const matchableStrings = [
+    ...patient.name?.map((name) => name.family),
+    ...patient.name?.flatMap((name) => name.given),
+    ...patient.identifier?.map((identifier) => identifier.value),
+    // TODO: If necessary, expand this list with searchable patient data.
+  ].filter(Boolean);
+
+  return matchableStrings.some((str) => str.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+}
+
+function fhirPatientToSearchedPatient(patient: fhir.Patient): SearchedPatient {
+  // Patients accessed offline are in the FHIR format.
+  // This module expects the REST structure, requiring us to make a FHIR->REST conversion.
+  // This is based upon the reversed code here:
+  // https://github.com/openmrs/openmrs-esm-patient-management/blob/e54fbb473aaccf6bbc0b89a2e3d5db22e04e1c4c/packages/esm-patient-search-app/src/patient-search-result/patient-search-result.component.tsx#L81
+  const name = patient.name?.[0];
+  const preferredAddress = patient.address?.[0];
+
+  return {
+    uuid: patient.id,
+    person: {
+      personName: {
+        givenName: name?.given?.[0],
+        middleName: name?.given?.[1],
+        familyName: name?.family,
+      },
+      addresses: preferredAddress
+        ? [
+            {
+              preferred: true,
+              cityVillage: preferredAddress.city,
+              country: preferredAddress.country,
+              postalCode: preferredAddress.postalCode,
+              stateProvince: preferredAddress.state,
+            },
+          ]
+        : [],
+      birthdate: patient.birthDate,
+      deathDate: patient.deceasedDateTime,
+      death: patient.deceasedBoolean,
+      gender: patient.gender,
+    },
+    patientIdentifier: {
+      identifier: patient.identifier?.[0]?.value,
+    },
+    attributes:
+      patient.telecom?.map((x) => ({
+        value: x.value,
+        attributeType: { name: 'Telephone Number' },
+      })) ?? [],
+  };
 }
