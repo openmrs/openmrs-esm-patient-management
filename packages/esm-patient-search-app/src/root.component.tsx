3c94c926e36d5830f2d50069d6a9b28efc211cc4
Vineet Sharma <sharmava05@gmail.com>
2022-07-06 00:12:24 +0530

    Removed console log

--- packages/esm-patient-search-app/src/root.component.tsx
+++ packages/esm-patient-search-app/src/root.component.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import PatientSearchPageComponent from './patient-search-page/patient-search-page.component';
 
 const PatientSearchRootComponent: React.FC = () => {
-  console.log('root.component');
   return (
     <BrowserRouter basename={`${window['getOpenmrsSpaBase']()}search`}>
       <Route path="/:query" component={PatientSearchPageComponent} />
