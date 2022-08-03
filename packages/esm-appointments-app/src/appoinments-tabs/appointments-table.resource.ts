81c032f7eca8bbdd958ab979a898f1e23fa46f2b
CynthiaKamau <cynthiakamau54@gmail.com>
2022-08-03 16:25:42 +0300

    O3-1432 Edit Appointments (#283)
    * O3-1432 Edit Appointments
    
    * code review

--- packages/esm-appointments-app/src/appoinments-tabs/appointments-table.resource.ts
+++ packages/esm-appointments-app/src/appoinments-tabs/appointments-table.resource.ts
@@ -1,14 +1,10 @@
 import useSWR from 'swr';
 import { openmrsFetch, formatDate, formatDatetime, parseDate } from '@openmrs/esm-framework';
-import { mockAppointmentsData } from '../../../../__mocks__/appointments.mock';
-import { AppointmentsFetchResponse } from '../types';
+import { AppointmentsFetchResponse, AppointmentPayload, AppointmentService } from '../types';
 
 export function useAppointments() {
-  const apiUrl = `/ws/rest/v1/appointments?v=full`;
-  const { data, error, isValidating } = useSWR<{ data: { results: Array<AppointmentsFetchResponse> } }, Error>(
-    apiUrl,
-    openmrsFetch,
-  );
+  const apiUrl = `/ws/rest/v1/appointment/all`;
+  const { data, error, isValidating } = useSWR<{ data: Array<AppointmentsFetchResponse> }, Error>(apiUrl, openmrsFetch);
 
   const mappedAppointment = (appointment) => ({
     id: appointment.uuid,
@@ -26,10 +22,35 @@ export function useAppointments() {
     comments: appointment.comments ? appointment.comments : '--',
   });
 
-  const appointmentEntries = mockAppointmentsData.data?.map(mappedAppointment);
+  const appointmentEntries = data?.data?.map(mappedAppointment);
 
   return {
-    appointments: appointmentEntries ? appointmentEntries : null,
+    appointments: appointmentEntries ? appointmentEntries : [],
+    isLoading: !data && !error,
+    isError: error,
+    isValidating,
+  };
+}
+
+export function editAppointment(appointment: AppointmentPayload, abortController: AbortController) {
+  return openmrsFetch(`/ws/rest/v1/appointment`, {
+    method: 'POST',
+    signal: abortController.signal,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: appointment,
+  });
+}
+
+export const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+export function useServices() {
+  const apiUrl = `/ws/rest/v1/appointmentService/all/default`;
+  const { data, error, isValidating } = useSWR<{ data: Array<AppointmentService> }, Error>(apiUrl, openmrsFetch);
+
+  return {
+    services: data ? data.data : [],
     isLoading: !data && !error,
     isError: error,
     isValidating,
