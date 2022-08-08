e38351ddac7d0a9dc785a13659bd21219d6f77c6
CynthiaKamau <CynthiaKamau54@gmail.com>
2022-08-08 16:48:14 +0300

    O3-999 - Added scheduled visits list (removed mock data)

--- packages/esm-outpatient-app/__mocks__/patient-scheduled-visits.mock.ts
+++ packages/esm-outpatient-app/__mocks__/patient-scheduled-visits.mock.ts
@@ -1,33 +1,108 @@
 export const mockRecentVisits = {
-  data: [
+  recentVisits: [
     {
-      id: '1',
-      visit_type: 'Adult diabetes return visit',
-      clinic: 'NCD clinic',
-      visit_date: '2022-02-23T22:44:32.000+0000',
-    },
-    {
-      id: '2',
-      visit_type: 'Adult HIV return visit',
-      clinic: 'HIV clinic',
-      visit_date: '2022-02-23T22:44:32.000+0000',
-    },
-    {
-      id: '3',
-      visit_type: 'Adult HIV return visit',
-      clinic: 'HIV clinic',
-      visit_date: '2022-02-23T22:44:32.000+0000',
+      uuid: '6baa7963-68ea-497e-b258-6fb82382bd07',
+      appointmentNumber: '0000',
+      patient: {
+        identifier: 'M4T4J',
+        name: 'WILLIAM LAGAT',
+        uuid: 'cd34e972-a325-4371-9b2a-57ba82d628ab',
+      },
+      service: {
+        appointmentServiceId: 1,
+        name: 'Cardiology Consultation 1',
+        description: null,
+        speciality: {},
+        startTime: '09:00:00',
+        endTime: '17:30:00',
+        maxAppointmentsLimit: 30,
+        durationMins: 30,
+        location: {
+          name: '10 Engineer VCT',
+          uuid: '673d1efd-6a96-47c3-a332-54c30d9f0c29',
+        },
+        uuid: '4d93cd63-ad6e-4546-a790-90892f736e73',
+        color: '#00ff00',
+        initialAppointmentStatus: null,
+        creatorName: null,
+      },
+      serviceType: null,
+      provider: null,
+      location: null,
+      startDateTime: 1659970578000,
+      endDateTime: 1659970578000,
+      appointmentKind: 'WalkIn',
+      status: 'Scheduled',
+      comments: null,
+      additionalInfo: null,
+      teleconsultation: null,
+      providers: [
+        {
+          uuid: 'bef0f4f0-1bf2-4c0a-8191-f898bc107e3d',
+          comments: 'available',
+          response: 'ACCEPTED',
+          name: 'Super User',
+        },
+      ],
+      voided: false,
+      extensions: {
+        patientEmailDefined: false,
+      },
+      teleconsultationLink: null,
+      recurring: false,
     },
   ],
-};
-
-export const mockFutureVisits = {
-  data: [
+  pastVisits: [
     {
-      id: '1',
-      visit_type: 'Adult HIV return visit',
-      clinic: 'HIV clinic',
-      visit_date: '2022-02-23T22:44:32.000+0000',
+      uuid: '6baa7963-68ea-497e-b258-6fb82382bd07',
+      appointmentNumber: '0000',
+      patient: {
+        identifier: 'M4T4J',
+        name: 'WILLIAM LAGAT',
+        uuid: 'cd34e972-a325-4371-9b2a-57ba82d628ab',
+      },
+      service: {
+        appointmentServiceId: 1,
+        name: 'Cardiology Consultation 2',
+        description: null,
+        speciality: {},
+        startTime: '09:00:00',
+        endTime: '17:30:00',
+        maxAppointmentsLimit: 30,
+        durationMins: 30,
+        location: {
+          name: '10 Engineer VCT',
+          uuid: '673d1efd-6a96-47c3-a332-54c30d9f0c29',
+        },
+        uuid: '4d93cd63-ad6e-4546-a790-90892f736e73',
+        color: '#00ff00',
+        initialAppointmentStatus: null,
+        creatorName: null,
+      },
+      serviceType: null,
+      provider: null,
+      location: null,
+      startDateTime: 1659970578000,
+      endDateTime: 1659970578000,
+      appointmentKind: 'WalkIn',
+      status: 'Scheduled',
+      comments: null,
+      additionalInfo: null,
+      teleconsultation: null,
+      providers: [
+        {
+          uuid: 'bef0f4f0-1bf2-4c0a-8191-f898bc107e3d',
+          comments: 'available',
+          response: 'ACCEPTED',
+          name: 'Super User',
+        },
+      ],
+      voided: false,
+      extensions: {
+        patientEmailDefined: false,
+      },
+      teleconsultationLink: null,
+      recurring: false,
     },
   ],
 };
