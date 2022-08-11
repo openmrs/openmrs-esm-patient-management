0d4d448df341c02f5f20ca8b56014f4c87e6a5cb
Ian <52504170+ibacher@users.noreply.github.com>
2022-08-11 11:56:29 -0400

    (fix) Address fields should display properly (#303)

--- packages/esm-patient-registration-app/src/patient-registration/field/address/address-hierarchy.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/address/address-hierarchy.component.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import styles from '../field.scss';
 import { useTranslation } from 'react-i18next';
 import { ResourcesContext } from '../../../offline.resources';
-import { ComboInput } from '../../input/comboinput/comboinput.component';
+import { ComboInput } from '../../input/combo-input/combo-input.component';
 import { SkeletonText } from 'carbon-components-react';
 
 export function getFieldValue(field: string, doc: XMLDocument) {
@@ -20,7 +20,7 @@ function getTagAsDocument(tagName: string, template: XMLDocument) {
 
 export const AddressHierarchy: React.FC = () => {
   const [selected, setSelected] = useState('');
-  const [addressLayout, setaddressLayout] = useState([]);
+  const [addressLayout, setAddressLayout] = useState([]);
   const { t } = useTranslation();
   const { addressTemplate } = useContext(ResourcesContext);
   const addressTemplateXml = addressTemplate?.results[0].value;
@@ -30,32 +30,31 @@ export const AddressHierarchy: React.FC = () => {
   };
 
   useEffect(() => {
-    if (addressTemplateXml) {
-      const templateXmlDoc = parseString(addressTemplateXml);
-      const elementDefaults = getTagAsDocument('elementdefaults', templateXmlDoc);
-      const nameMappings = getTagAsDocument('nameMappings', templateXmlDoc);
-      const properties = nameMappings.getElementsByTagName('entry');
-      const propertiesObj = Array.prototype.map.call(properties, (property: Element) => {
-        const name = property.getElementsByTagName('string')[0].innerHTML;
-        /*
-          Please do not remove this comment. It exists to force the translation keys to be generated.
-          t('postalCode')
-          t('address1')
-          t('stateProvince')
-          t('cityVillage')
-          t('country')
-        */
-        const labelText = t(name, property.getElementsByTagName('string')[1].innerHTML);
-        const value = getFieldValue(name, elementDefaults);
-        return {
-          id: name,
-          name,
-          labelText,
-          value,
-        };
-      });
-      setaddressLayout(propertiesObj);
-    }
+    const templateXmlDoc = parseString(addressTemplateXml);
+    const elementDefaults = getTagAsDocument('elementdefaults', templateXmlDoc);
+    const nameMappings = getTagAsDocument('nameMappings', templateXmlDoc);
+    const properties = nameMappings.getElementsByTagName('property');
+    const propertiesObj = Array.prototype.map.call(properties, (property: Element) => {
+      const name = property.getAttribute('name');
+      /*
+        DO NOT REMOVE THIS COMMENT UNLESS YOU UNDERSTAND WHY IT IS HERE
+
+        t('postalCode')
+        t('address1')
+        t('stateProvince')
+        t('cityVillage')
+        t('country')
+      */
+      const labelText = t(name, property.getAttribute('value'));
+      const value = getFieldValue(name, elementDefaults);
+      return {
+        id: name,
+        name,
+        labelText,
+        value,
+      };
+    });
+    setAddressLayout(propertiesObj);
   }, [t, addressTemplateXml]);
 
   if (!addressTemplate) {
@@ -75,13 +74,11 @@ export const AddressHierarchy: React.FC = () => {
           width: '50%',
           paddingBottom: '5%',
         }}>
-        {addressLayout.map((attributes) => (
+        {addressLayout.map((attributes, idx) => (
           <ComboInput
+            id={attributes.name + '_' + idx}
             name={attributes.name}
-            labeltext={attributes.labelText}
-            items={[]}
-            id={attributes.name}
-            placeholder={attributes.labelText}
+            labelText={attributes.labelText}
             setSelectedValue={setSelectedValue}
             selected={selected}
           />
