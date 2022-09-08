3cafffa4db681c8a43f587e984942c84a01a80a3
Gabriel Mbatha Ngao <gabriel.mbatha@gmail.com>
2022-09-08 18:09:23 +0300

    O3-1055: Cancel appointment. Add ability to cancel patients appointment (#341)
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>

---
+++ packages/esm-appointments-app/src/appointment-forms/cancel-appointment.component.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import dayjs from 'dayjs';
+import { Button, ButtonSet, TextArea } from '@carbon/react';
+import { useSession, showToast, showNotification, ExtensionSlot, usePatient, parseDate } from '@openmrs/esm-framework';
+import { AppointmentPayload, MappedAppointment } from '../types';
+import { cancelAppointment, saveAppointment } from './appointment-forms.resource';
+import { useProviders } from '../hooks/useProviders';
+import { closeOverlay } from '../hooks/useOverlay';
+import styles from './cancel-appointment.scss';
+import { useSWRConfig } from 'swr';
+import { startDate } from '../helpers';
+
+interface CancelAppointmentProps {
+  appointment: MappedAppointment;
+}
+const CancelAppointment: React.FC<CancelAppointmentProps> = ({ appointment }) => {
+  const { t } = useTranslation();
+  const { mutate } = useSWRConfig();
+  const { patient } = usePatient(appointment.patientUuid);
+  const session = useSession();
+  const [selectedLocation, setSelectedLocation] = useState(appointment.location);
+  const [reason, setReason] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (selectedLocation && session?.sessionLocation?.uuid) {
+      setSelectedLocation(session?.sessionLocation?.uuid);
+    }
+  }, [selectedLocation, session]);
+
+  const handleSubmit = async () => {
+    const abortController = new AbortController();
+    setIsSubmitting(true);
+    const { status } = await cancelAppointment('Cancelled', appointment.id, abortController);
+    if (status === 200) {
+      showToast({
+        critical: true,
+        kind: 'success',
+        description: t('appointmentNowVisible', 'It has been cancelled successfully'),
+        title: t('appointmentCancelled', 'Appointment Cancelled'),
+      });
+      mutate(`/ws/rest/v1/appointment/appointmentStatus?forDate=${startDate}&status=Scheduled`);
+      closeOverlay();
+    } else {
+      showNotification({
+        title: t('appointmentCancelError', 'Error cancelling appointment'),
+        kind: 'error',
+        critical: true,
+        description: t('errorMessage', 'Error canceling the appointment'),
+      });
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      {patient && (
+        <ExtensionSlot
+          extensionSlotName="patient-header-slot"
+          state={{
+            patient,
+            patientUuid: appointment.patientUuid,
+          }}
+        />
+      )}
+
+      <TextArea
+        id="reason"
+        light
+        value={reason}
+        className={styles.inputContainer}
+        labelText={t('reasonForChanges', 'Reason For Changes')}
+        onChange={(event) => setReason(event.target.value)}
+      />
+
+      <div className={styles.buttonContainer}>
+        <Button onClick={() => closeOverlay()} kind="secondary">
+          {t('discard', 'Discard')}
+        </Button>
+        <Button onClick={handleSubmit} disabled={isSubmitting} kind="primary" type="submit">
+          {t('cancelAppointment', 'Cancel Appointment')}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CancelAppointment;
