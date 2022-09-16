342a300a83c53860da5c31244b639fb73a245c04
CynthiaKamau <cynthiakamau54@gmail.com>
2022-09-16 12:43:16 +0300

    (fix) Added default selected service in queue metrics (#367)

---
+++ __mocks__/metrics.mock.ts
@@ -0,0 +1,24 @@
+export const mockMetrics = {
+  scheduledAppointments: 100,
+  waiting: 8,
+  averageMinutes: 28,
+};
+
+export const mockServices = ['Clinical consultation', 'Triage'];
+
+export const mockServiceTypes = {
+  data: [
+    {
+      display: 'Clinical Consulltation',
+      uuid: '7bff050e-9a83-4fdb-9212-1a4c2cee349b',
+    },
+    {
+      display: 'Lab Testing',
+      uuid: '0e960ced-d35e-43de-b87f-bc08ef3b6ec3',
+    },
+    {
+      display: 'Triage',
+      uuid: 'dbc32cf4-e11d-4006-b398-c71204cfc0d0',
+    },
+  ],
+};
--- packages/esm-outpatient-app/__mocks__/metrics.mock.ts
+++
@@ -1,7 +0,0 @@
-export const mockMetrics = {
-  scheduledAppointments: 100,
-  waiting: 8,
-  averageMinutes: 28,
-};
-
-export const mockServices = ['Clinical consultation', 'Triage'];
