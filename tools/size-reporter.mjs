654d0ff7dd73542865970391bf5760f2b5cfc4f2
Allan Keygen <kigen.work@gmail.com>
2022-09-18 19:51:13 +0300

    (chore) Set up `compress-size-action` to track bundle sizes

--- tools/size-reporter.mjs
+++
@@ -1,9 +0,0 @@
-import { reportFileSizeImpact, readGitHubWorkflowEnv } from '@jsenv/file-size-impact';
-
-await reportFileSizeImpact({
-  ...readGitHubWorkflowEnv(),
-  buildCommand: 'npx lerna run build',
-  installCommand: 'npx lerna bootstrap',
-  fileSizeReportModulePath: './tools/size-generator.mjs#fileSizeReport',
-  projectDirectoryUrl: new URL('../', import.meta.url),
-});
