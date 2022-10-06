cbae1d1cdd3f0c36d30dd665551ca6e202ef0a58
CynthiaKamau <cynthiakamau54@gmail.com>
2022-10-06 15:32:08 +0300

    (feat) Updated providers count in appointment metrics (#409)

--- packages/esm-appointments-app/src/appointments-metrics/appointments-metrics.test.tsx
+++ packages/esm-appointments-app/src/appointments-metrics/appointments-metrics.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { openmrsFetch } from '@openmrs/esm-framework';
 import AppointmentsMetrics from './appointments-metrics.component';
-import { mockAppointmentMetrics } from '../../../../__mocks__/appointments.mock';
+import { mockAppointmentMetrics, mockProvidersCount } from '../../../../__mocks__/appointments.mock';
 
 const mockedOpenmrsFetch = openmrsFetch as jest.Mock;
 
@@ -16,6 +16,11 @@ jest.mock('../hooks/useClinicalMetrics', () => {
       highestServiceLoad: mockAppointmentMetrics.highestServiceLoad,
       isLoading: mockAppointmentMetrics.isLoading,
       error: mockAppointmentMetrics.error,
+    })),
+    useAllAppointmentsByDate: jest.fn().mockImplementation(() => ({
+      totalProviders: mockProvidersCount.totalProviders,
+      isLoading: mockProvidersCount.isLoading,
+      error: mockProvidersCount.error,
     })),
   };
 });
@@ -33,7 +38,7 @@ describe('Appointment metrics', () => {
     expect(screen.getByText(/16/i)).toBeInTheDocument();
     expect(screen.getByText(/high volume service. today/i)).toBeInTheDocument();
     expect(screen.getByText(/providers available today/i)).toBeInTheDocument();
-    expect(screen.getByText(/0/i)).toBeInTheDocument();
+    expect(screen.getByText(/4/i)).toBeInTheDocument();
   });
 });
 
