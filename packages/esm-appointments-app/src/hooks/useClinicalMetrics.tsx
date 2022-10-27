abf568589e10428389298c9636effc397e7455d4
Donald Kibet <chelashawdonald@yahoo.com>
2022-10-27 22:35:33 +1100

    bug: fix label translation for current date and provider count

--- packages/esm-appointments-app/src/hooks/useClinicalMetrics.tsx
+++ packages/esm-appointments-app/src/hooks/useClinicalMetrics.tsx
@@ -9,6 +9,7 @@ import {
   useAppointmentDate,
 } from '../helpers';
 import { omrsDateFormat } from '../constants';
+import uniqBy from 'lodash-es/uniqBy';
 
 export const useClinicalMetrics = () => {
   const startDate = useAppointmentDate();
@@ -39,8 +40,11 @@ export function useAllAppointmentsByDate() {
   const apiUrl = `/ws/rest/v1/appointment/all?forDate=${startDate}`;
   const { data, error, isValidating, mutate } = useSWR<{ data: Array<Appointment> }, Error>(apiUrl, openmrsFetch);
 
-  let providersCount = data?.data?.map((appointment) => appointment.providers).flat()?.length;
-
+  const providersArray = data?.data?.filter(({ providers }) => providers !== null) ?? [];
+  const providersCount = uniqBy(
+    providersArray.map(({ providers }) => providers).flat(),
+    (provider) => provider.uuid,
+  ).length;
   return {
     totalProviders: providersCount ? providersCount : 0,
     isLoading: !data && !error,
