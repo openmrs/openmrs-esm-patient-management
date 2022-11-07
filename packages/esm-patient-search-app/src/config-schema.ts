65c322784c2bbe84f6e57af8f7bc67148db3ebd6
Dennis Kigen <kigen.work@gmail.com>
2022-11-07 18:17:16 +0300

    (fix) Add type property to `patientResultUrl` config schema

--- packages/esm-patient-search-app/src/config-schema.ts
+++ packages/esm-patient-search-app/src/config-schema.ts
@@ -2,6 +2,7 @@ import { Type, validators } from '@openmrs/esm-framework';
 export const configSchema = {
   search: {
     patientResultUrl: {
+      _type: Type.String,
       _default: '${openmrsSpaBase}/patient/${patientUuid}/chart',
       _description: 'Where clicking a patient result takes the user. Accepts template parameter ${patientUuid}',
       _validators: [validators.isUrlWithTemplateParameters(['patientUuid'])],
