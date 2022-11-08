61a1c3742f8b4fd695b4c99a1e52b987bdcfc9b2
Donald Kibet <chelashawdonald@yahoo.com>
2022-11-08 17:13:29 +0300

    feat: Add ability to download appointments (#468)

--- packages/esm-appointments-app/src/appointments/appointment-list.component.tsx
+++ packages/esm-appointments-app/src/appointments/appointment-list.component.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Tab, TabList, Tabs, TabPanel, TabPanels } from '@carbon/react';
-import { Calendar } from '@carbon/react/icons';
+import { Calendar, Download } from '@carbon/react/icons';
 import CompletedAppointments from '../appointments-tabs/completed-appointments.component';
 import styles from './appointment-list.scss';
-import { navigate } from '@openmrs/esm-framework';
+import { formatDate, navigate } from '@openmrs/esm-framework';
 import { spaBasePath } from '../constants';
 import ScheduledAppointments from '../appointments-tabs/schedule-appointment.component';
 import CheckInAppointments from '../appointments-tabs/checkedinappointments.component';
@@ -12,6 +12,7 @@ import { useAppointmentDate } from '../helpers';
 import dayjs from 'dayjs';
 import UnScheduledAppointments from '../appointments-tabs/unscheduled-appointments.component';
 import PendingAppointments from '../appointments-tabs/pending-appointments.component';
+import { useAppointments } from '../appointments-tabs/appointments-table.resource';
 
 enum AppointmentTypes {
   SCHEDULED = 'Scheduled',
@@ -23,10 +24,39 @@ enum AppointmentTypes {
 const AppointmentList: React.FC = () => {
   const { t } = useTranslation();
   const startDate = useAppointmentDate();
+  const { appointments } = useAppointments();
   const isToday = dayjs(new Date(startDate)).isSame(new Date(), 'date');
   const [selectedTab, setSelectedTab] = useState(0);
+
+  const appointmentDownloadInfo = () => {
+    const downloadInfo: any = appointments
+      ?.map((appointment) => ({
+        patientName: appointment.name,
+        phoneNumber: appointment.phoneNumber ?? '--',
+      }))
+      .map(function (appointment) {
+        return JSON.stringify(Object.values(appointment));
+      })
+      .join('\n')
+      .replace(/(^\[)|(\]$)/gm, '');
+    return new Blob([downloadInfo], {
+      type: 'text/csv;charset=utf-8;',
+    });
+  };
   return (
     <div className={styles.appointmentList}>
+      <div className={styles.downloadButton}>
+        {appointments.length > 0 && (
+          <Button renderIcon={Download} kind="tertiary">
+            <a
+              className={styles.downloadLink}
+              download={`Appointments ${startDate}.csv`}
+              href={window.URL.createObjectURL(appointmentDownloadInfo())}>
+              {t('downloadAppointmentList', 'Download appointment list')}
+            </a>
+          </Button>
+        )}
+      </div>
       <Tabs
         selectedIndex={selectedTab}
         onChange={({ selectedIndex }) => setSelectedTab(selectedIndex)}
