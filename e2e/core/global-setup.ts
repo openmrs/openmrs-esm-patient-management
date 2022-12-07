15979c88347379a15f957ed6b2570a6b00079da9
Jayasanka <jayasanka.sack@gmail.com>
2022-12-07 17:38:40 +0530

    Create a github action
    Add env file
    
    Add env file
    
    Add env file

--- e2e/core/global-setup.ts
+++ e2e/core/global-setup.ts
@@ -1,32 +1,29 @@
-import { chromium } from '@playwright/test';
-import { LoginPage } from '../pages';
+import { chromium, request } from '@playwright/test';
 
 /**
  * This configuration is to reuse the signed-in state in the tests
  * by log in only once and then skip the log in step for all the tests.
  *
- * The state will be saved in the e2e/storageState.json file.
- * Keep in mind that the setup process will not run if this file
- * already generated. If the server requires you to re-authenticate,
- * you can delete the state file and try again.
- *
  * https://playwright.dev/docs/auth#reuse-signed-in-state
  */
 
 async function globalSetup() {
-  const browser = await chromium.launch();
-  const page = await browser.newPage({ baseURL: process.env.E2E_UI_BASE_URL });
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.login(
-    process.env.E2E_USER_ADMIN_USERNAME ?? '',
-    process.env.E2E_USER_ADMIN_PASSWORD ?? '',
-    process.env.E2E_LOGIN_DEFAULT_LOCATION_NAME ?? '',
+  const requestContext = await request.newContext();
+  const token = Buffer.from(`${process.env.E2E_USER_ADMIN_USERNAME}:${process.env.E2E_USER_ADMIN_PASSWORD}`).toString(
+    'base64',
   );
-  await page.waitForURL(/home$/);
-  // Save signed-in state to 'storageState.json'.
-  await page.context().storageState({ path: 'e2e/storageState.json' });
-  await browser.close();
+  await requestContext.post(`${process.env.E2E_WS_BASE_URL}/rest/v1/session`, {
+    data: {
+      sessionLocation: process.env.E2E_LOGIN_DEFAULT_LOCATION_UUID,
+      locale: 'en',
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${token}`,
+    },
+  });
+  await requestContext.storageState({ path: 'e2e/storageState.json' });
+  await requestContext.dispose();
 }
 
 export default globalSetup;
