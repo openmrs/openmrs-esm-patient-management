59204300c6191a755064b5f373ae8a9eff2157a0
Vineet Sharma <sharmava05@gmail.com>
2023-02-01 19:21:06 +0530

    (fix) O3-1668 & O3-1724: Fixed the re-focus to search input when entering different fields  (#527)
    * Fixed the focus to the input
    
    * Added comment
    
    * Added another comment
    
    * Lint fixes

--- packages/esm-patient-search-app/src/hooks/useArrowNavigation.tsx
+++ packages/esm-patient-search-app/src/hooks/useArrowNavigation.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useCallback } from 'react';
 
 const useArrowNavigation = (
-  inputRef: React.MutableRefObject<HTMLInputElement>,
   totalResults: number,
   enterCallback: (evt: any, index: number) => void,
   resetFocusCallback: () => void,
@@ -12,25 +11,25 @@ const useArrowNavigation = (
   const handleKeyPress = useCallback(
     (e) => {
       if (e.key === 'ArrowUp') {
-        setFocussedResult((prev) => Math.max(-1, prev - 1));
+        const newFocussedResult = Math.max(-1, focussedResult - 1);
+        setFocussedResult(newFocussedResult);
+        if (newFocussedResult === -1) {
+          resetFocusCallback();
+        }
       } else if (e.key === 'ArrowDown') {
         setFocussedResult((prev) => Math.min(totalResults - 1, prev + 1));
       } else if (e.key === 'Enter' && focussedResult > -1) {
         enterCallback(e, focussedResult);
-      } else if (document.activeElement !== inputRef.current) {
+      } else if (focussedResult !== -1) {
+        // This condition will be met when scrolling through the list, the user presses another
+        // key, then the user should be focussed to the input.
+        // The focus to input should only be called when the user is scrolling through the list
+        // Hence the if condition
         resetFocusCallback();
         setFocussedResult(initalFocussedResult);
       }
     },
-    [
-      setFocussedResult,
-      totalResults,
-      focussedResult,
-      enterCallback,
-      initalFocussedResult,
-      inputRef,
-      resetFocusCallback,
-    ],
+    [setFocussedResult, totalResults, focussedResult, enterCallback, initalFocussedResult, resetFocusCallback],
   );
 
   useEffect(() => {
