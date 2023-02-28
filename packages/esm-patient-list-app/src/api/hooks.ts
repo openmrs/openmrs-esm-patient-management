f1227563b5306095a917bc7cbc400e2ec54f27a6
Dennis Kigen <kigen.work@gmail.com>
2023-02-28 02:17:11 -0800

    Yet another fixup

--- packages/esm-patient-list-app/src/api/hooks.ts
+++ packages/esm-patient-list-app/src/api/hooks.ts
@@ -42,8 +42,8 @@ export function usePatientListDetails(patientListUuid: string) {
     error,
     isLoading,
     mutate,
-  }
-};
+  };
+}
 
 export function usePatientListMembers(
   patientListUuid: string,
