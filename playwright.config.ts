fa17e8bc751a75fa34c48a7d2c583a009877f178
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-03-17 21:13:10 +0530

    Remove the retry count in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ const config: PlaywrightTestConfig = {
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  retries: 0,
   reporter: process.env.CI ? [['junit', { outputFile: 'results.xml' }], ['html']] : [['html']],
   globalSetup: require.resolve('./e2e/core/global-setup'),
   use: {
