099f5e88452bdd0aa0da7d295c4f470b1b441f30
Dennis Kigen <kigen.work@gmail.com>
2023-03-19 21:42:28 +0300

    (feat) Patient search UI enhancements

--- packages/esm-patient-search-app/src/patient-search-page/patient-search-views.component.tsx
+++ packages/esm-patient-search-app/src/patient-search-page/patient-search-views.component.tsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import styles from './patient-search-lg.scss';
 import { Layer, Tile } from '@carbon/react';
 import EmptyDataIllustration from '../ui-components/empty-data-illustration.component';
 import PatientBanner, { PatientBannerSkeleton } from './patient-banner/banner/patient-banner.component';
 import { SearchedPatient } from '../types';
+import styles from './patient-search-lg.scss';
 
-interface EmptyQueryIllustrationProps {
+interface CommonProps {
   inTabletOrOverlay: boolean;
 }
 
-export const EmptyQueryIllustration: React.FC<EmptyQueryIllustrationProps> = ({ inTabletOrOverlay }) => {
+interface PatientSearchResultsProps {
+  searchResults: SearchedPatient[];
+  handlePatientSelection: (evt: any, patientUuid: string) => void;
+}
+
+export const EmptyState: React.FC<CommonProps> = ({ inTabletOrOverlay }) => {
   const { t } = useTranslation();
   return (
     <Layer>
       <Tile className={`${styles.emptySearchResultsTile} ${inTabletOrOverlay && styles.paddedEmptySearchResultsTile}`}>
         <EmptyDataIllustration />
         <p className={styles.emptyResultText}>
-          {t('noPatientChartsFoundMessage', 'Sorry, no patient charts have been found')}
+          {t('noPatientChartsFoundMessage', 'Sorry, no patient charts were found')}
         </p>
         <p className={styles.actionText}>
-          <span>{t('trySearchWithPatientUniqueID', "Try searching with the patient's unique ID number")}</span>
-          <br />
-          <span>{t('orPatientName', "OR the patient's name(s)")}</span>
+          <span>{t('trySearchWithPatientUniqueID', "Try to search again using the patient's unique ID number")}</span>
         </p>
       </Tile>
     </Layer>
   );
 };
 
-interface LoadingSearchResultsProps {}
-
-export const LoadingSearchResults: React.FC<LoadingSearchResultsProps> = () => {
+export const LoadingState: React.FC<CommonProps> = ({ inTabletOrOverlay }) => {
   return (
-    <div className={styles.results}>
+    <div className={`${styles.results} ${inTabletOrOverlay && styles.paddedEmptySearchResultsTile}`}>
       <PatientBannerSkeleton />
       <PatientBannerSkeleton />
       <PatientBannerSkeleton />
@@ -43,11 +44,7 @@ export const LoadingSearchResults: React.FC<LoadingSearchResultsProps> = () => {
   );
 };
 
-interface FetchErrorIllustrationProps {
-  inTabletOrOverlay: boolean;
-}
-
-export const FetchErrorIllustration: React.FC<FetchErrorIllustrationProps> = ({ inTabletOrOverlay }) => {
+export const ErrorState: React.FC<CommonProps> = ({ inTabletOrOverlay }) => {
   const { t } = useTranslation();
   return (
     <Layer>
@@ -67,35 +64,22 @@ export const FetchErrorIllustration: React.FC<FetchErrorIllustrationProps> = ({ 
   );
 };
 
-interface EmptySearchResultsIllustrationProps {
-  inTabletOrOverlay: boolean;
-}
-
-export const EmptySearchResultsIllustration: React.FC<EmptySearchResultsIllustrationProps> = ({
-  inTabletOrOverlay,
-}) => {
+export const SearchResultsEmptyState: React.FC<CommonProps> = ({ inTabletOrOverlay }) => {
   const { t } = useTranslation();
   return (
     <Layer>
       <Tile className={`${styles.emptySearchResultsTile} ${inTabletOrOverlay && styles.paddedEmptySearchResultsTile}`}>
         <EmptyDataIllustration />
         <p className={styles.emptyResultText}>
-          {t('noPatientChartsFoundMessage', 'Sorry, no patient charts have been found')}
+          {t('noPatientChartsFoundMessage', 'Sorry, no patient charts were found')}
         </p>
         <p className={styles.actionText}>
-          <span>{t('trySearchWithPatientUniqueID', "Try searching with the patient's unique ID number")}</span>
-          <br />
-          <span>{t('orPatientName', "OR the patient's name(s)")}</span>
+          <span>{t('trySearchWithPatientUniqueID', "Try to search again using the patient's unique ID number")}</span>
         </p>
       </Tile>
     </Layer>
   );
 };
-
-interface PatientSearchResultsProps {
-  searchResults: SearchedPatient[];
-  handlePatientSelection: (evt: any, patientUuid: string) => void;
-}
 
 export const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({
   searchResults,
