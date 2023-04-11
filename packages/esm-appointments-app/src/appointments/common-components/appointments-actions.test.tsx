ce21d9a6b2b3acf8a6473f51e8e8c268ae2350f8
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-11 10:05:59 +0300

    (bug) fix broken workflow for launching follow up form (#651)

---
+++ packages/esm-appointments-app/src/appointments/common-components/appointments-actions.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import AppointmentActions from './appointments-actions.component';
+import { MappedAppointment } from '../../types';
+
+describe('AppointmentActions', () => {
+  const defaultProps = {
+    visits: [],
+    appointment: {
+      patientUuid: '123',
+      dateTime: new Date().toISOString(),
+    } as MappedAppointment,
+    scheduleType: 'Pending',
+  };
+
+  it('renders the correct button when the patient has checked out', () => {
+    const visits = [
+      {
+        patient: { uuid: '123' },
+        startDatetime: new Date().toISOString(),
+        stopDatetime: new Date().toISOString(),
+      },
+    ];
+    const props = { ...defaultProps, visits };
+    const { getByText } = render(<AppointmentActions {...props} />);
+    const button = getByText('Checked out');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders the correct button when the patient has an active visit and today is the appointment date', () => {
+    const visits = [
+      {
+        patient: { uuid: '123' },
+        startDatetime: new Date().toISOString(),
+      },
+    ];
+    const props = { ...defaultProps, visits };
+    render(<AppointmentActions {...props} />);
+    const button = screen.getByText('Check out');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders the correct button when today is the appointment date and the schedule type is pending', () => {
+    const props = { ...defaultProps };
+    render(<AppointmentActions {...props} />);
+    const button = screen.getByRole('button', { name: 'CheckIn' });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders the correct button when today is the appointment date and the schedule type is not pending', () => {
+    const props = { ...defaultProps, scheduleType: 'Confirmed' };
+    render(<AppointmentActions {...props} />);
+    const button = screen.getByRole('button', { name: 'CheckIn' });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders the correct button when the appointment is in the past or has not been scheduled', () => {
+    const props = { ...defaultProps, appointment: { ...defaultProps.appointment, dateTime: '2022-01-01' } };
+    render(<AppointmentActions {...props} />);
+    const button = screen.getByRole('button', { name: 'Follow up' });
+    expect(button).toBeInTheDocument();
+  });
+});
