5c0419e9471cbf0174fa620989fff3a14e687e37
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2023-05-24 18:33:13 +0530

    (test) Integrate Playwright 'test.step' for Enhanced BDD-like E2E Testing (#704)
    * Integrate Playwright 'test.step' for Enhanced BDD-like E2E Testing
    
    * Fix search result expectation
    
    * Fix search result issue
    
    * Change the Base URL
    
    ---------
    
    Co-authored-by: Anjula Shanaka <anjulashanaka@gmail.com>
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- e2e/specs/patientSearch.spec.ts
+++ e2e/specs/patientSearch.spec.ts
@@ -9,45 +9,71 @@ test.beforeEach(async ({ api }) => {
   patient = await generateRandomPatient(api);
 });
 
-test('should be able to search patients by identifier', async ({ page }) => {
+test('Search patient by patient identifier', async ({ page, api }) => {
   // extract details from the created patient
   const openmrsIdentifier = patient.identifiers[0].display.split('=')[1].trim();
   const firstName = patient.person.display.split(' ')[0];
   const lastName = patient.person.display.split(' ')[1];
-
   const homePage = new HomePage(page);
-  await homePage.goto();
 
-  await homePage.searchPatient(openmrsIdentifier);
+  await test.step('When I visit the home page', async () => {
+    await homePage.goto();
+  });
 
-  await expect(homePage.floatingSearchResultsContainer()).toHaveText(/1 search result/);
-  await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(firstName));
-  await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(lastName));
+  await test.step('And I enter a valid patient identifier into the search field', async () => {
+    await homePage.searchPatient(openmrsIdentifier);
+  });
 
-  await homePage.clickOnPatientResult(firstName);
-  await expect(homePage.page).toHaveURL(
-    `${process.env.E2E_BASE_URL}/spa/patient/${patient.uuid}/chart/Patient Summary`,
-  );
+  await test.step('Then I should see only the patient with the entered identifier', async () => {
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(/1 search result/);
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(firstName));
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(lastName));
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(openmrsIdentifier));
+  });
+
+  await test.step('When I click on the patient', async () => {
+    await homePage.clickOnPatientResult(firstName);
+  });
+
+  await test.step("Then I should be in the patient's chart page", async () => {
+    await expect(homePage.page).toHaveURL(
+      `${process.env.E2E_BASE_URL}/spa/patient/${patient.uuid}/chart/Patient Summary`,
+    );
+  });
 });
 
-test('should be able to search patients by name', async ({ page }) => {
+test('Search patient by full name', async ({ page, api }) => {
   // extract details from the created patient
   const openmrsIdentifier = patient.identifiers[0].display.split('=')[1].trim();
   const firstName = patient.person.display.split(' ')[0];
   const lastName = patient.person.display.split(' ')[1];
 
   const homePage = new HomePage(page);
-  await homePage.goto();
 
-  await homePage.searchPatient(`${firstName} ${lastName}`);
+  await test.step('When I visit the home page', async () => {
+    await homePage.goto();
+  });
 
-  await expect(homePage.floatingSearchResultsContainer()).toHaveText(/1 search result/);
-  await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(openmrsIdentifier));
+  await test.step('And I enter a valid patient name into the search field', async () => {
+    await homePage.searchPatient(`${firstName} ${lastName}`);
+  });
 
-  await homePage.clickOnPatientResult(openmrsIdentifier);
-  await expect(homePage.page).toHaveURL(
-    `${process.env.E2E_BASE_URL}/spa/patient/${patient.uuid}/chart/Patient Summary`,
-  );
+  await test.step('Then I should see only the patient with the entered name', async () => {
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(/1 search result/);
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(firstName));
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(lastName));
+    await expect(homePage.floatingSearchResultsContainer()).toHaveText(new RegExp(openmrsIdentifier));
+  });
+
+  await test.step('When I click on the patient', async () => {
+    await homePage.clickOnPatientResult(openmrsIdentifier);
+  });
+
+  await test.step("Then I should be in the patient's chart page", async () => {
+    await expect(homePage.page).toHaveURL(
+      `${process.env.E2E_BASE_URL}/spa/patient/${patient.uuid}/chart/Patient Summary`,
+    );
+  });
 });
 
 test.afterEach(async ({ api }) => {
