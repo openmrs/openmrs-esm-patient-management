97660a8998d142aeb8d44c815ad3c26e9678c156
Ayush <54752747+ayush-AI@users.noreply.github.com>
2023-06-19 18:26:04 +0530

    (fix) O3-2081: Add unit tests for patient-registration-validation (#707)

--- packages/esm-patient-registration-app/src/patient-registration/validation/patient-registration-validation.test.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/validation/patient-registration-validation.test.tsx
@@ -1,129 +1,143 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { Formik, Form } from 'formik';
-import { useConfig } from '@openmrs/esm-framework';
 import { validationSchema } from './patient-registration-validation';
-import { NameField } from '../field/name/name-field.component';
-import { PatientRegistrationContext } from '../patient-registration-context';
-import { initialFormValues } from '../patient-registration.component';
-import { FormValues } from '../patient-registration-types';
 
-const mockUseConfig = useConfig as jest.Mock;
-const mockFieldConfigs = {
-  fieldConfigurations: {
-    name: {
-      displayMiddleName: true,
-    },
-  },
-};
-
-jest.mock('@openmrs/esm-framework', () => {
-  const originalModule = jest.requireActual('@openmrs/esm-framework');
-
-  return {
-    ...originalModule,
-    useConfig: jest.fn(),
-    validator: jest.fn(),
-  };
-});
-
-describe('Name input', () => {
-  const formValues: FormValues = initialFormValues;
-
-  const testValidName = (givenNameValue: string, middleNameValue: string, familyNameValue: string) => {
-    it(
-      'does not display error message when givenNameValue: ' +
-        givenNameValue +
-        ', middleNameValue: ' +
-        middleNameValue +
-        ', familyNameValue: ' +
-        familyNameValue,
-      async () => {
-        const error = await updateNameAndReturnError(givenNameValue, middleNameValue, familyNameValue);
-        Object.values(error).map((currentError) => expect(currentError).toBeNull());
+describe('Patient Registration Validation', () => {
+  describe('validationSchema', () => {
+    const validFormValues = {
+      givenName: 'John',
+      familyName: 'Doe',
+      additionalGivenName: '',
+      additionalFamilyName: '',
+      gender: 'Male',
+      birthdate: new Date('1990-01-01'),
+      birthdateEstimated: false,
+      deathDate: null,
+      email: 'john.doe@example.com',
+      identifiers: {
+        nationalId: {
+          required: true,
+          identifierValue: '123456789',
+        },
+        passportId: {
+          required: false,
+          identifierValue: '',
+        },
       },
-    );
-  };
-
-  const testInvalidName = (
-    givenNameValue: string,
-    middleNameValue: string,
-    familyNameValue: string,
-    expectedError: string,
-    errorType: string,
-  ) => {
-    it.skip(
-      'displays error message when givenNameValue: ' +
-        givenNameValue +
-        ', middleNameValue: ' +
-        middleNameValue +
-        ', familyNameValue: ' +
-        familyNameValue,
-      async () => {
-        const error = (await updateNameAndReturnError(givenNameValue, middleNameValue, familyNameValue))[errorType];
-        expect(error.textContent).toEqual(expectedError);
-      },
-    );
-  };
-
-  const updateNameAndReturnError = async (givenNameValue: string, middleNameValue: string, familyNameValue: string) => {
-    const user = userEvent.setup();
-
-    mockUseConfig.mockReturnValue(mockFieldConfigs);
-
-    render(
-      <Formik
-        initialValues={{
-          givenName: '',
-          middleName: '',
-          familyName: '',
-        }}
-        onSubmit={null}
-        validationSchema={validationSchema}>
-        <Form>
-          <PatientRegistrationContext.Provider
-            value={{
-              initialFormValues: null,
-              identifierTypes: [],
-              validationSchema,
-              setValidationSchema: () => {},
-              values: formValues,
-              inEditMode: false,
-              setFieldValue: () => null,
-              currentPhoto: 'TEST',
-              isOffline: true,
-              setCapturePhotoProps: (value) => {},
-            }}>
-            <NameField />
-          </PatientRegistrationContext.Provider>
-        </Form>
-      </Formik>,
-    );
-    const givenNameInput = screen.getByLabelText('First Name') as HTMLInputElement;
-    const middleNameInput = screen.getByLabelText(/Middle Name/i) as HTMLInputElement;
-    const familyNameInput = screen.getByLabelText('Family Name') as HTMLInputElement;
-
-    await user.click(givenNameInput);
-
-    fireEvent.change(givenNameInput, { target: { value: givenNameValue } });
-    fireEvent.blur(givenNameInput);
-    fireEvent.change(middleNameInput, { target: { value: middleNameValue } });
-    fireEvent.blur(middleNameInput);
-    fireEvent.change(familyNameInput, { target: { value: familyNameValue } });
-    fireEvent.blur(familyNameInput);
-
-    return {
-      givenNameError: screen.queryByText('Given name is required'),
-      middleNameError: screen.queryByText('Middle name is required'),
-      familyNameError: screen.queryByText('Family name is required'),
     };
-  };
 
-  testValidName('Aaron', 'A', 'Aaronson');
-  testValidName('No', '', 'Middle Name');
-  testInvalidName('', '', '', 'Given name is required', 'givenNameError');
-  testInvalidName('', '', '', 'Family name is required', 'familyNameError');
-  testInvalidName('', 'No', 'Given Name', 'Given name is required', 'givenNameError');
-  testInvalidName('No', 'Family Name', '', 'Family name is required', 'familyNameError');
+    const validateFormValues = async (formValues) => {
+      try {
+        await validationSchema.validate(formValues, { abortEarly: false });
+      } catch (err) {
+        return err;
+      }
+    };
+
+    it('should allow valid form values', async () => {
+      const validationError = await validateFormValues(validFormValues);
+      expect(validationError).toBeFalsy();
+    });
+
+    it('should require givenName', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        givenName: '',
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('givenNameRequired');
+    });
+
+    it('should require familyName', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        familyName: '',
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('familyNameRequired');
+    });
+
+    it('should require additionalGivenName when addNameInLocalLanguage is true', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        addNameInLocalLanguage: true,
+        additionalGivenName: '',
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('givenNameRequired');
+    });
+
+    it('should require additionalFamilyName when addNameInLocalLanguage is true', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        addNameInLocalLanguage: true,
+        additionalFamilyName: '',
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('familyNameRequired');
+    });
+
+    it('should require gender', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        gender: '',
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('genderUnspecified');
+    });
+
+    it('should allow Male as a valid gender', async () => {
+      const validFormValuesWithOtherGender = {
+        ...validFormValues,
+        gender: 'Male',
+      };
+      const validationError = await validateFormValues(validFormValuesWithOtherGender);
+      expect(validationError).toBeFalsy();
+    });
+
+    it('should allow Female as a valid gender', async () => {
+      const validFormValuesWithOtherGender = {
+        ...validFormValues,
+        gender: 'Female',
+      };
+      const validationError = await validateFormValues(validFormValuesWithOtherGender);
+      expect(validationError).toBeFalsy();
+    });
+
+    it('should throw error when date of birth is a future date', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        birthdate: new Date('2100-01-01'),
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('birthdayNotInTheFuture');
+    });
+
+    it('should require yearsEstimated when birthdateEstimated is true', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        birthdateEstimated: true,
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('yearsEstimateRequired');
+    });
+
+    it('should throw error when monthEstimated is negative', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        birthdateEstimated: true,
+        yearsEstimated: 0,
+        monthsEstimated: -1,
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('negativeMonths');
+    });
+
+    it('should throw error when deathDate is in future', async () => {
+      const invalidFormValues = {
+        ...validFormValues,
+        deathDate: new Date('2100-01-01'),
+      };
+      const validationError = await validateFormValues(invalidFormValues);
+      expect(validationError.errors).toContain('deathdayNotInTheFuture');
+    });
+  });
 });
