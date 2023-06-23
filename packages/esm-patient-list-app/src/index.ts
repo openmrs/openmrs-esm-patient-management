460f6b19e0fbb21f27cbb5e883033b91ca73d2fa
Ian <ian_bacher@brown.edu>
2023-06-23 13:52:59 -0400

    (fix) Exports should reference the result of getSync / getAsync

--- packages/esm-patient-list-app/src/index.ts
+++ packages/esm-patient-list-app/src/index.ts
@@ -38,30 +38,29 @@ export function startupApp() {
   ]);
 }
 
-export const root = () => getAsyncLifecycle(() => import('./root.component'), options);
+export const root = getAsyncLifecycle(() => import('./root.component'), options);
 
-export const addPatientToListModal = () =>
-  getAsyncLifecycle(() => import('./add-patient/add-patient.component'), {
-    featureName: 'patient-actions-modal',
-    moduleName,
-  });
+export const addPatientToListModal = getAsyncLifecycle(() => import('./add-patient/add-patient.component'), {
+  featureName: 'patient-actions-modal',
+  moduleName,
+});
 
-export const addPatientToPatientListMenuItem = () =>
-  getAsyncLifecycle(() => import('./add-patient-to-patient-list-menu-item.component'), {
+export const addPatientToPatientListMenuItem = getAsyncLifecycle(
+  () => import('./add-patient-to-patient-list-menu-item.component'),
+  {
     featureName: 'patient-actions-slot',
     moduleName,
-  });
+  },
+);
 
-export const patientListActionButton = () =>
-  getAsyncLifecycle(() => import('./patient-list-action-button.component'), {
-    featureName: 'patient-list-action-menu-item',
-    moduleName,
-  });
+export const patientListActionButton = getAsyncLifecycle(() => import('./patient-list-action-button.component'), {
+  featureName: 'patient-list-action-menu-item',
+  moduleName,
+});
 
-export const patientListDashboardLink = () => getSyncLifecycle(createDashboardLink(dashboardMeta), options);
+export const patientListDashboardLink = getSyncLifecycle(createDashboardLink(dashboardMeta), options);
 
-export const patientTable = () =>
-  getAsyncLifecycle(() => import('./patient-table/patient-table.component'), {
-    featureName: 'patient-table',
-    moduleName,
-  });
+export const patientTable = getAsyncLifecycle(() => import('./patient-table/patient-table.component'), {
+  featureName: 'patient-table',
+  moduleName,
+});
