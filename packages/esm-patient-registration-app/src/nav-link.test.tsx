75cfa4ec61489bbb2b35c8d9c90f76abbc789a70
Ayush <ayushmishra2854@gmail.com>
2023-07-02 02:39:12 +0530

    add-patient-link and nav-link tests

---
+++ packages/esm-patient-registration-app/src/nav-link.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Root from './nav-link';
+
+describe('Root component', () => {
+  it('renders a link to the patient registration page', () => {
+    const { getByText } = render(<Root />);
+    const linkElement = getByText('Patient Registration');
+
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', '/openmrs/spa/patient-registration');
+  });
+});
