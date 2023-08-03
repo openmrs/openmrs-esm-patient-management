ecccb23d3c40f79135138cac067231b3a57a05d3
piumal1999 <piumalrathnayake@hotmail.com>
2023-08-03 18:11:50 +0800

    Remove unwanted MFs

--- e2e/support/github/run-e2e-docker-env.sh
+++ e2e/support/github/run-e2e-docker-env.sh
@@ -29,7 +29,7 @@ echo "Creating dynamic spa-assemble-config.json..."
 jq -n \
   --arg apps "$apps" \
   --arg app_names "$(echo ${app_names[@]})" \
-  '{"@openmrs/esm-login-app": "next", "@openmrs/esm-primary-navigation-app": "next", "@openmrs/esm-home-app": "next", "@openmrs/esm-form-entry-app": "next", "@openmrs/esm-generic-patient-widgets-app": "next", "@openmrs/esm-patient-allergies-app": "next", "@openmrs/esm-patient-banner-app": "next", "@openmrs/esm-patient-chart-app": "next"} + (
+  '{"@openmrs/esm-login-app": "next", "@openmrs/esm-primary-navigation-app": "next", "@openmrs/esm-home-app": "next", "@openmrs/esm-generic-patient-widgets-app": "next", "@openmrs/esm-patient-chart-app": "next"} + (
     ($apps | split("\n")) as $apps | ($app_names | split(" ") | map("/app/" + .)) as $app_files
     | [$apps, $app_files]
     | transpose
