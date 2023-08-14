3a954c06f527866c2db37407902654f0a407a75d
Ayush <ayushmishra2854@gmail.com>
2023-08-15 03:29:45 +0530

    Add tests for patient-search.component.tsx

---
+++ packages/esm-patient-search-app/src/compact-patient-search/patient-search.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import PatientSearch from './patient-search.component';
+import { useConfig } from '@openmrs/esm-framework';
+import { useTranslation } from 'react-i18next';
+
+const mockedUseConfig = useConfig as jest.Mock;
+
+jest.mock('@openmrs/esm-framework', () => ({
+  ...jest.requireActual('@openmrs/esm-framework'),
+}));
+
+describe('PatientSearch', () => {
+  const mockProps = {
+    query: 'John',
+    isLoading: false,
+    data: [
+      {
+        uuid: 'patient-1',
+        person: {
+          personName: {
+            givenName: 'John',
+            middleName: 'Doe',
+            familyName: 'Smith',
+          },
+          gender: 'M',
+          birthdate: '1990-01-01',
+        },
+        identifiers: [
+          {
+            identifierType: {
+              uuid: 'identifier-type-1',
+              display: 'National ID',
+            },
+            identifier: '123456',
+          },
+        ],
+      },
+    ],
+    fetchError: false,
+    loadingNewData: false,
+    setPage: jest.fn(),
+    hasMore: false,
+    totalResults: 1,
+  };
+
+  beforeEach(() => {
+    mockedUseConfig.mockReturnValue({
+      defaultIdentifierTypes: ['identifier-type-1', 'identifier-type-2'],
+      search: {
+        patientResultUrl: '/patient/{{patientUuid}}/chart',
+      },
+    });
+  });
+
+  it('should render search results correctly', () => {
+    render(<PatientSearch currentPage={0} {...mockProps} />);
+
+    const resultsTextElement = screen.getByText('John Doe Smith');
+    expect(resultsTextElement).toBeInTheDocument();
+  });
+
+  it('should render loading state correctly', () => {
+    render(<PatientSearch currentPage={0} {...mockProps} isLoading={true} />);
+
+    const skeletonAvatar = screen.getAllByTestId('search-skeleton');
+    expect(skeletonAvatar.length).toBeGreaterThan(0);
+  });
+
+  it('should render error state correctly', () => {
+    const error = new Error('Error');
+    render(<PatientSearch {...mockProps} fetchError={error} />);
+
+    const errorMessageElement = screen.getByText('Error');
+    expect(errorMessageElement).toBeInTheDocument();
+  });
+});
