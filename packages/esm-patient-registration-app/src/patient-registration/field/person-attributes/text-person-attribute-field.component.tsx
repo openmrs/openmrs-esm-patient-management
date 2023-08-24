0d28fbe11bd5374f452fa1d26c8f7c34a0c4ca33
Ayush <ayushmishra2854@gmail.com>
2023-08-24 15:40:07 +0530

    fixed errors

--- packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/text-person-attribute-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/text-person-attribute-field.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Field } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { Input } from '../../input/basic-input/input/input.component';
+import styles from './../field.scss';
 import { PersonAttributeTypeResponse } from '../../patient-registration.types';
 
 export interface TextPersonAttributeFieldProps {
