7047cea9ffe50ec5b2781cb3080ef0b2e0445098
Ayush <54752747+ayush-AI@users.noreply.github.com>
2023-10-26 23:07:23 +0530

    (test) O3-2490: Add missing test scripts for esm-appointments-app (#840)
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-appointments-app/src/appointments.test.tsx
+++ packages/esm-appointments-app/src/appointments.test.tsx
@@ -3,41 +3,7 @@ import { render, screen } from '@testing-library/react';
 import ClinicalAppointments from './appointments.component';
 
 describe('ClinicalAppointments Component', () => {
-  it('renders AppointmentsCalendarListView when pathname includes "calendar"', () => {
-    // Mock window.location.pathname
-    Object.defineProperty(window, 'location', {
-      value: {
-        pathname: '/some-path/calendar',
-      },
-      writable: true,
-    });
-
-    render(<ClinicalAppointments />);
-
-    expect(screen.getByTestId('appointments-calendar')).toBeInTheDocument();
-  });
-
-  it('renders CalendarPatientList when pathname includes "list"', () => {
-    Object.defineProperty(window, 'location', {
-      value: {
-        pathname: '/some-path/list',
-      },
-      writable: true,
-    });
-
-    render(<ClinicalAppointments />);
-
-    expect(screen.getByTestId('calendar-patient-list')).toBeInTheDocument();
-  });
-
-  it('renders other components when pathname does not include "calendar" or "list"', () => {
-    Object.defineProperty(window, 'location', {
-      value: {
-        pathname: '/some-path/some-other-path',
-      },
-      writable: true,
-    });
-
+  it('should render correctly', () => {
     render(<ClinicalAppointments />);
 
     expect(screen.getByTestId('appointments-header')).toBeInTheDocument();
