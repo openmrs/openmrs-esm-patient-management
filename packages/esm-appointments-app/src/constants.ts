e6b6921738aacfd5126e6bf3c867b51f43d5e670
Chi Bong Ho <cbho@pih.org>
2023-12-13 17:20:39 -0500

    (feat) In Appointments App, filter out location dropdowns to only show appointment locations

--- packages/esm-appointments-app/src/constants.ts
+++ packages/esm-appointments-app/src/constants.ts
@@ -2,3 +2,4 @@ export const spaRoot = window['getOpenmrsSpaBase'];
 export const basePath = '/appointments';
 export const spaBasePath = `${window.spaBase}/home`;
 export const omrsDateFormat = 'YYYY-MM-DDTHH:mm:ss.SSSZZ';
+export const appointmentLocationTagName = 'Appointment Location';
