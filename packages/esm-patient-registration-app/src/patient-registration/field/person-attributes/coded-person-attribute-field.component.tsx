471f749a274cc3ff33aaf78d9786d9521eb11d82
Brandon Istenes <bistenes@gmail.com>
2023-12-24 12:08:52 -0500

    (fix) Coded person attribute field should report error if not configured correctly

--- packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/coded-person-attribute-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/coded-person-attribute-field.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { Field } from 'formik';
@@ -7,6 +7,7 @@ import { Input } from '../../input/basic-input/input/input.component';
 import { type PersonAttributeTypeResponse } from '../../patient-registration.types';
 import { useConceptAnswers } from '../field.resource';
 import styles from './../field.scss';
+import { reportError } from '@openmrs/esm-framework';
 
 export interface CodedPersonAttributeFieldProps {
   id: string;
@@ -24,6 +25,22 @@ export function CodedPersonAttributeField({
   const { data: conceptAnswers, isLoading: isLoadingConceptAnswers } = useConceptAnswers(answerConceptSetUuid);
   const { t } = useTranslation();
   const fieldName = `attributes.${personAttributeType.uuid}`;
+
+  useEffect(() => {
+    if (!answerConceptSetUuid) {
+      reportError(
+        `The person attribute field '${id}' is of type 'coded' but has been defined without an answer concept set UUID. The 'answerConceptSetUuid' key is required.`,
+      );
+    }
+  }, [answerConceptSetUuid]);
+
+  useEffect(() => {
+    if (!isLoadingConceptAnswers && conceptAnswers?.length == 0) {
+      reportError(
+        `The coded person attribute field '${id}' has been defined with an answer concept set UUID '${answerConceptSetUuid}' that does not have any concept answers.`,
+      );
+    }
+  }, [isLoadingConceptAnswers, conceptAnswers]);
 
   return (
     <div className={classNames(styles.customField, styles.halfWidthInDesktopView)}>
