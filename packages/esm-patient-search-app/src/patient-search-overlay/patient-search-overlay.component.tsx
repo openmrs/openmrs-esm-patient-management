7c0e572bc700b555cef2f2818db192701a026c39
Usama Idriss Kakumba <53287480+usamaidrsk@users.noreply.github.com>
2024-02-05 21:00:57 +0300

    (feat) O3-2792: Increase advanced search page result count from 5 to 20 (#958)
    * refactor: change number of patients listed on advanced search page from 5 to 20
    
    * Fixup
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-patient-search-app/src/patient-search-overlay/patient-search-overlay.component.tsx
+++ packages/esm-patient-search-app/src/patient-search-overlay/patient-search-overlay.component.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDebounce } from '@openmrs/esm-framework';
+import AdvancedPatientSearchComponent from '../patient-search-page/advanced-patient-search.component';
 import Overlay from '../ui-components/overlay';
 import PatientSearchBar from '../patient-search-bar/patient-search-bar.component';
-import debounce from 'lodash-es/debounce';
-import AdvancedPatientSearchComponent from '../patient-search-page/advanced-patient-search.component';
 
 interface PatientSearchOverlayProps {
   onClose: () => void;
@@ -14,28 +14,22 @@ interface PatientSearchOverlayProps {
 const PatientSearchOverlay: React.FC<PatientSearchOverlayProps> = ({ onClose, query = '', header }) => {
   const { t } = useTranslation();
   const [searchTerm, setSearchTerm] = useState(query);
-  const handleClear = useCallback(() => setSearchTerm(''), [setSearchTerm]);
-  const showSearchResults = useMemo(() => !!searchTerm?.trim(), [searchTerm]);
+  const showSearchResults = Boolean(searchTerm?.trim());
+  const debouncedSearchTerm = useDebounce(searchTerm);
 
-  useEffect(() => {
-    if (query) {
-      setSearchTerm(query);
-    }
-  }, [query]);
+  const handleClearSearchTerm = useCallback(() => setSearchTerm(''), [setSearchTerm]);
 
-  const onSearchQueryChange = debounce((val) => {
-    setSearchTerm(val);
-  }, 300);
+  const onSearchTermChange = (query: string) => setSearchTerm(query);
 
   return (
     <Overlay header={header ?? t('searchResults', 'Search results')} close={onClose}>
       <PatientSearchBar
         initialSearchTerm={query}
-        onSubmit={onSearchQueryChange}
-        onChange={onSearchQueryChange}
-        onClear={handleClear}
+        onChange={onSearchTermChange}
+        onClear={handleClearSearchTerm}
+        onSubmit={onSearchTermChange}
       />
-      {showSearchResults && <AdvancedPatientSearchComponent query={searchTerm} inTabletOrOverlay />}
+      {showSearchResults && <AdvancedPatientSearchComponent query={debouncedSearchTerm} inTabletOrOverlay />}
     </Overlay>
   );
 };
