b95039765501477775a773fb0397e60e39550f17
Gaurav23 <97234987+gaurav-lomte23@users.noreply.github.com>
2024-02-13 00:05:19 +0530

    Update provider-operations.ts
    Implement error handling to catch any errors that might occur during API requests and handle them appropriately, I have added the function for it if you find it useful please merge it. Thank you.

--- e2e/commands/provider-operations.ts
+++ e2e/commands/provider-operations.ts
@@ -2,10 +2,23 @@ import { Provider } from '../../packages/esm-appointments-app/src/types/index';
 import { APIRequestContext, expect } from '@playwright/test';
 
 export const getProvider = async (api: APIRequestContext): Promise<Provider> => {
-  const providerRes = await api.get('provider?q=admin', {
-    data: {},
-  });
-  await expect(providerRes.ok()).toBeTruthy();
-  const { results } = await providerRes.json();
-  return await results[0];
+  try {
+    const response = await api.get('provider?q=admin', {
+      data: {},
+    });
+    // Ensure the response is successful
+    if (!response.ok()) {
+      throw new Error('Failed to fetch provider');
+    }
+    const { results } = await response.json();
+    // Ensure there's at least one provider returned
+    if (!results || results.length === 0) {
+      throw new Error('No provider found');
+    }
+    // Return the first provider
+    return results[0];
+  } catch (error) {
+    console.error('Error while fetching provider:', error);
+    throw error; // Propagate the error to the caller
+  }
 };
