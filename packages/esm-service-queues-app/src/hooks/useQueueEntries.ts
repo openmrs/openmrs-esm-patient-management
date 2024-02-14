74c810e26377f0ba913213045478db127dc59982
Chi Bong Ho <cbho@pih.org>
2024-02-14 15:50:53 -0500

    change useQueueEntries to add a rep param

--- packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
+++ packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
@@ -2,14 +2,12 @@ import { openmrsFetch } from '@openmrs/esm-framework';
 import { type QueueEntry, type QueueEntrySearchCriteria } from '../types';
 import useSWR from 'swr';
 
-// TODO: make a utility function in esm-core for unpacking the result
-// from useSWR
-export function useQueueEntries(searchCriteria?: QueueEntrySearchCriteria) {
+export function useQueueEntries(searchCriteria?: QueueEntrySearchCriteria, rep: string = 'default') {
   const searchParam = new URLSearchParams();
   for (let [key, value] of Object.entries(searchCriteria)) {
     searchParam.append(key, value.toString());
   }
-  searchParam.append('v', 'full');
+  searchParam.append('v', rep);
 
   const apiUrl = `/ws/rest/v1/queue-entry?` + searchParam.toString();
   const { data, ...rest } = useSWR<{ data: { results: Array<QueueEntry> } }, Error>(apiUrl, openmrsFetch);
