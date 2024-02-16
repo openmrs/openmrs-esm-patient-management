79de892beb92f8f5906d45410c8258efec517459
Dennis Kigen <kigen.work@gmail.com>
2024-02-17 00:40:31 +0300

    (chore) Remove duplicated SWR config

--- packages/esm-appointments-app/src/root.component.tsx
+++ packages/esm-appointments-app/src/root.component.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { SWRConfig } from 'swr';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppointmentsCalendarView from './calendar/appointments-calendar-view.component';
 import Appointments from './appointments.component';
 import CalendarPatientList from './calendar/patient-list/calendar-patient-list.component';
-
-const swrConfiguration = {
-  errorRetryCount: 3,
-};
 
 const RootComponent: React.FC = () => {
   const appointmentsBasename = window.getOpenmrsSpaBase() + 'home/appointments';
 
   return (
     <main>
-      <SWRConfig value={swrConfiguration}>
-        <BrowserRouter basename={appointmentsBasename}>
-          <Routes>
-            <Route path="/" element={<Appointments />} />
-            <Route path="/calendar" element={<AppointmentsCalendarView />} />
-            <Route path="/list/:dateTime/:serviceName" element={<CalendarPatientList />} />
-          </Routes>
-        </BrowserRouter>
-      </SWRConfig>
+      <BrowserRouter basename={appointmentsBasename}>
+        <Routes>
+          <Route path="/" element={<Appointments />} />
+          <Route path="/calendar" element={<AppointmentsCalendarView />} />
+          <Route path="/list/:dateTime/:serviceName" element={<CalendarPatientList />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 };
--- packages/esm-service-queues-app/src/root.component.tsx
+++ packages/esm-service-queues-app/src/root.component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { SWRConfig } from 'swr';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppointmentsTable from './queue-patient-linelists/scheduled-appointments-table.component';
 import Home from './home.component';
@@ -7,28 +6,21 @@ import ServicesTable from './queue-patient-linelists/queue-services-table.compon
 import QueueScreen from './queue-screen/queue-screen.component';
 import QueueTableByStatusView from './views/queue-table-by-status-view.component';
 
-const swrConfiguration = {
-  // Maximum number of retries when the backend returns an error
-  errorRetryCount: 3,
-};
-
 const Root: React.FC = () => {
   const serviceQueuesBasename = window.getOpenmrsSpaBase() + 'home/service-queues';
 
   return (
     <main>
-      <SWRConfig value={swrConfiguration}>
-        <BrowserRouter basename={serviceQueuesBasename}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/queue-table-by-status/:queueUuid" element={<QueueTableByStatusView />} />
-            <Route path="/queue-table-by-status/:queueUuid/:statusUuid" element={<QueueTableByStatusView />} />
-            <Route path="/screen" element={<QueueScreen />} />
-            <Route path="/appointments-list/:value/" element={<AppointmentsTable />} />
-            <Route path="/queue-list/:service/:serviceUuid/:locationUuid" element={<ServicesTable />} />
-          </Routes>
-        </BrowserRouter>
-      </SWRConfig>
+      <BrowserRouter basename={serviceQueuesBasename}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/queue-table-by-status/:queueUuid" element={<QueueTableByStatusView />} />
+          <Route path="/queue-table-by-status/:queueUuid/:statusUuid" element={<QueueTableByStatusView />} />
+          <Route path="/screen" element={<QueueScreen />} />
+          <Route path="/appointments-list/:value/" element={<AppointmentsTable />} />
+          <Route path="/queue-list/:service/:serviceUuid/:locationUuid" element={<ServicesTable />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 };
