b17a4bd811aca693d995de6a11a8eb581a3d5bfe
Dennis Kigen <kigen.work@gmail.com>
2024-02-18 07:08:20 +0300

    (chore) Remove duplicated SWR config (#987)

--- packages/esm-appointments-app/src/home/index.tsx
+++ packages/esm-appointments-app/src/home/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { SWRConfig } from 'swr';
 
 import AppointmentsBaseTable from './appointments-base-table.component';
 
-const swrConfiguration = {
-  errorRetryCount: 3,
-};
-
 const HomeAppointments = () => {
-  return (
-    <SWRConfig value={swrConfiguration}>
-      <AppointmentsBaseTable />
-    </SWRConfig>
-  );
+  return <AppointmentsBaseTable />;
 };
 
 export default HomeAppointments;
