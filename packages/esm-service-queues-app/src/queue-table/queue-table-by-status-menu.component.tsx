f105b6fe4613b317c36cbef4ba5472ed78f40644
Michael Seaton <mseaton@pih.org>
2024-02-29 13:56:41 -0500

    use configurable link

--- packages/esm-service-queues-app/src/queue-table/queue-table-by-status-menu.component.tsx
+++ packages/esm-service-queues-app/src/queue-table/queue-table-by-status-menu.component.tsx
@@ -1,23 +1,38 @@
 import { SideNavMenu, SideNavMenuItem } from '@carbon/react';
-import { useSession } from '@openmrs/esm-framework';
+import { ConfigurableLink } from '@openmrs/esm-framework';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { type Queue } from '../types/index';
 import { useQueues } from '../hooks/useQueues';
+import classNames from "classnames";
+import {BrowserRouter, useLocation} from "react-router-dom";
 
-interface QueueTableByStatusMenuProps {
-  locationUuid: string;
+function QueueTableLink({ basePath, queue }: { basePath:string, queue: Queue }) {
+  const { t } = useTranslation();
+  const location = useLocation();
+  const pageUrl = basePath + '/' + queue.uuid;
+  const selected = location.pathname.startsWith(pageUrl);
+  return (
+    <ConfigurableLink
+      className={classNames('cds--side-nav__link', { 'active-left-nav-link': selected })}
+      to={`${basePath}/${queue.uuid}`}
+    >
+      {queue.display}
+    </ConfigurableLink>
+  );
 }
 
-export default function QueueTableByStatusMenu({ locationUuid }: QueueTableByStatusMenuProps) {
+export default function QueueTableByStatusMenu() {
   const { t } = useTranslation();
-  const { sessionLocation } = useSession();
   const { queues } = useQueues();
-
+  const basePath = `${window.spaBase}/home/service-queues/queue-table-by-status`; // TODO: Configure elsewhere?
   return (
     <SideNavMenu defaultExpanded={true} title={t('serviceQueues', 'Service Queues')}>
       {queues.map((queue) => (
-        <SideNavMenuItem key={queue.uuid} href={`home/service-queues/queue-table-by-status/${queue.uuid}`}>
-          {queue.display}
+        <SideNavMenuItem key={queue.uuid}>
+          <BrowserRouter>
+            <QueueTableLink basePath={basePath} queue={queue} />
+          </BrowserRouter>
         </SideNavMenuItem>
       ))}
     </SideNavMenu>
