abb5f4aa591535a9933675f784c83713dc25f6c5
Mark Goodrich <mgoodrich@pih.org>
2024-03-12 17:55:21 -0400

    O3-2922: Appointments: Support Visits at Parent Locations

--- packages/esm-appointments-app/src/hooks/useVisits.ts
+++ packages/esm-appointments-app/src/hooks/useVisits.ts
@@ -14,9 +14,7 @@ export const useVisits = () => {
 
   const visitsUrl = `${restBaseUrl}/visit?includeInactive=true&includeParentLocations=true&v=custom:(uuid,patient:(uuid,identifiers:(identifier,uuid),person:(age,display,gender,uuid)),visitType:(uuid,name,display),location:(uuid,name,display),startDatetime,stopDatetime)&fromStartDate=${dayjs(
     currentAppointmentDate,
-  ).format('YYYY-MM-DD')}&toStartDate=${dayjs(currentAppointmentDate)
-    .add(1, 'day')
-    .format('YYYY-MM-DD')}&location=${session?.sessionLocation?.uuid}`;
+  ).format('YYYY-MM-DD')}&location=${session?.sessionLocation?.uuid}`;
 
   const { data, error, isLoading, mutate } = useSWR<{ data: { results: Visit[] } }>(visitsUrl, openmrsFetch);
   const visits = data?.data?.results ?? [];
