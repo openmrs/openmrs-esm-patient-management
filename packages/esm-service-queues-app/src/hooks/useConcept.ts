bbdaab8732735c4d1ab640829ce05db9fd33fa7c
Michael Seaton <mseaton@pih.org>
2024-03-12 13:39:27 -0400

    Update packages/esm-service-queues-app/src/hooks/useConcept.ts
    Co-authored-by: Ian <52504170+ibacher@users.noreply.github.com>

--- packages/esm-service-queues-app/src/hooks/useConcept.ts
+++ packages/esm-service-queues-app/src/hooks/useConcept.ts
@@ -3,7 +3,7 @@ import useSWRImmutable from 'swr/immutable';
 import { type Concept } from '../types';
 
 export function useConcept(uuid: string) {
-  const apiUrl = `${restBaseUrl}/concept/${uuid}`;
+  const apiUrl = uuid ? `${restBaseUrl}/concept/${uuid}` : undefined;
   const { data, error, isLoading } = useSWRImmutable<{ data: Concept }, Error>(apiUrl, openmrsFetch);
   return {
     concept: data?.data,
