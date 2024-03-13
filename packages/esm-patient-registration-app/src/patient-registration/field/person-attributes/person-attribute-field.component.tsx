c527eca144bc9c54d2d4e8f09bb6b310d1030645
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-03-13 14:39:28 +0300

    ft-create-location-attribute component

--- packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/person-attribute-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/person-attribute-field.component.tsx
@@ -6,6 +6,7 @@ import { usePersonAttributeType } from './person-attributes.resource';
 import { TextPersonAttributeField } from './text-person-attribute-field.component';
 import { useTranslation } from 'react-i18next';
 import styles from '../field.scss';
+import { LocationPersonAttributeField } from './location-person-attribute-field.component';
 
 export interface PersonAttributeFieldProps {
   fieldDefinition: FieldDefinition;
@@ -38,6 +39,16 @@ export function PersonAttributeField({ fieldDefinition }: PersonAttributeFieldPr
             label={fieldDefinition.label}
             id={fieldDefinition?.id}
             customConceptAnswers={fieldDefinition.customConceptAnswers ?? []}
+          />
+        );
+      case 'org.openmrs.Location':
+        return (
+          <LocationPersonAttributeField
+            personAttributeType={personAttributeType}
+            // answerConceptSetUuid={fieldDefinition.answerConceptSetUuid}
+            label={fieldDefinition.label}
+            id={fieldDefinition?.id}
+            // customConceptAnswers={fieldDefinition.customConceptAnswers ?? []}
           />
         );
       default:
