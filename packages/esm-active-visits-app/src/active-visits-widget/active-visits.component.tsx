5684dfb6e512f5b1aa464e79c93cfb17ab96b049
Brandon Istenes <bistenes@gmail.com>
2024-03-14 11:48:56 -0400

    Fixup

--- packages/esm-active-visits-app/src/active-visits-widget/active-visits.component.tsx
+++ packages/esm-active-visits-app/src/active-visits-widget/active-visits.component.tsx
@@ -231,12 +231,10 @@ const ActiveVisitsTable = () => {
                     const patientChartUrl = '${openmrsSpaBase}/patient/${patientUuid}/chart/Patient%20Summary';
 
                     return (
-                      <React.Fragment key={index}>
-                        <TableExpandRow
-                          {...getRowProps({ row })}
-                          data-testid={`activeVisitRow${currentVisit.patientUuid || 'unknown'}`}>
+                      <React.Fragment key={`active-visit-row-${index}`}>
+                        <TableExpandRow {...getRowProps({ row })}>
                           {row.cells.map((cell) => (
-                            <TableCell key={cell.id} data-testid={cell.id}>
+                            <TableCell key={`active-visit-row-${index}-cell-${cell.id}`}>
                               {cell.info.header === 'name' && currentVisit.patientUuid ? (
                                 <ConfigurableLink
                                   to={patientChartUrl}
