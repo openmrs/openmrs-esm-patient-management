8e9ece01b88dd4bef09ff4f9893ea79c1575257a
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-03-16 17:11:03 +0300

    feat: create person location attribute

--- packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/location-person-attribute-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/person-attributes/location-person-attribute-field.component.tsx
@@ -1,66 +1,76 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, useState, useEffect } from 'react';
 import classNames from 'classnames';
-import { Field, useField } from 'formik';
-import { useTranslation } from 'react-i18next';
-import { Input } from '../../input/basic-input/input/input.component';
+import { useField } from 'formik';
 import { type PersonAttributeTypeResponse } from '../../patient-registration.types';
 import styles from './../field.scss';
-import { useAddressEntries, useAddressEntryFetchConfig } from './location-person-attribute-field.resource';
-import ComboInput from '../../input/combo-input/combo-input.component';
-
+import { useLocations } from './location-person-attribute-field.resource';
+import { ComboBox, Layer } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
 export interface LocationPersonAttributeFieldProps {
   id: string;
   personAttributeType: PersonAttributeTypeResponse;
   validationRegex?: string;
   label?: string;
+  locationTag: string;
   required?: boolean;
 }
 
 export function LocationPersonAttributeField({
-  id,
   personAttributeType,
-  validationRegex,
+  id,
   label,
+  locationTag,
   required,
 }: LocationPersonAttributeFieldProps) {
   const { t } = useTranslation();
   const [field, meta, { setValue }] = useField(`attributes.${personAttributeType.uuid}`);
-  const { fetchEntriesForField, searchString, updateChildElements } = useAddressEntryFetchConfig(
-    personAttributeType.name,
-  );
-  const { entries } = useAddressEntries(fetchEntriesForField, searchString);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const { locations } = useLocations(locationTag || null, 5, searchQuery);
 
-  const handleInputChange = useCallback(
-    (newValue) => {
-      setValue(newValue);
+  const locationOptions = useMemo(() => {
+    return locations.map(({ resource: { id, name } }) => ({ value: id, label: name }));
+  }, [locations]);
+
+  useEffect(() => {
+    if (meta?.value?.uuid) {
+      setValue(meta.value.uuid);
+    }
+  }, [meta, setValue]);
+
+  const onInputChange = useCallback(
+    (value: string | null) => {
+      if (value) {
+        if (locationOptions.find(({ label }) => label === value)) return;
+        setSearchQuery(value);
+        setValue(null);
+      }
+    },
+    [locationOptions, setValue],
+  );
+
+  const handleSelect = useCallback(
+    ({ selectedItem }) => {
+      if (selectedItem) {
+        setValue(selectedItem.value);
+      }
     },
     [setValue],
   );
 
-  const handleSelection = useCallback(
-    (selectedItem) => {
-      if (meta.value !== selectedItem) {
-        setValue(selectedItem);
-        updateChildElements();
-      }
-    },
-    [updateChildElements, meta.value, setValue],
-  );
-
   return (
     <div className={classNames(styles.customField, styles.halfWidthInDesktopView)}>
-      <ComboInput
-        entries={entries}
-        handleSelection={handleSelection}
-        name={`attributes.${personAttributeType.uuid}`}
-        fieldProps={{
-          ...field,
-          id: personAttributeType.uuid,
-          labelText: label,
-          required: required,
-        }}
-        handleInputChange={handleInputChange}
-      />
+      <Layer>
+        <ComboBox
+          titleText={label}
+          items={locationOptions}
+          id={id}
+          placeholder={t('searchLocationPersonAttribute', 'Search location')}
+          onInputChange={onInputChange}
+          required={required}
+          onChange={handleSelect}
+          selectedItem={locationOptions.find(({ value }) => value === field.value) || null}
+        />
+      </Layer>
     </div>
   );
 }
