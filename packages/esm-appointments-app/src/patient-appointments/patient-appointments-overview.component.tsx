5afc1020aa195debe960380226b44a3a5e8e3682
Mark Goodrich <mgoodrich@pih.org>
2024-03-22 09:11:15 -0400

    (feature) O3-2892: Add Patient-Centric View within the Appointments App

--- packages/esm-appointments-app/src/patient-appointments/patient-appointments-overview.component.tsx
+++ packages/esm-appointments-app/src/patient-appointments/patient-appointments-overview.component.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { usePatient } from '@openmrs/esm-framework';
+import { usePatient, useLayoutType } from '@openmrs/esm-framework';
 import PatientAppointmentsBase from './patient-appointments-base.component';
 import { useParams } from 'react-router-dom';
 import Overlay from '../overlay.component';
 import PatientAppointmentContext, { PatientAppointmentContextTypes } from '../hooks/patientAppointmentContext';
 import PatientAppointmentsHeader from './patient-appointments-header';
+import { DataTableSkeleton } from '@carbon/react';
 
 const PatientAppointmentsOverview: React.FC = () => {
   let params = useParams();
   const response = usePatient(params.patientUuid);
+  const layout = useLayoutType();
+  const isDesktop = layout === 'small-desktop' || layout === 'large-desktop';
 
   return response.isLoading ? (
-    <></>
+    <DataTableSkeleton role="progressbar" compact={isDesktop} zebra />
   ) : (
     <PatientAppointmentContext.Provider value={PatientAppointmentContextTypes.APPOINTMENTS_APP}>
       <PatientAppointmentsHeader patient={response.patient} />
