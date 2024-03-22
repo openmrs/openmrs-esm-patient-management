a4bf468bc5d8e0d3470719962ad035dbac03ba87
Mark Goodrich <mgoodrich@pih.org>
2024-03-22 08:58:57 -0400

    (feature) O3-2892: Add Patient-Centric View within the Appointments App

--- packages/esm-appointments-app/src/hooks/patientAppointmentContext.ts
+++ packages/esm-appointments-app/src/hooks/patientAppointmentContext.ts
@@ -1,5 +1,9 @@
 import { createContext } from 'react';
 
+/**
+ * When rendering the Patient Appointments view, let the view know if this is being rendered in the Patient Chart or the Appointments App
+ * (we need to know this to determine if we open the appointments form as part of the patient chart workspace or as an overlay in the appointments app)
+ */
 export enum PatientAppointmentContextTypes {
   PATIENT_CHART,
   APPOINTMENTS_APP,
