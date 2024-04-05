2697ab913b7564151b2711f1ede1afd8d5649562
Mark Goodrich <mgoodrich@pih.org>
2024-04-05 10:24:19 -0400

    (refactor) O3-2685: Inconsistent / Duplicated Appointments List UI between Home App and Appointments App

--- packages/esm-appointments-app/src/home/index.tsx
+++ packages/esm-appointments-app/src/home/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import AppointmentsList from '../appointments/scheduled/appointments-list.component';
 import dayjs from 'dayjs';
 import styles from './index.scss';
+import Overlay from '../overlay.component';
 
 const HomeAppointments = () => {
   const { t } = useTranslation();
@@ -13,6 +14,7 @@ const HomeAppointments = () => {
         date={dayjs().startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSSZZ')}
         title={t('todays', "Today's")}
       />
+      <Overlay />
     </div>
   );
 };
