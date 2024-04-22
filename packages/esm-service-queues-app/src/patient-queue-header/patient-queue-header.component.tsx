3651ba41af11e0541dafbd74c4f10dbc9572a529
Bhargav Kodali <kodalibhargav019@gmail.com>
2024-04-22 17:50:33 +0530

    fix-Remove Location dropdown

--- packages/esm-service-queues-app/src/patient-queue-header/patient-queue-header.component.tsx
+++ packages/esm-service-queues-app/src/patient-queue-header/patient-queue-header.component.tsx
@@ -1,36 +1,15 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Calendar, Location } from '@carbon/react/icons';
-import { Dropdown } from '@carbon/react';
 import { formatDate, useSession } from '@openmrs/esm-framework';
 import PatientQueueIllustration from './patient-queue-illustration.component';
-import { useQueueLocations } from '../patient-search/hooks/useQueueLocations';
 
-import {
-  updateSelectedQueueLocationUuid,
-  updateSelectedQueueLocationName,
-  updateSelectedServiceName,
-  useSelectedQueueLocationName,
-} from '../helpers/helpers';
 import styles from './patient-queue-header.scss';
 
 const PatientQueueHeader: React.FC<{ title?: string }> = ({ title }) => {
   const { t } = useTranslation();
-  const { queueLocations, isLoading, error } = useQueueLocations();
   const userSession = useSession();
   const userLocation = userSession?.sessionLocation?.display;
-  const currentQueueLocationName = useSelectedQueueLocationName();
-
-  const handleQueueLocationChange = useCallback(({ selectedItem }) => {
-    if (selectedItem.id === 'all') {
-      updateSelectedQueueLocationUuid(null);
-      updateSelectedQueueLocationName(null);
-    } else {
-      updateSelectedQueueLocationUuid(selectedItem.id);
-      updateSelectedQueueLocationName(selectedItem.name);
-      updateSelectedServiceName('All');
-    }
-  }, []);
 
   return (
     <>
@@ -49,19 +28,6 @@ const PatientQueueHeader: React.FC<{ title?: string }> = ({ title }) => {
             <span className={styles.middot}>&middot;</span>
             <Calendar size={16} />
             <span className={styles.value}>{formatDate(new Date(), { mode: 'standard' })}</span>
-          </div>
-          <div className={styles.dropdownContainer}>
-            <Dropdown
-              aria-label="Select queue location"
-              className={styles.dropdown}
-              id="queueLocationDropdown"
-              label={currentQueueLocationName ?? t('all', 'All')}
-              items={[{ id: 'all', name: t('all', 'All') }, ...queueLocations]}
-              itemToString={(item) => (item ? item.name : '')}
-              titleText={t('view', 'View')}
-              type="inline"
-              onChange={handleQueueLocationChange}
-            />
           </div>
         </div>
       </div>
