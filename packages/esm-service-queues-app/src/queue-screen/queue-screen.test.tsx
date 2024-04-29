01bc5471491eb4ff76ba6acbb1e8d4f331af7447
McCarthy <mccarthymutebi10@gmail.com>
2024-04-29 22:36:04 +0300

    (fix)the function useSelectedQueueLocationUuid was undefined during tests

--- packages/esm-service-queues-app/src/queue-screen/queue-screen.test.tsx
+++ packages/esm-service-queues-app/src/queue-screen/queue-screen.test.tsx
@@ -5,6 +5,7 @@ import QueueScreen from './queue-screen.component';
 
 jest.mock('./useActiveTickets');
 jest.mock('../helpers/helpers', () => ({
+  ...jest.requireActual('../helpers/helpers'),
   useSelectedQueueLocationName: jest.fn().mockReturnValue('Room A'),
 }));
 
