2cc7c0fafcecd9a6a9f6746fe256e3eb79fddf8c
Chi Bong Ho <cbho@pih.org>
2024-04-30 22:22:47 -0400

    remove tablesConfig mocks in tests

--- packages/esm-service-queues-app/src/queue-table/default-queue-table.test.tsx
+++ packages/esm-service-queues-app/src/queue-table/default-queue-table.test.tsx
@@ -20,58 +20,6 @@ mockedUseConfig.mockReturnValue({
   ...getDefaultsFromConfigSchema(configSchema),
   visitQueueNumberAttributeUuid: 'c61ce16f-272a-41e7-9924-4c555d0932c5',
   customPatientChartUrl: 'someUrl',
-  // TODO: remove after O3-3139 is fixed
-  tablesConfig: {
-    columnDefinitions: [
-      {
-        id: 'patient-name',
-        columnType: 'patient-name-column',
-      },
-      {
-        id: 'priority',
-        columnType: 'priority-column',
-        config: {
-          priorities: [
-            {
-              conceptUuid: 'priority-concept-uuid',
-              tagClassName: 'tag',
-              tagType: 'red',
-            },
-          ],
-        },
-      },
-      {
-        id: 'status',
-        columnType: 'status-column',
-        config: {
-          statuses: [
-            {
-              conceptUuid: 'status-concept-uuid',
-              iconComponent: 'InProgress',
-            },
-          ],
-        },
-      },
-      {
-        id: 'comingFrom',
-        columnType: 'queue-coming-from-column',
-      },
-      {
-        id: 'queue',
-        columnType: 'current-queue-column',
-      },
-      {
-        id: 'wait-time',
-        columnType: 'wait-time-column',
-      },
-    ],
-    tableDefinitions: [
-      {
-        columns: ['patient-name', 'comingFrom', 'priority', 'status', 'queue', 'wait-time'],
-        appliedTo: [{ queue: null, status: null }],
-      },
-    ],
-  },
 });
 
 jest.mock('@openmrs/esm-framework', () => {
@@ -159,7 +107,15 @@ describe('DefaultQueueTable: ', () => {
     expect(john).toBeInTheDocument();
     expect(john).toHaveAttribute('href', 'someUrl');
 
-    const expectedColumnHeaders = [/name/i, /priority/i, /coming from/i, /status/i, /^queue$/i, /wait time/i];
+    const expectedColumnHeaders = [
+      /name/i,
+      /priority/i,
+      /coming from/i,
+      /status/i,
+      /^queue$/i,
+      /wait time/i,
+      /actions/i,
+    ];
     expectedColumnHeaders.forEach((header) => {
       expect(
         screen.getByRole('columnheader', {
--- packages/esm-service-queues-app/src/queue-table/queue-table.test.tsx
+++ packages/esm-service-queues-app/src/queue-table/queue-table.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { defineConfigSchema, getDefaultsFromConfigSchema, useConfig, useSession } from '@openmrs/esm-framework';
+import { defineConfigSchema, useSession } from '@openmrs/esm-framework';
 import { screen, within } from '@testing-library/react';
 import { mockQueueEntries, mockSession } from '__mocks__';
 import { renderWithSwr } from 'tools';
@@ -7,65 +7,6 @@ import QueueTable from './queue-table.component';
 import { configSchema, defaultTablesConfig } from '../config-schema';
 
 const mockUseSession = useSession as jest.Mock;
-const mockedUseConfig = useConfig as jest.Mock;
-
-mockedUseConfig.mockReturnValue({
-  ...getDefaultsFromConfigSchema(configSchema),
-  visitQueueNumberAttributeUuid: 'c61ce16f-272a-41e7-9924-4c555d0932c5',
-  customPatientChartUrl: 'someUrl',
-  // TODO: remove after O3-3139 is fixed
-  tablesConfig: {
-    columnDefinitions: [
-      {
-        id: 'patient-name',
-        columnType: 'patient-name-column',
-      },
-      {
-        id: 'priority',
-        columnType: 'priority-column',
-        config: {
-          priorities: [
-            {
-              conceptUuid: 'priority-concept-uuid',
-              tagClassName: 'tag',
-              tagType: 'red',
-            },
-          ],
-        },
-      },
-      {
-        id: 'status',
-        columnType: 'status-column',
-        config: {
-          statuses: [
-            {
-              conceptUuid: 'status-concept-uuid',
-              iconComponent: 'InProgress',
-            },
-          ],
-        },
-      },
-      {
-        id: 'comingFrom',
-        columnType: 'queue-coming-from-column',
-      },
-      {
-        id: 'queue',
-        columnType: 'current-queue-column',
-      },
-      {
-        id: 'wait-time',
-        columnType: 'wait-time-column',
-      },
-    ],
-    tableDefinitions: [
-      {
-        columns: ['patient-name', 'comingFrom', 'priority', 'status', 'queue', 'wait-time'],
-        appliedTo: [{ queue: null, status: null }],
-      },
-    ],
-  },
-});
 
 describe('QueueTable: ', () => {
   beforeAll(() => {
@@ -83,7 +24,7 @@ describe('QueueTable: ', () => {
     expect(rows).toHaveLength(1); // should only have the header row
 
     const headerRow = rows[0];
-    const expectedColumnHeaders = [/name/i, /priority/i, /coming from/i, /status/i, /wait time/i];
+    const expectedColumnHeaders = [/name/i, /priority/i, /coming from/i, /status/i, /wait time/i, /actions/i];
     for (const expectedHeader of expectedColumnHeaders) {
       expect(within(headerRow).getByText(expectedHeader)).toBeInTheDocument();
     }
