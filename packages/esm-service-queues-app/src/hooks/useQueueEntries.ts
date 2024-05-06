68d989059e4c28c247fb2b8f72563ad739a71bf7
Vineet Sharma <sharmava05@gmail.com>
2024-05-06 13:05:26 +0530

    Fetching all the queue entries since fetching is happening on the frontend

--- packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
+++ packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
@@ -1,30 +1,61 @@
-import { openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
+import { type FetchResponse, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
 import { type QueueEntry, type QueueEntrySearchCriteria } from '../types';
-import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
+import { useEffect, useState } from 'react';
+
+type QueueEntryResponse = FetchResponse<{
+  results: Array<QueueEntry>;
+  links: Array<{
+    rel: 'prev' | 'next';
+    link: '';
+  }>;
+  totalCount: number;
+}>;
 
 const repString =
   'custom:(uuid,display,queue,status,patient,visit:(uuid,display,startDatetime,encounters:(uuid,display,diagnoses,encounterDatetime,encounterType,obs,encounterProviders,voided)),priority,priorityComment,sortWeight,startedAt,endedAt,locationWaitingFor,queueComingFrom,providerWaitingFor,previousQueueEntry)';
 
 export function useQueueEntries(searchCriteria?: QueueEntrySearchCriteria, rep: string = repString) {
-  const searchParam = new URLSearchParams();
-  for (let [key, value] of Object.entries(searchCriteria)) {
-    if (value != null) {
-      searchParam.append(key, value?.toString());
+  const [totalCount, setTotalCount] = useState<number>();
+  const chunkSize = 50;
+
+  const getUrl = (pageIndex: number, previousPageData: QueueEntryResponse) => {
+    if (pageIndex && !previousPageData?.data?.links?.some((link) => link.rel === 'next')) {
+      return null;
     }
-  }
 
-  searchParam.append('v', rep);
-  searchParam.append('totalCount', 'true');
+    const apiUrl = `${restBaseUrl}/queue-entry`;
+    const searchParam = new URLSearchParams();
+    searchParam.append('v', rep);
+    searchParam.append('totalCount', 'true');
 
-  const apiUrl = `${restBaseUrl}/queue-entry?` + searchParam.toString();
-  const { data, ...rest } = useSWR<{ data: { results: Array<QueueEntry>; totalCount: number } }, Error>(
-    apiUrl,
-    openmrsFetch,
-  );
+    for (let [key, value] of Object.entries(searchCriteria)) {
+      if (value != null) {
+        searchParam.append(key, value?.toString());
+      }
+    }
+
+    if (pageIndex) {
+      searchParam.append('startIndex', `${pageIndex * chunkSize}`);
+    }
+    return `${apiUrl}?${searchParam.toString()}`;
+  };
+
+  const { data, ...rest } = useSWRInfinite<QueueEntryResponse, Error>(getUrl, openmrsFetch, {
+    initialSize: totalCount ? Math.ceil(totalCount / chunkSize) : 1,
+  });
+
+  useEffect(() => {
+    if (data?.[0]?.data?.totalCount && data[0].data.totalCount !== totalCount) {
+      setTotalCount(data[0].data.totalCount);
+    }
+  }, [data?.[0], totalCount, setTotalCount]);
+
+  const allResults: QueueEntry[] = data ? [].concat(...data?.map((response) => response?.data?.results)) : [];
 
   return {
-    queueEntries: data?.data?.results, // note that results can be paginated, thus, result.length != totalCount
-    totalCount: data?.data?.totalCount,
+    queueEntries: allResults,
+    totalCount,
     ...rest,
   };
 }
