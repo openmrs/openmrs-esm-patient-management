8a9c8cb909f0841efc9c6f5837e60f39b0ea8f16
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-05-08 15:15:21 +0300

    fix: fix tests

--- packages/esm-appointments-app/src/appointments/details/appointment-details.test.tsx
+++ packages/esm-appointments-app/src/appointments/details/appointment-details.test.tsx
@@ -58,14 +58,25 @@ jest.mock('../../hooks/usePatientAppointmentHistory', () => ({
   }),
 }));
 
-jest.mock('../../hooks/usePatientDetails', () => ({
-  usePatientDetails: () => ({
-    patientDetails: {
-      dateOfBirth: '22-Mar-2020',
-    },
-  }),
-}));
-test('renders appointment details correctly', () => {
+jest.mock('@openmrs/esm-framework', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-framework');
+  return {
+    ...originalModule,
+    usePatient: jest.fn().mockImplementation((...args) => ({
+      patient: {
+        birthDate: '22-Mar-2020',
+        telecom: [
+          {
+            uuid: 'tel-uuid-1',
+            value: '0899129989932',
+          },
+        ],
+      },
+    })),
+  };
+});
+
+test('renders appointment details correctly', async () => {
   const { getByText } = render(<AppointmentDetails appointment={appointment} />);
   expect(getByText(/Patient name/i)).toBeInTheDocument();
   expect(getByText(/John Wilson/i)).toBeInTheDocument();
@@ -74,6 +85,10 @@ test('renders appointment details correctly', () => {
   expect(getByText(/Gender/i)).toBeInTheDocument();
   expect(getByText(/Male/i)).toBeInTheDocument();
   expect(getByText(/Date of birth/i)).toBeInTheDocument();
+  expect(getByText(/Date of birth/i)).toBeInTheDocument();
+  expect(getByText(/22-Mar-2020/i)).toBeInTheDocument();
+  expect(getByText(/Phone 1/i)).toBeInTheDocument();
+  expect(getByText(/0899129989932/i)).toBeInTheDocument();
   expect(getByText(/Appointment Notes/i)).toBeInTheDocument();
   expect(getByText(/Some comments/i)).toBeInTheDocument();
   expect(getByText(/Appointment History/i)).toBeInTheDocument();
