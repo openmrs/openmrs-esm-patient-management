7182a90590fc713b9d3d85d863ce9f5bbe566e27
Vineet Sharma <sharmava05@gmail.com>
2024-05-10 14:08:16 +0530

    Review changes

--- packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
+++ packages/esm-service-queues-app/src/hooks/useQueueEntries.ts
@@ -1,13 +1,13 @@
 import { type FetchResponse, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
 import { type QueueEntry, type QueueEntrySearchCriteria } from '../types';
 import useSWRInfinite from 'swr/infinite';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 type QueueEntryResponse = FetchResponse<{
   results: Array<QueueEntry>;
   links: Array<{
     rel: 'prev' | 'next';
-    link: '';
+    uri: string;
   }>;
   totalCount: number;
 }>;
@@ -17,33 +17,48 @@ const repString =
 
 export function useQueueEntries(searchCriteria?: QueueEntrySearchCriteria, rep: string = repString) {
   const [totalCount, setTotalCount] = useState<number>();
-  const chunkSize = 50;
 
-  const getUrl = (pageIndex: number, previousPageData: QueueEntryResponse) => {
-    if (pageIndex && !previousPageData?.data?.links?.some((link) => link.rel === 'next')) {
-      return null;
-    }
-
-    const apiUrl = `${restBaseUrl}/queue-entry`;
-    const searchParam = new URLSearchParams();
-    searchParam.append('v', rep);
-    searchParam.append('totalCount', 'true');
-
-    for (let [key, value] of Object.entries(searchCriteria)) {
-      if (value != null) {
-        searchParam.append(key, value?.toString());
+  const getUrl = useCallback(
+    (pageIndex: number, previousPageData: QueueEntryResponse) => {
+      if (pageIndex && !previousPageData?.data?.links?.some((link) => link.rel === 'next')) {
+        return null;
       }
-    }
 
-    if (pageIndex) {
-      searchParam.append('startIndex', `${pageIndex * chunkSize}`);
-    }
-    return `${apiUrl}?${searchParam.toString()}`;
-  };
+      if (previousPageData?.data?.links?.some((link) => link.rel === 'next')) {
+        const nextUrl = new URL(previousPageData.data.links.find((link) => link.rel === 'next')?.uri);
+        // default for production
+        if (nextUrl.origin === window.location.origin) {
+          return nextUrl;
+        }
 
-  const { data, ...rest } = useSWRInfinite<QueueEntryResponse, Error>(getUrl, openmrsFetch, {
-    initialSize: totalCount ? Math.ceil(totalCount / chunkSize) : 1,
-  });
+        // in development, the request should be funnelled through the local proxy
+        return new URL(`${nextUrl.pathname}${nextUrl.search ? nextUrl.search : ''}`, window.location.origin).toString();
+      }
+
+      const apiUrl = `${restBaseUrl}/queue-entry`;
+      const searchParam = new URLSearchParams();
+      searchParam.append('v', rep);
+      searchParam.append('totalCount', 'true');
+
+      for (let [key, value] of Object.entries(searchCriteria)) {
+        if (value != null) {
+          searchParam.append(key, value?.toString());
+        }
+      }
+
+      return `${apiUrl}?${searchParam.toString()}`;
+    },
+    [searchCriteria, rep],
+  );
+
+  const { data, size, setSize, ...rest } = useSWRInfinite<QueueEntryResponse, Error>(getUrl, openmrsFetch);
+
+  useEffect(() => {
+    if (data && data.length && data?.[data?.length - 1]?.data?.links?.some((link) => link.rel === 'next')) {
+      // Calling for the next set of data
+      setSize((prev) => prev + 1);
+    }
+  }, [data, setSize]);
 
   useEffect(() => {
     if (data?.[0]?.data?.totalCount && data[0].data.totalCount !== totalCount) {
