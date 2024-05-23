1b9eed4a44458bab3ecb68100a391e09b21d7840
Bhargav kodali <115476530+kb019@users.noreply.github.com>
2024-05-24 02:26:53 +0530

    (fix)- Replace overlays in the Appointments app (#1133)

--- packages/esm-appointments-app/src/appointments.component.tsx
+++ packages/esm-appointments-app/src/appointments.component.tsx
@@ -4,7 +4,7 @@ import dayjs from 'dayjs';
 import AppointmentTabs from './appointments/appointment-tabs.component';
 import AppointmentsHeader from './header/appointments-header.component';
 import AppointmentMetrics from './metrics/appointments-metrics.component';
-import Overlay from './overlay.component';
+import { WorkspaceOverlay } from '@openmrs/esm-framework';
 import { useParams } from 'react-router-dom';
 import SelectedDateContext from './hooks/selectedDateContext';
 import { omrsDateFormat } from './constants';
@@ -37,7 +37,7 @@ const Appointments: React.FC = () => {
       />
       <AppointmentMetrics appointmentServiceType={appointmentServiceType} />
       <AppointmentTabs appointmentServiceType={appointmentServiceType} />
-      <Overlay />
+      <WorkspaceOverlay contextKey="appointments" />
     </SelectedDateContext.Provider>
   );
 };
