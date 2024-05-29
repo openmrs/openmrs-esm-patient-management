8137fac24d3925fda05e92f64d0750d0a2ac5b96
Vineet Sharma <sharmava05@gmail.com>
2024-05-29 18:01:05 +0530

    Update UI as per ticket description

--- packages/esm-service-queues-app/src/queue-table/queue-table.component.tsx
+++ packages/esm-service-queues-app/src/queue-table/queue-table.component.tsx
@@ -22,6 +22,7 @@ import { useTranslation } from 'react-i18next';
 import { type QueueEntry, type QueueTableColumn } from '../types';
 import styles from './queue-table.scss';
 import { useColumns } from './cells/columns.resource';
+import classNames from 'classnames';
 
 interface QueueTableProps {
   queueEntries: QueueEntry[];
@@ -64,7 +65,7 @@ function QueueTable({
   const [currentPageSize, setPageSize] = useState(10);
   const pageSizes = [10, 20, 30, 40, 50];
 
-  const { goTo, results: paginatedQueueEntries, currentPage } = usePagination(queueEntries, currentPageSize);
+  const { goTo, results: paginatedQueueEntries, currentPage, paginated } = usePagination(queueEntries, currentPageSize);
   const layout = useLayoutType();
   const responsiveSize = isDesktop(layout) ? 'sm' : 'lg';
 
@@ -130,7 +131,13 @@ function QueueTable({
                     <React.Fragment key={row.id}>
                       <Row {...getRowProps({ row })}>
                         {row.cells.map((cell) => (
-                          <TableCell key={cell.id}>{cell.value}</TableCell>
+                          <TableCell
+                            key={cell.id}
+                            className={classNames({
+                              'cds--table-column-menu': cell?.id?.split(':')?.[1] === 'actions',
+                            })}>
+                            {cell.value}
+                          </TableCell>
                         ))}
                       </Row>
                       {ExpandedRow && row.isExpanded && (
@@ -154,22 +161,24 @@ function QueueTable({
               </Tile>
             </div>
           )}
-          <Pagination
-            forwardText={t('nextPage', 'Next page')}
-            backwardText={t('previousPage', 'Previous page')}
-            page={currentPage}
-            pageSize={currentPageSize}
-            pageSizes={pageSizes}
-            totalItems={queueEntries?.length}
-            onChange={({ pageSize, page }) => {
-              if (pageSize !== currentPageSize) {
-                setPageSize(pageSize);
-              }
-              if (page !== currentPage) {
-                goTo(page);
-              }
-            }}
-          />
+          {paginated && (
+            <Pagination
+              forwardText={t('nextPage', 'Next page')}
+              backwardText={t('previousPage', 'Previous page')}
+              page={currentPage}
+              pageSize={currentPageSize}
+              pageSizes={pageSizes}
+              totalItems={queueEntries?.length}
+              onChange={({ pageSize, page }) => {
+                if (pageSize !== currentPageSize) {
+                  setPageSize(pageSize);
+                }
+                if (page !== currentPage) {
+                  goTo(page);
+                }
+              }}
+            />
+          )}
         </>
       )}
     </DataTable>
