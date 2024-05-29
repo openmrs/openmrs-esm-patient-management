c7c1281c7dc014ba0d3407b53778a3037e6e4a4d
Brandon Istenes <bistenes@gmail.com>
2024-05-28 22:53:19 -0400

    (feat) O3-2724: Move service-queue overlays into the framework (#1103)

---
+++ packages/esm-service-queues-app/src/patient-search/patient-search.workspace.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { SearchTypes } from '../types';
+import PatientScheduledVisits from './patient-scheduled-visits.component';
+import VisitForm from './visit-form/visit-form.component';
+import {
+  type DefaultWorkspaceProps,
+  ExtensionSlot,
+  usePatient,
+  useVisit,
+  PatientBannerPatientInfo,
+  PatientPhoto,
+  PatientBannerToggleContactDetailsButton,
+  PatientBannerContactDetails,
+  displayName,
+} from '@openmrs/esm-framework';
+import ExistingVisitFormComponent from './visit-form/existing-visit-form.component';
+import styles from './patient-search.scss';
+
+interface PatientSearchProps extends DefaultWorkspaceProps {
+  viewState: {
+    selectedPatientUuid?: string;
+  };
+}
+
+const PatientSearch: React.FC<PatientSearchProps> = ({ closeWorkspace, viewState }) => {
+  const { selectedPatientUuid } = viewState;
+  const { patient } = usePatient(selectedPatientUuid);
+  const { activeVisit } = useVisit(selectedPatientUuid);
+  const [searchType, setSearchType] = useState<SearchTypes>(SearchTypes.SCHEDULED_VISITS);
+  const [newVisitMode, setNewVisitMode] = useState<boolean>(false);
+  const [showContactDetails, setContactDetails] = useState(false);
+
+  const toggleSearchType = (searchType: SearchTypes, mode: boolean = false) => {
+    setSearchType(searchType);
+    setNewVisitMode(mode);
+  };
+
+  const patientName = displayName(patient);
+  return patient ? (
+    <>
+      <div className={styles.patientBannerContainer}>
+        <div className={styles.patientBanner}>
+          <div className={styles.patientPhoto}>
+            <PatientPhoto patientUuid={patient.id} patientName={patientName} />
+          </div>
+          <PatientBannerPatientInfo patient={patient} />
+          <PatientBannerToggleContactDetailsButton
+            showContactDetails={showContactDetails}
+            toggleContactDetails={() => setContactDetails(!showContactDetails)}
+          />
+        </div>
+        {showContactDetails ? (
+          <PatientBannerContactDetails patientId={patient.id} deceased={patient.deceasedBoolean} />
+        ) : null}
+      </div>
+      <div>
+        {activeVisit ? (
+          <ExistingVisitFormComponent visit={activeVisit} closePanel={closeWorkspace} />
+        ) : searchType === SearchTypes.SCHEDULED_VISITS ? (
+          <PatientScheduledVisits
+            patientUuid={selectedPatientUuid}
+            toggleSearchType={toggleSearchType}
+            closeWorkspace={closeWorkspace}
+          />
+        ) : searchType === SearchTypes.VISIT_FORM ? (
+          <VisitForm
+            patientUuid={selectedPatientUuid}
+            toggleSearchType={toggleSearchType}
+            closePanel={closeWorkspace}
+            mode={newVisitMode}
+          />
+        ) : null}
+      </div>
+    </>
+  ) : null;
+};
+
+export default PatientSearch;
