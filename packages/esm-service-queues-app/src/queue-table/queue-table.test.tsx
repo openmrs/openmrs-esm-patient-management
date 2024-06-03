d7bed13dd512e91878db5a192cbb2bcf73b4c462
Brandon Istenes <bistenes@gmail.com>
2024-06-03 09:45:38 -0400

    Update packages/esm-service-queues-app/src/queue-table/queue-table.test.tsx
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-service-queues-app/src/queue-table/queue-table.test.tsx
+++ packages/esm-service-queues-app/src/queue-table/queue-table.test.tsx
@@ -222,7 +222,7 @@ describe('QueueTable', () => {
           },
         ],
       },
-    } as ConfigObject);
+    });
 
     renderWithSwr(<QueueTable queueEntries={mockQueueEntries} statusUuid={null} queueUuid={'triage-queue-uuid'} />);
 
