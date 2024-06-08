2a316beb7d235fc3394ff23fe0466f67cc7573ed
Chi Bong Ho <cbho@pih.org>
2024-06-07 20:27:14 -0400

    remove WIP admission workspace for purpose of PR

--- packages/esm-ward-app/src/ward-patient-card/bento-elements/ward-patient-admission-time.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { WardPatientCardBentoElement } from '../../types';
-import styles from '../admitted-patient-details.scss';
-
-const WardPatientAdmissionTime: WardPatientCardBentoElement = () => {
-  return <div className={styles.timeLapsed}>ADMITTED: 30 hours ago</div>;
-};
-
-export default WardPatientAdmissionTime;
--- packages/esm-ward-app/src/ward-patient-card/ward-patient-card.resources.ts
+++ packages/esm-ward-app/src/ward-patient-card/ward-patient-card.resources.ts
@@ -1,13 +1,17 @@
 import { useConfig } from '@openmrs/esm-framework';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { BentoElementDefinition, builtInBentoElements, defaultBentoElementConfig, type WardConfigObject } from '../config-schema';
+import {
+  type BentoElementDefinition,
+  builtInBentoElements,
+  defaultBentoElementConfig,
+  type WardConfigObject,
+} from '../config-schema';
 import type { WardPatientCardBentoElement } from '../types';
 import wardPatientAddress from './bento-elements/ward-patient-header-address';
 import WardPatientAge from './bento-elements/ward-patient-age';
 import WardPatientBedNumber from './bento-elements/ward-patient-bed-number';
 import WardPatientName from './bento-elements/ward-patient-name';
-import WardPatientAdmissionTime from './bento-elements/ward-patient-admission-time';
 
 export function useCardSlots(location: string) {
   const { t } = useTranslation();
@@ -17,10 +21,15 @@ export function useCardSlots(location: string) {
   // map of bentoElementId to its corresponding React component
   const bentoElementsMap = useMemo(() => {
     const map = new Map<string, WardPatientCardBentoElement>();
-    for(const elementType of builtInBentoElements) {
-      map.set(elementType, getBentoElementFromDefinition({
-        id: elementType, elementType, config: defaultBentoElementConfig
-      }));
+    for (const elementType of builtInBentoElements) {
+      map.set(
+        elementType,
+        getBentoElementFromDefinition({
+          id: elementType,
+          elementType,
+          config: defaultBentoElementConfig,
+        }),
+      );
     }
     for (const bentoElementDef of bentoElementDefinitions) {
       map.set(bentoElementDef.id, getBentoElementFromDefinition(bentoElementDef));
@@ -33,10 +42,7 @@ export function useCardSlots(location: string) {
       cardDefinitions.find((cardDef) => {
         const appliedTo = cardDef.appliedTo;
 
-        return (
-          appliedTo == null ||
-          appliedTo.some(criteria => criteria.location == location)
-        );
+        return appliedTo == null || appliedTo.some((criteria) => criteria.location == location);
       }),
     [wardPatientCards, location],
   );
@@ -49,8 +55,8 @@ export function useCardSlots(location: string) {
   return bentoElements;
 }
 
-function getBentoElementFromDefinition(bentoElementDef : BentoElementDefinition): WardPatientCardBentoElement {
-  const {elementType, config} = bentoElementDef;
+function getBentoElementFromDefinition(bentoElementDef: BentoElementDefinition): WardPatientCardBentoElement {
+  const { elementType, config } = bentoElementDef;
   switch (elementType) {
     case 'bed-number':
       return WardPatientBedNumber;
@@ -62,7 +68,5 @@ function getBentoElementFromDefinition(bentoElementDef : BentoElementDefinition)
       // TODO: configure address fields to pass in
       return wardPatientAddress(config);
     }
-    case 'admission-time':
-      return WardPatientAdmissionTime;
   }
 }
