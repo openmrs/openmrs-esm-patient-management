b40ef2eab51be9311ae9ca6e658c4e9e37984b72
Vineet Sharma <sharmava05@gmail.com>
2024-06-18 19:40:59 +0530

    Added config prop for defining status concept UUIDs

--- packages/esm-service-queues-app/src/config-schema.ts
+++ packages/esm-service-queues-app/src/config-schema.ts
@@ -3,6 +3,7 @@ import vitalsConfigSchema, { type VitalsConfigObject } from './current-visit/vis
 import biometricsConfigSchema, {
   type BiometricsConfigObject,
 } from './current-visit/visit-details/biometrics-config-schema';
+import _default from 'react-hook-form/dist/utils/createSubject';
 
 const columnTypes = [
   'actions',
@@ -134,12 +135,26 @@ export const configSchema = {
       _description: 'The Uuids of the obs that are displayed on the previous visit modal',
       _default: ['161643AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'],
     },
+    waitingQueueStatusConceptUuid: {
+      _type: Type.ConceptUuid,
+      _description: 'The UUID of the concept that represents Status = Waiting.',
+      _default: '167407AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    inServiceQueueStatusConceptUuid: {
+      _type: Type.ConceptUuid,
+      _description: 'The UUID of the concept that represents Status = In Service.',
+      _default: '167408AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    finishedServiceQueueStatusConceptUuid: {
+      _type: Type.ConceptUuid,
+      _description: 'The UUID of the concept that represents Status = Finished service.',
+      _default: 'b559fb77-4e1e-4285-b9b7-1d03e0ba983f',
+    },
   },
   contactAttributeType: {
     _type: Type.UUID,
     _description:
       'The UUID of the person attribute type that captures contact information such as `Next of kin contact details`',
-    _default: '',
   },
   vitals: vitalsConfigSchema,
   biometrics: biometricsConfigSchema,
@@ -411,6 +426,9 @@ export interface ConfigObject {
     respiratoryRateUuid: string;
     emergencyPriorityConceptUuid: string;
     historicalObsConceptUuid: Array<string>;
+    waitingQueueStatusConceptUuid: string;
+    inServiceQueueStatusConceptUuid: string;
+    finishedServiceQueueStatusConceptUuid: string;
   };
   contactAttributeType: Array<string>;
   vitals: VitalsConfigObject;
