47474883e0d0e8bf663a0d84e030e4cc6f7da270
pius <piuskariuki19@gmail.com>
2024-06-22 23:14:13 +0300

    (chore) listen to the Custom Event and fire a re-fetch by calling a "mutate"

--- packages/esm-service-queues-app/src/queue-table/default-queue-table.component.tsx
+++ packages/esm-service-queues-app/src/queue-table/default-queue-table.component.tsx
@@ -35,7 +35,7 @@ This is used in the main dashboard of the queues app. (Currently behind a featur
 function DefaultQueueTable() {
   const selectedQueueUuid = useSelectedServiceUuid();
   const currentLocationUuid = useSelectedQueueLocationUuid();
-  const { queueEntries, isLoading, error } = useQueueEntries({
+  const { queueEntries, isLoading, error, mutate } = useQueueEntries({
     queue: selectedQueueUuid,
     location: currentLocationUuid,
     isEnded: false,
@@ -82,6 +82,15 @@ function DefaultQueueTable() {
       });
     });
   }, [queueEntries, searchTerm]);
+
+  useEffect(() => {
+    const handlePatientAddedToQueue = () => mutate();
+
+    window.addEventListener('refetchQueues', handlePatientAddedToQueue);
+    return () => {
+      window.removeEventListener('refetchQueues', handlePatientAddedToQueue);
+    };
+  }, []);
 
   if (isLoading) {
     return <DataTableSkeleton role="progressbar" />;
