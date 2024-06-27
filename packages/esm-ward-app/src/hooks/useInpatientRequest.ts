f3af3e499c43379aa6d6a5a6de21cc32f8d89aa3
Vineet Sharma <sharmava05@gmail.com>
2024-06-27 17:48:53 +0530

    Use OpenmrsSWR for data fetching

--- packages/esm-ward-app/src/hooks/useInpatientRequest.ts
+++ packages/esm-ward-app/src/hooks/useInpatientRequest.ts
@@ -1,13 +1,14 @@
-import { openmrsFetch } from '@openmrs/esm-framework';
+import { type FetchResponse, openmrsFetch, restBaseUrl, useOpenmrsSWR } from '@openmrs/esm-framework';
 import useSWR from 'swr';
 import type { InpatientRequest } from '../types';
 
 export function useInpatientRequest(locationUuid: string) {
-  const apiUrl = `/ws/rest/emrapi/inpatient/admissionRequests?admissionLocation=${locationUuid}`;
-  const { data, ...rest } = useSWR<{ data: Array<InpatientRequest> }, Error>(apiUrl, openmrsFetch);
+  const { data, ...rest } = useSWR<FetchResponse<Array<InpatientRequest>>, Error>(
+    locationUuid ? `${restBaseUrl}/emrapi/inpatient/admissionRequests?admissionLocation=${locationUuid}` : null,
+  );
 
   return {
-    inpatientRequests: data?.data || null,
+    inpatientRequests: data?.data,
     ...rest,
   };
 }
