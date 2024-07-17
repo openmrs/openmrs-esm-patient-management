e4a4728e4bec08d1dfa292ac1d4cb8fe36f4342f
faith <faith@marascientific.org>
2024-07-17 23:27:02 +0300

    (fix)O3-3590: Unable to change Provider and appointment duration when editing appointment

--- packages/esm-appointments-app/src/form/appointments-form.component.tsx
+++ packages/esm-appointments-app/src/form/appointments-form.component.tsx
@@ -275,21 +275,23 @@ const AppointmentsForm: React.FC<AppointmentsFormProps> = ({
     const appointmentPayload = constructAppointmentPayload(data);
 
     // check if Duplicate Response Occurs
-    const response: FetchResponse = await checkAppointmentConflict(appointmentPayload);
-    let errorMessage = t('appointmentConflict', 'Appointment conflict');
-    if (response?.data?.hasOwnProperty('SERVICE_UNAVAILABLE')) {
-      errorMessage = t('serviceUnavailable', 'Appointment time is outside of service hours');
-    } else if (response?.data?.hasOwnProperty('PATIENT_DOUBLE_BOOKING')) {
-      errorMessage = t('patientDoubleBooking', 'Patient already booked for an appointment at this time');
-    }
-    if (response.status === 200) {
-      setIsSubmitting(false);
-      showSnackbar({
-        isLowContrast: true,
-        kind: 'error',
-        title: errorMessage,
-      });
-      return;
+    if (context !== 'editing') {
+      const response: FetchResponse = await checkAppointmentConflict(appointmentPayload);
+      let errorMessage = t('appointmentConflict', 'Appointment conflict');
+      if (response?.data?.hasOwnProperty('SERVICE_UNAVAILABLE')) {
+        errorMessage = t('serviceUnavailable', 'Appointment time is outside of service hours');
+      } else if (response?.data?.hasOwnProperty('PATIENT_DOUBLE_BOOKING')) {
+        errorMessage = t('patientDoubleBooking', 'Patient already booked for an appointment at this time');
+      }
+      if (response.status === 200) {
+        setIsSubmitting(false);
+        showSnackbar({
+          isLowContrast: true,
+          kind: 'error',
+          title: errorMessage,
+        });
+        return;
+      }
     }
 
     // Construct recurring pattern payload
