5de94c19c25ae7ef747d49c3d95706dba40e6e32
McCarthy <121826239+mccarthyaaron@users.noreply.github.com>
2024-07-17 19:42:00 +0300

    (fix) O3-3273: Scheduled appointments card always shows "Not arrived' even for past dates (#1150)

--- packages/esm-appointments-app/src/metrics/metrics-card.component.tsx
+++ packages/esm-appointments-app/src/metrics/metrics-card.component.tsx
@@ -1,41 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import dayjs from 'dayjs';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 dayjs.extend(isSameOrBefore);
 import isEmpty from 'lodash-es/isEmpty';
-import { ConfigurableLink } from '@openmrs/esm-framework';
-import { ArrowRight } from '@carbon/react/icons';
-import { basePath, spaHomePage } from '../constants';
 import styles from './metrics-card.scss';
+import SelectedDateContext from '../hooks/selectedDateContext';
 
 interface MetricsCardProps {
   label: string;
   value: number;
   headerLabel: string;
-  children?: React.ReactNode;
-  view?: string;
   count?: { pendingAppointments: Array<any>; arrivedAppointments: Array<any> };
-  appointmentDate?: string;
 }
 
-const MetricsCard: React.FC<MetricsCardProps> = ({
-  label,
-  value,
-  headerLabel,
-  children,
-  view,
-  count,
-  appointmentDate,
-}) => {
+const MetricsCard: React.FC<MetricsCardProps> = ({ label, value, headerLabel, count }) => {
   const { t } = useTranslation();
-  const isDateInPast = useMemo(() => !dayjs(appointmentDate).isBefore(dayjs(), 'date'), [appointmentDate]);
-
-  const metricsLink = {
-    patients: 'appointments-list/scheduled',
-    highVolume: 'appointments-list/high-volume-service',
-    providers: 'appointments-list/providers-link',
-  };
+  const { selectedDate } = useContext(SelectedDateContext);
+  const isSelectedDateInPast = useMemo(() => dayjs(selectedDate).isBefore(dayjs(), 'date'), [selectedDate]);
 
   return (
     <article className={styles.container}>
@@ -43,16 +25,7 @@ const MetricsCard: React.FC<MetricsCardProps> = ({
         <div className={styles.tileHeader}>
           <div className={styles.headerLabelContainer}>
             <label className={styles.headerLabel}>{headerLabel}</label>
-            {children}
           </div>
-          {view && (
-            <div className={styles.link}>
-              <ConfigurableLink className={styles.link} to={`${spaHomePage}${basePath}/${metricsLink[view]}`}>
-                <span style={{ fontSize: '0.825rem', marginRight: '0.325rem' }}>{t('view', 'View')}</span>{' '}
-                <ArrowRight size={16} className={styles.viewListBtn} />
-              </ConfigurableLink>
-            </div>
-          )}
         </div>
         <div className={styles.metricsGrid}>
           <div>
@@ -62,7 +35,7 @@ const MetricsCard: React.FC<MetricsCardProps> = ({
           {!isEmpty(count) && (
             <div className={styles.countGrid}>
               <span>{t('checkedIn', 'Checked in')}</span>
-              <span>{isDateInPast ? t('notArrived', 'Not arrived') : t('missed', 'Missed')}</span>
+              <span>{isSelectedDateInPast ? t('missed', 'Missed') : t('notArrived', 'Not arrived')}</span>
               <p style={{ color: '#319227' }}>{count.arrivedAppointments?.length}</p>
               <p style={{ color: '#da1e28' }}>{count.pendingAppointments?.length}</p>
             </div>
