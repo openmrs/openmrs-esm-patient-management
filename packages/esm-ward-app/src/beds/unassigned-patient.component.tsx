4f0596e1b4748a5c6561f9336cb8ee5dcf1cd6f4
Mark Goodrich <mgoodrich@pih.org>
2024-07-18 09:47:02 -0400

    O3-3125: Ward App - show patient cards for patients admitted but not assigned a bed

--- packages/esm-ward-app/src/beds/unassigned-patient.component.tsx
+++ packages/esm-ward-app/src/beds/unassigned-patient.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import WardPatientCard from '../ward-patient-card/ward-patient-card';
-import styles from './unassigned-bed.scss';
+import styles from './unassigned-patient.scss';
 import { type WardPatient } from '../types';
 
 export interface UnassignedPatientProps {
@@ -9,7 +9,7 @@ export interface UnassignedPatientProps {
 
 const UnassignedPatient: React.FC<UnassignedPatientProps> = ({ wardPatient }) => {
   return (
-    <div className={styles.unassignedBed}>
+    <div className={styles.unassignedPatient}>
       <div key={'unassigned-bed-pt-' + wardPatient.patient.uuid}>
         <WardPatientCard patient={wardPatient.patient} visit={wardPatient.visit} />
       </div>
