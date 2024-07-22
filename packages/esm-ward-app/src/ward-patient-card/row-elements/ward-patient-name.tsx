4f7713b191ab392e0f542b3cb727b8ddfb343136
Brandon Istenes <bistenes@gmail.com>
2024-07-22 18:58:32 -0400

    Update packages/esm-ward-app/src/ward-patient-card/row-elements/ward-patient-name.tsx

--- packages/esm-ward-app/src/ward-patient-card/row-elements/ward-patient-name.tsx
+++ packages/esm-ward-app/src/ward-patient-card/row-elements/ward-patient-name.tsx
@@ -6,7 +6,7 @@ const WardPatientName: WardPatientCardElement = ({ patient }) => {
   // TODO: BED-10
   // TODO: this is a hack to support both the patient name rep returned by the bed endpoint (given and family) and the display used by the emr api; this should be fixed once the emr api endpoint is updated
   const { givenName, familyName, display } = patient?.person?.preferredName || {};
-  const name = display ? `${display}` : `${givenName} ${familyName}`;
+  const name = display ?? `${givenName} ${familyName}`;
   return <div className={styles.wardPatientName}>{name}</div>;
 };
 
