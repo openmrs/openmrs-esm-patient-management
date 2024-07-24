9085e0eb30a3dbd338f6442c3133232d312f6e71
Brandon Istenes <bistenes@gmail.com>
2024-07-24 11:05:56 -0400

    Review feedback

--- packages/esm-ward-app/src/ward-workspace/admission-requests-workspace.component.tsx
+++ packages/esm-ward-app/src/ward-workspace/admission-requests-workspace.component.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './admission-requests-workspace.scss';
 import AdmissionRequestCard from './admission-request-card.component';
-import { type DefaultWorkspaceProps } from '@openmrs/esm-framework';
+import { getCoreTranslation, type DefaultWorkspaceProps } from '@openmrs/esm-framework';
 import { useInpatientRequest } from '../hooks/useInpatientRequest';
 import useWardLocation from '../hooks/useWardLocation';
 import { Loading } from '@carbon/react';
 import { InlineNotification } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
 
 const AdmissionRequestsWorkspace: React.FC<DefaultWorkspaceProps> = () => {
+  const { t } = useTranslation();
   const { location } = useWardLocation();
   const { inpatientRequests, isLoading, error } = useInpatientRequest(location?.uuid);
   const admissionRequests = inpatientRequests?.filter((request) => request.dispositionType == 'ADMIT');
@@ -17,7 +19,13 @@ const AdmissionRequestsWorkspace: React.FC<DefaultWorkspaceProps> = () => {
   }
 
   if (error) {
-    return <InlineNotification kind="error" title="Error Loading patient admission requests" />;
+    return (
+      <InlineNotification
+        kind="error"
+        title={t('errorLoadingPatientAdmissionRequests', 'Error Loading patient admission requests')}>
+        {getCoreTranslation('errorCopy')}
+      </InlineNotification>
+    );
   }
 
   return (
