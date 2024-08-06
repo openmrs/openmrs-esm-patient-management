b047161669940fa2b2205df4080cfb33ad1a0acd
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-08-06 14:58:53 +0300

    feat: add the name and bed number to the patient-banner on in-patient workspace

--- packages/esm-ward-app/src/ward-workspace/patient-banner/patient-banner.component.tsx
+++ packages/esm-ward-app/src/ward-workspace/patient-banner/patient-banner.component.tsx
@@ -3,16 +3,23 @@ import type { WardPatient } from '../../types';
 import { WardPatientCardElement } from '../../ward-patient-card/ward-patient-card-element.component';
 import { useCurrentWardCardConfig } from '../../hooks/useCurrentWardCardConfig';
 import styles from './style.scss';
+import WardPatientBedNumber from '../../ward-patient-card/row-elements/ward-patient-bed-number';
+import WardPatientName from '../../ward-patient-card/row-elements/ward-patient-name';
 
 const WardPatientWorkspaceBanner = (props: WardPatient) => {
   const { headerRowElements } = useCurrentWardCardConfig();
   const { patient, bed, visit, firstAdmissionOrTransferEncounter, encounterAssigningToCurrentInpatientLocation } =
     props;
 
-  if (!(patient && bed && visit)) return null;
+  if (!(patient && visit)) {
+    console.warn('Patient details and visit details where not received by the workspace');
+    return null;
+  }
 
   return (
     <div className={styles.patientBanner}>
+      {bed ? <WardPatientBedNumber bed={bed} /> : null}
+      <WardPatientName patient={patient} />
       {headerRowElements.map((elementId, i) => (
         <WardPatientCardElement
           key={`ward-card-${patient.uuid}-header-${i}`}
