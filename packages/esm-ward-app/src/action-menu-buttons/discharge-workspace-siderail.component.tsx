f34a67b5fb380e26e0fb96eb111068becafc5849
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-08-19 13:58:42 +0300

    fix: siderail button type

--- packages/esm-ward-app/src/action-menu-buttons/discharge-workspace-siderail.component.tsx
+++ packages/esm-ward-app/src/action-menu-buttons/discharge-workspace-siderail.component.tsx
@@ -1,22 +1,20 @@
-import { ActionMenuButton, type DefaultWorkspaceProps, launchWorkspace } from '@openmrs/esm-framework';
+import { ActionMenuButton, launchWorkspace } from '@openmrs/esm-framework';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Exit } from '@carbon/react/icons';
 
-interface PatientTransferAndSwapSiderailIconProps extends DefaultWorkspaceProps {}
-
-export default function PatientTransferAndSwapSiderailIcon(additionalProps: PatientTransferAndSwapSiderailIconProps) {
+export default function PatientDischargeSideRailIcon() {
   const { t } = useTranslation();
   const handler = () => {
-    launchWorkspace('patient-transfer-swap-workspace');
+    launchWorkspace('patient-discharge-workspace');
   };
   return (
     <ActionMenuButton
       getIcon={(props) => <Exit {...props} />}
-      label={t('transfers', 'Transfers')}
-      iconDescription={t('transfers', 'Transfers')}
+      label={t('discharge', 'Discharge')}
+      iconDescription={t('discharge', 'Discharge')}
       handler={handler}
-      type="transfer-swap-bed-form"
+      type="patient-discharge-workspace"
     />
   );
 }
--- packages/esm-ward-app/src/ward-workspace/patient-discharge/patient-discharge.workspace.tsx
+++ packages/esm-ward-app/src/ward-workspace/patient-discharge/patient-discharge.workspace.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { ExtensionSlot, showSnackbar, useFeatureFlag, useSession } from '@openmrs/esm-framework';
-import { Button, ButtonSet, ContentSwitcher, InlineNotification, Switch } from '@carbon/react';
+import { Button, ButtonSet, InlineNotification } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import styles from './patient-discharge.scss';
 import WardPatientWorkspaceBanner from '../patient-banner/patient-banner.component';
@@ -22,7 +22,6 @@ type DischargeTypeValues = (typeof DischargeTypes)[keyof typeof DischargeTypes];
 export default function PatientDischargeWorkspace(props: WardPatientWorkspaceProps) {
   const { wardPatient, closeWorkspaceWithSavedChanges } = props;
   const { t } = useTranslation();
-  const [selectedSection, setSelectedSection] = useState<DischargeTypeValues>(DischargeTypes.MEDICAL);
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const isBedManagementModuleInstalled = useFeatureFlag('bedmanagement-module');
   const { currentProvider } = useSession();
@@ -30,12 +29,6 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
   const { emrConfiguration, isLoadingEmrConfiguration, errorFetchingEmrConfiguration } = useEmrConfiguration();
   const { mutate: mutateAdmissionLocation } = useAdmissionLocation();
   const { mutate: mutateInpatientRequest } = useInpatientRequest();
-  const dispositionsWithTypeDischarge = useMemo(
-    () => emrConfiguration?.dispositions.filter(({ type }) => type === 'DISCHARGE'),
-    [emrConfiguration],
-  );
-
-  // console.log(dispositionsWithTypeDischarge);
 
   const submitDischarge = useCallback(() => {
     setIsSubmitting(true);
@@ -48,7 +41,7 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
 
     createEncounter({
       patient: props?.wardPatient?.patient?.uuid,
-      encounterType: emrConfiguration.visitNoteEncounterType.uuid,
+      encounterType: emrConfiguration.dischargeForm.uuid,
       location: location.uuid,
       encounterProviders: [
         {
@@ -56,12 +49,7 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
           provider: currentProvider?.uuid,
         },
       ],
-      obs: [
-        {
-          concept: emrConfiguration.dischargeForm.uuid,
-          groupMembers: obs,
-        },
-      ],
+      obs: [],
     })
       .then(() => {
         showSnackbar({
@@ -94,18 +82,6 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
       <div className={styles.patientWorkspaceBanner}>
         <WardPatientWorkspaceBanner {...props?.wardPatient} />
       </div>
-      {isBedManagementModuleInstalled && (
-        <div>
-          <h2 className={styles.productiveHeading02}>{t('typeOfDischarge', 'Type of discharge')}</h2>
-          <div className={styles.contentSwitcher}>
-            <ContentSwitcher onChange={({ name }) => setSelectedSection(name)}>
-              <Switch name={DischargeTypes.MEDICAL} text={t('medical', 'Medical')} />
-              <Switch name={DischargeTypes.AGAINST_ADVICE} text={t('againstAdvice', 'Against Advice')} />
-              <Switch name={DischargeTypes.ABSCONDED} text={t('absconded', 'Absconded')} />
-            </ContentSwitcher>
-          </div>
-        </div>
-      )}
       <div className={styles.workspaceForm}>
         <div>
           {errorFetchingEmrConfiguration && (
@@ -124,16 +100,16 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
           )}
         </div>
         <ExtensionSlot name="ward-patient-discharge-slot" />
+        <ButtonSet className={styles.buttonSet}>
+          <Button
+            type="submit"
+            size="xl"
+            disabled={isLoadingEmrConfiguration || isSubmitting || errorFetchingEmrConfiguration}
+            onClick={submitDischarge}>
+            {t('proceedWithPatientDischarge', 'Proceed with patient discharge')}
+          </Button>
+        </ButtonSet>
       </div>
-      <ButtonSet className={styles.buttonSet}>
-        <Button
-          type="submit"
-          size="xl"
-          disabled={isLoadingEmrConfiguration || isSubmitting || errorFetchingEmrConfiguration}
-          onClick={submitDischarge}>
-          {t('proceedWithPatientDischarge', 'Proceed with patient discharge')}
-        </Button>
-      </ButtonSet>
     </div>
   );
 }
