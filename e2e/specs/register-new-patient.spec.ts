87de5288ae83ae58574405b9171bd29ffcb14c45
Dennis Kigen <kigen.work@gmail.com>
2024-08-20 20:29:06 +0300

    (test) Assert presence of patient details in patient banner (#1286)
    * (test) Assert presence of patient details in patient banner
    
    * Wait for patient banner to render before running assertions
    
    * Add a check for a success toast notification
    
    * Use patient banner aria role instead
    
    * Set a custom timeout
    
    * Increase timeout
    
    * Add patient-banner-app to the E2E environment
    
    ---------
    
    Co-authored-by: Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>

--- e2e/specs/register-new-patient.spec.ts
+++ e2e/specs/register-new-patient.spec.ts
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs';
 import { expect } from '@playwright/test';
 import { test } from '../core';
 import { type PatientRegistrationFormValues, RegistrationAndEditPage } from '../pages';
@@ -6,7 +5,7 @@ import { deletePatient, getPatient } from '../commands';
 
 let patientUuid: string;
 
-test('Register a new patient', async ({ page, api }) => {
+test('Register a new patient', async ({ page }) => {
   test.setTimeout(5 * 60 * 1000);
   const patientRegistrationPage = new RegistrationAndEditPage(page);
 
@@ -31,29 +30,49 @@ test('Register a new patient', async ({ page, api }) => {
     await patientRegistrationPage.waitUntilTheFormIsLoaded();
   });
 
-  await test.step('And then I click on fill new values into the registration form and then click the `Submit` button', async () => {
+  await test.step('And then I fill the registration form with the data in `formValues` and then click the `Submit` button', async () => {
     await patientRegistrationPage.fillPatientRegistrationForm(formValues);
   });
 
-  await test.step("Then I should be redirected to the new patient's chart page and a new patient record should be created from the information captured in the form", async () => {
-    await expect(page).toHaveURL(new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$'));
-    const patientUuid = /patient\/(.+)\/chart/.exec(page.url())?.[1] ?? null;
-    expect(patientUuid).not.toBeNull();
+  await test.step('Then I should see a success notification', async () => {
+    await expect(page.getByText(/new patient created/i)).toBeVisible();
+  });
 
-    const patient = await getPatient(api, patientUuid);
-    const { person } = patient;
-    const { givenName, middleName, familyName, sex } = formValues;
+  await test.step("And I should be redirected to the new patient's chart page", async () => {
+    const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
+    await page.waitForURL(patientChartUrlRegex);
+    await expect(page).toHaveURL(patientChartUrlRegex);
+  });
 
-    expect(person.display).toBe(`${givenName} ${middleName} ${familyName}`);
-    expect(person.gender).toMatch(new RegExp(sex[0], 'i'));
-    expect(dayjs(person.birthdate).format('DD/MM/YYYY')).toBe(
-      `${formValues.birthdate.day}/${formValues.birthdate.month}/${formValues.birthdate.year}`,
-    );
-    expect(person.preferredAddress.address1).toBe(formValues.address1);
-    expect(person.preferredAddress.cityVillage).toBe(formValues.cityVillage);
-    expect(person.preferredAddress.stateProvince).toBe(formValues.stateProvince);
-    expect(person.preferredAddress.country).toBe(formValues.country);
-    expect(person.attributes[0].display).toBe(formValues.phone);
+  await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
+    const patientBanner = page.locator('header[aria-label="patient banner"]');
+
+    await expect(patientBanner).toBeVisible();
+    await expect(patientBanner.getByText('Johnny Donny Ronny')).toBeVisible();
+    await expect(patientBanner.getByText(/male/i)).toBeVisible();
+    await expect(patientBanner.getByText(/4 yrs, 6 mths/i)).toBeVisible();
+    await expect(patientBanner.getByText(/01 — Feb — 2020/i)).toBeVisible();
+    await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
+  });
+
+  await test.step('And when I click the `Show details` button in the patient banner', async () => {
+    await page
+      .getByLabel('patient banner')
+      .getByRole('button', { name: /show details/i })
+      .click();
+  });
+
+  await test.step("Then I should see the patient's address and contact details displayed in the patient banner", async () => {
+    const patientBanner = page.locator('header[aria-label="patient banner"]');
+
+    await expect(patientBanner.getByRole('button', { name: /hide details/i })).toBeVisible();
+    await expect(patientBanner.getByText(/^address$/i)).toBeVisible();
+    await expect(patientBanner.getByText(/address line 1: Bom Jesus Street/i)).toBeVisible();
+    await expect(patientBanner.getByText(/city: Recife/i)).toBeVisible();
+    await expect(patientBanner.getByText(/state: Pernambuco/i)).toBeVisible();
+    await expect(patientBanner.getByText(/country: Brazil/i)).toBeVisible();
+    await expect(patientBanner.getByText(/contact details/i)).toBeVisible();
+    await expect(patientBanner.getByText(/telephone number: 5555551234/i)).toBeVisible();
   });
 });
 
@@ -65,11 +84,11 @@ test('Register an unknown patient', async ({ api, page }) => {
     await patientRegistrationPage.waitUntilTheFormIsLoaded();
   });
 
-  await test.step(`And I click on the unknown 'patient's name' tab`, async () => {
+  await test.step("And I click the `No` tab in the `Patient's Name is Known?` field", async () => {
     await page.getByRole('tab', { name: /no/i }).first().click();
   });
 
-  await test.step('And I select `female` as the patient gender', async () => {
+  await test.step('And then I set the gender to `Female`', async () => {
     await page
       .locator('label')
       .filter({ hasText: /female/i })
@@ -78,11 +97,11 @@ test('Register an unknown patient', async ({ api, page }) => {
       .click();
   });
 
-  await test.step('And I click on the unknown `Date of Birth` tab', async () => {
+  await test.step('And then I click on the `No` tab in the "Date of Birth Known" field', async () => {
     await page.getByRole('tab', { name: /no/i }).nth(1).click();
   });
 
-  await test.step('And I fill the field for estimated age in years', async () => {
+  await test.step('And then I fill in 25 as the estimated age in years', async () => {
     const estimatedAgeField = await page.getByLabel(/estimated age in years/i);
     await estimatedAgeField.clear();
     await estimatedAgeField.fill('25');
@@ -92,13 +111,25 @@ test('Register an unknown patient', async ({ api, page }) => {
     await page.getByRole('button', { name: /register patient/i }).click();
   });
 
-  await test.step('Then I should see a success toast notification', async () => {
+  await test.step('Then I should see a success notification', async () => {
     await expect(page.getByText(/new patient created/i)).toBeVisible();
   });
 
-  await test.step('And I should see the newly recorded unknown patient dispalyed on the dashboard', async () => {
-    const patient = await getPatient(api, /patient\/(.+)\/chart/.exec(page.url())?.[1]);
-    expect(patient?.person?.display).toBe('UNKNOWN UNKNOWN');
+  await test.step("And I should be redirected to the new patient's chart page", async () => {
+    const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
+    await page.waitForURL(patientChartUrlRegex);
+    await expect(page).toHaveURL(patientChartUrlRegex);
+  });
+
+  await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
+    const patientBanner = page.locator('header[aria-label="patient banner"]');
+
+    expect(patientBanner).toBeVisible();
+    await expect(patientBanner.getByText('Unknown Unknown')).toBeVisible();
+    await expect(patientBanner.getByText(/female/i)).toBeVisible();
+    await expect(patientBanner.getByText(/25 yrs/i)).toBeVisible();
+    await expect(patientBanner.getByText(/01 — Jan — 1999/i)).toBeVisible();
+    await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
   });
 });
 
