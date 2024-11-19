602ce79becac2c9916ad13996d1917aba542f942
McCarthy <mccarthymutebi10@gmail.com>
2024-11-19 16:47:35 +0300

    feat: use PageHeader component instead of custom header component

--- packages/esm-bed-management-app/src/header/header.component.tsx
+++ packages/esm-bed-management-app/src/header/header.component.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Calendar, Location } from '@carbon/react/icons';
-import { ConfigurableLink, formatDate, useSession } from '@openmrs/esm-framework';
-import Illustration from './illustration.component';
+import {
+  ConfigurableLink,
+  formatDate,
+  InPatientPictogram,
+  PageHeader,
+  PageHeaderContent,
+  useSession,
+} from '@openmrs/esm-framework';
 import styles from './header.scss';
 
 type HeaderProps = {
@@ -16,16 +22,16 @@ const Header: React.FC<HeaderProps> = ({ route, headerTitle }) => {
   const userLocation = userSession?.sessionLocation?.display;
 
   return (
-    <div className={styles.header}>
-      <div className={styles.leftJustifiedItems}>
-        <ConfigurableLink to={`${window.getOpenmrsSpaBase()}bed-management`}>
-          <Illustration />
-        </ConfigurableLink>
-        <div className={styles.pageLabels}>
-          <p>{t(headerTitle ?? 'bedManagement', headerTitle ?? 'Bed management')}</p>
-          <p className={styles.pageName}>{route}</p>
-        </div>
-      </div>
+    <PageHeader className={styles.header}>
+      <PageHeaderContent
+        illustration={
+          <ConfigurableLink to={`${window.getOpenmrsSpaBase()}bed-management`}>
+            <InPatientPictogram className={styles.inPatientPictogram} />
+          </ConfigurableLink>
+        }
+        title={route}
+        className={styles.leftJustifiedItems}
+      />
       <div className={styles.rightJustifiedItems}>
         <div className={styles.dateAndLocation}>
           <Location size={16} />
@@ -35,7 +41,7 @@ const Header: React.FC<HeaderProps> = ({ route, headerTitle }) => {
           <span className={styles.value}>{formatDate(new Date(), { mode: 'standard' })}</span>
         </div>
       </div>
-    </div>
+    </PageHeader>
   );
 };
 
