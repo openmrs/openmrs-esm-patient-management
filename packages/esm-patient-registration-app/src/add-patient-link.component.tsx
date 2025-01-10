891f7d2963e7807930b5f68939cc67253adfc150
Usama Idriss Kakumba <usamakakumba@gmail.com>
2025-01-10 14:39:35 +0300

    feat: add transaltion for Add Patient

--- packages/esm-patient-registration-app/src/add-patient-link.component.tsx
+++ packages/esm-patient-registration-app/src/add-patient-link.component.tsx
@@ -3,14 +3,16 @@ import { HeaderGlobalAction } from '@carbon/react';
 import { UserFollow } from '@carbon/react/icons';
 import { navigate } from '@openmrs/esm-framework';
 import styles from './add-patient-link.scss';
+import { useTranslation } from 'react-i18next';
 
 export default function Root() {
+  const { t } = useTranslation();
   const addPatient = React.useCallback(() => navigate({ to: '${openmrsSpaBase}/patient-registration' }), []);
 
   return (
     <HeaderGlobalAction
-      aria-label="Add Patient"
-      aria-labelledby="Add Patient"
+      aria-label={t('addPatient', 'Add Patient')}
+      aria-labelledby={t('addPatient', 'Add Patient')}
       enterDelayMs={500}
       name="AddPatientIcon"
       onClick={addPatient}
