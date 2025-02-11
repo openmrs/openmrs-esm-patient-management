8469bf160078ce02eb40b323f7081420c609e239
Samuel Muli <samuelmuli95@gmail.com>
2025-02-11 09:28:26 +0300

    Update test suite

--- packages/esm-appointments-app/src/appointments.test.tsx
+++ packages/esm-appointments-app/src/appointments.test.tsx
@@ -11,7 +11,7 @@ describe('Appointments', () => {
 
     expect(screen.getByRole('button', { name: /appointments calendar/i })).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/dd-mmm-yyyy/i)).toBeInTheDocument();
-    expect(screen.getByText(/filter by service type/i)).toBeInTheDocument();
+    expect(screen.getByText(/filter appointments by service type/i)).toBeInTheDocument();
     expect(screen.getByText(/appointment metrics/i)).toBeInTheDocument();
     expect(screen.getByText(/scheduled appointments/i)).toBeInTheDocument();
     expect(screen.getByText(/highest volume service/i)).toBeInTheDocument();
