0d41b338f001de967d9f772b0ec3841c341d5440
its-kios09 <legacyitsolution@gmail.com>
2025-03-11 01:18:26 +0300

    (refactor) Addressed the PR suggestion

--- packages/esm-service-queues-app/src/transition-latest-queue-entry/transition-latest-queue-entry.component.tsx
+++ packages/esm-service-queues-app/src/transition-latest-queue-entry/transition-latest-queue-entry.component.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLatestQueueEntry } from './transition-latest-queue-entry.resource';
 import TransitionQueueEntryModal from '../queue-table/queue-entry-actions/transition-queue-entry.modal';
-import { showToast, showSnackbar } from '@openmrs/esm-framework';
-import { InlineLoading } from '@carbon/react';
-import AddPatientToQueueEntry from './add-patient-to-queue-entry/add-patient-to-queue-entry.component';
+import { useLatestQueueEntry } from './transition-latest-queue-entry.resource';
+import AddPatientToQueueModal from './add-patient-to-queue-modal/add-patient-to-queue-entry.modal';
 
 interface TransitionLatestQueueEntryProps {
   patientUuid: string;
   closeModal: () => void;
-  modalTitle?: string;
 }
 
-const TransitionLatestQueueEntry: React.FC<TransitionLatestQueueEntryProps> = ({
-  closeModal,
-  patientUuid,
-  modalTitle,
-}) => {
+const TransitionLatestQueueEntry: React.FC<TransitionLatestQueueEntryProps> = ({ closeModal, patientUuid }) => {
   const { t } = useTranslation();
   const { data: queueEntry } = useLatestQueueEntry(patientUuid);
 
@@ -26,11 +19,11 @@ const TransitionLatestQueueEntry: React.FC<TransitionLatestQueueEntryProps> = ({
         <TransitionQueueEntryModal
           queueEntry={queueEntry}
           closeModal={closeModal}
-          modalTitle={t('TransitionLatestQueueEntry', "Transition patient's latest queue")}
+          modalTitle={t('transitionLatestQueueEntryTitle', "Transition patient's latest queue entry")}
         />
       ) : (
-        <AddPatientToQueueEntry
-          modalTitle={t('addPatientToNewQueue', 'Add patient to new queue')}
+        <AddPatientToQueueModal
+          modalTitle={t('addPatientToQueueTitle', 'Add patient to queue')}
           patientUuid={patientUuid}
           closeModal={closeModal}
         />
