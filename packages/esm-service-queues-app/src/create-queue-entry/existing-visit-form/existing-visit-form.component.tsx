eff0747bcbd743f7cf3f5add1fe30dad52dae42a
its-kios09 <legacyitsolution@gmail.com>
2025-04-03 04:30:42 +0300

    (fix) fixed the build issue

--- packages/esm-service-queues-app/src/create-queue-entry/existing-visit-form/existing-visit-form.component.tsx
+++ packages/esm-service-queues-app/src/create-queue-entry/existing-visit-form/existing-visit-form.component.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import { Button, ButtonSet, Form, Row } from '@carbon/react';
 import { ExtensionSlot, useLayoutType, type Visit } from '@openmrs/esm-framework';
 import { useMutateQueueEntries } from '../../hooks/useQueueEntries';
-import { type QueueEntry } from '../../types';
 import QueueFields from '../queue-fields/queue-fields.component';
 import styles from './existing-visit-form.scss';
 
@@ -25,8 +24,8 @@ const ExistingVisitForm: React.FC<ExistingVisitFormProps> = ({ visit, closeWorks
 
   const { mutateQueueEntries } = useMutateQueueEntries();
   const [callback, setCallback] = useState<{
-    submitQueueEntry: (visit: Visit) => Promise<QueueEntry>;
-  }>(null);
+    submitQueueEntry: (visit: Visit) => Promise<unknown>;
+  } | null>(null);
 
   const handleCloseWorkspace = useCallback(() => {
     if (handleReturnToSearchList) {
