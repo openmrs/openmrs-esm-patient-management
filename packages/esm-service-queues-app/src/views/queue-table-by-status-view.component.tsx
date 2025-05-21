1971babee34f34a8e9f8c892fa643db7604ec2ae
Chi Bong Ho <cbho@pih.org>
2025-05-21 10:22:53 -0400

    move useParam() out of <QueueTableByStatus>

--- packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
+++ packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { useQueue } from '../hooks/useQueue';
 import QueueTablesForAllStatuses from './queue-tables-for-all-statuses.component';
 
 interface QueueTableByStatusViewProps {
-  /**
-   * If provided, this will be used to fetch the queue instead of the one in the URL.
-   * This is useful for when this component is used as an extension.
-   */
-  customQueueUuid?: string;
+  queueUuid: string;
 }
 
-const QueueTableByStatusView: React.FC<QueueTableByStatusViewProps> = ({ customQueueUuid }) => {
-  const { queueUuid } = useParams();
-  const { queue, isLoading: isLoadingQueue, error } = useQueue(customQueueUuid ?? queueUuid);
+/**
+ * This component renders the several tables, one for each status, for a given queue.
+ */
+const QueueTableByStatusView: React.FC<QueueTableByStatusViewProps> = ({ queueUuid }) => {
+  const { queue, isLoading: isLoadingQueue, error } = useQueue(queueUuid);
 
   return <QueueTablesForAllStatuses selectedQueue={queue} isLoadingQueue={isLoadingQueue} errorFetchingQueue={error} />;
 };
