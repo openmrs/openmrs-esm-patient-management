143f2ab0ec59adba5a6abb09df65633505efb304
Chi Bong Ho <cbho@pih.org>
2025-05-21 09:01:28 -0400

    (feat) O3-4734 Make Ward / Queues / Appointments apps available as extensions

--- packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
+++ packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
@@ -3,9 +3,17 @@ import { useParams } from 'react-router-dom';
 import { useQueue } from '../hooks/useQueue';
 import QueueTablesForAllStatuses from './queue-tables-for-all-statuses.component';
 
-const QueueTableByStatusView: React.FC = () => {
+interface QueueTableByStatusViewProps {
+  /**
+   * If provided, this will be used to fetch the queue instead of the one in the URL.
+   * This is useful for when this component is used as an extension.
+   */
+  customQueueUuid?: string;
+}
+
+const QueueTableByStatusView: React.FC<QueueTableByStatusViewProps> = ({ customQueueUuid }) => {
   const { queueUuid } = useParams();
-  const { queue, isLoading: isLoadingQueue, error } = useQueue(queueUuid);
+  const { queue, isLoading: isLoadingQueue, error } = useQueue(customQueueUuid ?? queueUuid);
 
   return <QueueTablesForAllStatuses selectedQueue={queue} isLoadingQueue={isLoadingQueue} errorFetchingQueue={error} />;
 };
