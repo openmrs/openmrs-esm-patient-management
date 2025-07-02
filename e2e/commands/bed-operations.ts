d6b59271578370e8ec4cd5ff81f4b46525804860
gitcliff <gitacliff48@gmail.com>
2025-07-02 11:17:27 +0300

    fixing CI error

--- e2e/commands/bed-operations.ts
+++ e2e/commands/bed-operations.ts
@@ -1,13 +1,13 @@
 import { type APIRequestContext, expect } from '@playwright/test';
 import { type BedType, type Bed } from '../types';
 
-export const generateRandomBed = async (api: APIRequestContext): Promise<Bed> => {
+export const generateRandomBed = async (api: APIRequestContext, bedType: BedType): Promise<Bed> => {
   const randomString = Math.random().toString(36).substring(2, 6).toUpperCase();
   const bedNumber = `B${randomString}${Math.floor(Math.random() * 100)}`;
   const bedRes = await api.post('/openmrs/ws/rest/v1/bed', {
     data: {
       bedNumber: bedNumber.substring(0, 10),
-      bedType: 'Duplix',
+      bedType: bedType.name,
       status: 'AVAILABLE',
       row: Math.floor(Math.random() * 10) + 1,
       column: Math.floor(Math.random() * 10) + 1,
