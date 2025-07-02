08d717837ddc3b9914aba43ede38679805e00abd
gitcliff <gitacliff48@gmail.com>
2025-07-02 09:28:01 +0300

    fixing tests formats and clear bed , bedtype

--- e2e/commands/bed-operations.ts
+++ e2e/commands/bed-operations.ts
@@ -2,13 +2,15 @@ import { type APIRequestContext, expect } from '@playwright/test';
 import { type BedType, type Bed } from '../types';
 
 export const generateRandomBed = async (api: APIRequestContext): Promise<Bed> => {
+  const randomString = Math.random().toString(36).substring(2, 6).toUpperCase();
+  const bedNumber = `B${randomString}${Math.floor(Math.random() * 100)}`;
   const bedRes = await api.post('/openmrs/ws/rest/v1/bed', {
     data: {
-      bedNumber: 'BMW-123',
+      bedNumber: bedNumber.substring(0, 10),
       bedType: 'Duplix',
       status: 'AVAILABLE',
-      row: 6,
-      column: 7,
+      row: Math.floor(Math.random() * 10) + 1,
+      column: Math.floor(Math.random() * 10) + 1,
       locationUuid: process.env.E2E_WARD_LOCATION_UUID,
     },
   });
@@ -26,4 +28,14 @@ export const generateBedType = async (api: APIRequestContext): Promise<BedType> 
   });
   await expect(bedRes.ok()).toBeTruthy();
   return await bedRes.json();
+};
+
+export const deleteBed = async (api: APIRequestContext, uuid: string) => {
+  const response = await api.delete(`bed/${uuid}`, { data: {} });
+  await expect(response.ok()).toBeTruthy();
+};
+
+export const deleteBedType = async (api: APIRequestContext, uuid: string) => {
+  const response = await api.delete(`bedtype/${uuid}`, { data: {} });
+  await expect(response.ok()).toBeTruthy();
 };
