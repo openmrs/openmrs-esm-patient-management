53ff32c1401b82496c3adda8095be1a7cc87d5bd
Brandon Istenes <bistenes@gmail.com>
2025-07-16 01:01:52 -0400

    (feat) O3-4637: Service Queues: "Call" button in table opens modal, confirm updates status and goes to chart (#1756)

--- packages/esm-service-queues-app/src/queue-table/default-queue-table.component.tsx
+++ packages/esm-service-queues-app/src/queue-table/default-queue-table.component.tsx
@@ -23,7 +23,7 @@ import { useQueueEntries } from '../hooks/useQueueEntries';
 import useQueueStatuses from '../hooks/useQueueStatuses';
 import useQueueServices from '../hooks/useQueueService';
 import usePatientSearchVisibility from '../hooks/usePatientSearchVisibility';
-import ClearQueueEntries from '../clear-queue-entries-modal/clear-queue-entries.component';
+import ClearQueueEntries from '../modals/clear-queue-entries-modal/clear-queue-entries.component';
 import QueueTableExpandedRow from './queue-table-expanded-row.component';
 import QueueTable from './queue-table.component';
 import styles from './queue-table.scss';
@@ -32,8 +32,6 @@ function DefaultQueuePage() {
   const { t } = useTranslation();
   const layout = useLayoutType();
   const selectedService = useSelectedService();
-  const currentLocationUuid = useSelectedQueueLocationUuid();
-  const selectedQueueStatus = useSelectedQueueStatus();
   const [patientSearchQuery, setPatientSearchQuery] = useState('');
 
   const { isPatientSearchOpen, hidePatientSearch, showPatientSearch } = usePatientSearchVisibility();
