30e7ef9d6b35c772a2136d001d81ff3171a1540c
Wilson Mutua <willymathuva@gmail.com>
2025-07-23 10:48:58 +0300

    O3-4813: Add 'Type' column to appointments table

--- packages/esm-appointments-app/src/appointments/common-components/appointments-table.component.tsx
+++ packages/esm-appointments-app/src/appointments/common-components/appointments-table.component.tsx
@@ -89,6 +89,10 @@ const AppointmentsTable: React.FC<AppointmentsTableProps> = ({
       key: 'serviceType',
     },
     {
+      header: t('type', 'Type'),
+      key: 'type',
+    },
+    {
       header: t('status', 'Status'),
       key: 'status',
     },
@@ -106,12 +110,13 @@ const AppointmentsTable: React.FC<AppointmentsTableProps> = ({
     ),
     nextAppointmentDate: '--',
     identifier: patientIdentifierType
-      ? appointment.patient[patientIdentifierType.replaceAll(' ', '')] ?? appointment.patient.identifier
+      ? (appointment.patient[patientIdentifierType.replaceAll(' ', '')] ?? appointment.patient.identifier)
       : appointment.patient.identifier,
     dateTime: formatDatetime(new Date(appointment.startDateTime)),
     serviceType: appointment.service.name,
     location: appointment.location?.name,
     provider: appointment.provider,
+    type: appointment.appointmentKind,
     status: <AppointmentActions appointment={appointment} />,
   }));
 
