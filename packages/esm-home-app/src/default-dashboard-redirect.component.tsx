939ab64c8bad0477fa773c2d094364baa5ab60ee
Donald Kibet <chelashawdonald@yahoo.com>
2025-10-07 16:59:55 +0300

    code review changes

--- packages/esm-home-app/src/default-dashboard-redirect.component.tsx
+++ packages/esm-home-app/src/default-dashboard-redirect.component.tsx
@@ -4,6 +4,8 @@ import { useAssignedExtensions, useConfig, useSession } from '@openmrs/esm-frame
 import { type DashboardConfig } from './types/index';
 import { type HomeConfig } from './config-schema';
 
+import { DashboardNotFound } from './dashboard-container/dashboard-not-found.component';
+
 export function DefaultDashboardRedirect() {
   const assignedExtensions = useAssignedExtensions('homepage-dashboard-slot');
   const { defaultDashboardPerRole } = useConfig<HomeConfig>();
@@ -14,11 +16,11 @@ export function DefaultDashboardRedirect() {
 
   const ungroupedDashboards = assignedExtensions.map((e) => e.meta).filter((e) => Object.keys(e).length) || [];
   const dashboards = ungroupedDashboards as Array<DashboardConfig>;
-  const activeDashboard = dashboards.find((dashboard) => dashboard.name === defaultDashboard);
+  const activeDashboard = dashboards.find((dashboard) => dashboard.name === defaultDashboard) ?? dashboards[0];
 
-  if (!activeDashboard && dashboards.length > 0) {
-    return <Navigate to={`/home/${dashboards[0].name}`} />;
+  if (activeDashboard) {
+    return <Navigate to={`/home/${activeDashboard.name}`} />;
   }
 
-  return <Navigate to={`/home/${activeDashboard.name}`} />;
+  return <DashboardNotFound />;
 }
