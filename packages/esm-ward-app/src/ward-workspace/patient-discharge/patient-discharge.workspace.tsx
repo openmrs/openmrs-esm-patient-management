ff06e9c4cdbf8fba72077f6dd3c164b9ae8f7a1d
Chi Bong Ho <cbho@pih.org>
2025-11-20 13:49:52 -0500

    (feat) O3-5221 Migrate ward app to use workspace v2

--- packages/esm-ward-app/src/ward-workspace/patient-discharge/patient-discharge.workspace.tsx
+++ packages/esm-ward-app/src/ward-workspace/patient-discharge/patient-discharge.workspace.tsx
@@ -2,14 +2,16 @@ import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonSet, InlineNotification } from '@carbon/react';
 import { Exit } from '@carbon/react/icons';
-import { ExtensionSlot, showSnackbar, useAppContext } from '@openmrs/esm-framework';
-import { type WardPatientWorkspaceProps, type WardViewContext } from '../../types';
+import { closeWorkspaceGroup2, ExtensionSlot, showSnackbar, useAppContext, Workspace2 } from '@openmrs/esm-framework';
+import { type WardPatientWorkspaceDefinition, type WardPatientWorkspaceProps, type WardViewContext } from '../../types';
 import { removePatientFromBed, useCreateEncounter } from '../../ward.resource';
 import WardPatientWorkspaceBanner from '../patient-banner/patient-banner.component';
 import styles from './patient-discharge.scss';
 
-export default function PatientDischargeWorkspace(props: WardPatientWorkspaceProps) {
-  const { wardPatient, closeWorkspaceWithSavedChanges } = props;
+export default function PatientDischargeWorkspace({
+  groupProps: { wardPatient },
+  closeWorkspace,
+}: WardPatientWorkspaceDefinition) {
   const { t } = useTranslation();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { createEncounter, emrConfiguration, isLoadingEmrConfiguration, errorFetchingEmrConfiguration } =
@@ -34,6 +36,9 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
             title: t('patientWasDischarged', 'Patient was discharged'),
             kind: 'success',
           });
+
+          closeWorkspace({ discardUnsavedChanges: true });
+          closeWorkspaceGroup2();
         }
       })
       .catch((err: Error) => {
@@ -45,51 +50,52 @@ export default function PatientDischargeWorkspace(props: WardPatientWorkspacePro
       })
       .finally(() => {
         setIsSubmitting(false);
-        closeWorkspaceWithSavedChanges();
         wardPatientGroupDetails?.mutate?.();
       });
-  }, [createEncounter, wardPatient, emrConfiguration, t, closeWorkspaceWithSavedChanges, wardPatientGroupDetails]);
+  }, [createEncounter, wardPatient, emrConfiguration, t, closeWorkspace, wardPatientGroupDetails]);
 
   if (!wardPatientGroupDetails) {
     return null;
   }
 
   return (
-    <div className={styles.workspaceContent}>
-      <div className={styles.patientWorkspaceBanner}>
-        <WardPatientWorkspaceBanner wardPatient={props?.wardPatient} />
-      </div>
-      <div className={styles.workspaceForm}>
-        <div>
-          {errorFetchingEmrConfiguration && (
-            <div className={styles.formError}>
-              <InlineNotification
-                kind="error"
-                title={t('somePartsOfTheFormDidntLoad', "Some parts of the form didn't load")}
-                subtitle={t(
-                  'fetchingEmrConfigurationFailed',
-                  'Fetching EMR configuration failed. Try refreshing the page or contact your system administrator.',
-                )}
-                lowContrast
-                hideCloseButton
-              />
-            </div>
-          )}
+    <Workspace2 title={t('discharge', 'Discharge')}>
+      <div className={styles.workspaceContent}>
+        <div className={styles.patientWorkspaceBanner}>
+          <WardPatientWorkspaceBanner wardPatient={wardPatient} />
         </div>
-        <ExtensionSlot name="ward-patient-discharge-slot" />
-        <ButtonSet className={styles.buttonSet}>
-          <Button
-            size="sm"
-            kind="ghost"
-            renderIcon={(props) => <Exit size={16} {...props} />}
-            disabled={
-              isLoadingEmrConfiguration || isSubmitting || errorFetchingEmrConfiguration || !wardPatient?.patient
-            }
-            onClick={submitDischarge}>
-            {t('proceedWithPatientDischarge', 'Proceed with patient discharge')}
-          </Button>
-        </ButtonSet>
+        <div className={styles.workspaceForm}>
+          <div>
+            {errorFetchingEmrConfiguration && (
+              <div className={styles.formError}>
+                <InlineNotification
+                  kind="error"
+                  title={t('somePartsOfTheFormDidntLoad', "Some parts of the form didn't load")}
+                  subtitle={t(
+                    'fetchingEmrConfigurationFailed',
+                    'Fetching EMR configuration failed. Try refreshing the page or contact your system administrator.',
+                  )}
+                  lowContrast
+                  hideCloseButton
+                />
+              </div>
+            )}
+          </div>
+          <ExtensionSlot name="ward-patient-discharge-slot" />
+          <ButtonSet className={styles.buttonSet}>
+            <Button
+              size="sm"
+              kind="ghost"
+              renderIcon={(props) => <Exit size={16} {...props} />}
+              disabled={
+                isLoadingEmrConfiguration || isSubmitting || errorFetchingEmrConfiguration || !wardPatient?.patient
+              }
+              onClick={submitDischarge}>
+              {t('proceedWithPatientDischarge', 'Proceed with patient discharge')}
+            </Button>
+          </ButtonSet>
+        </div>
       </div>
-    </div>
+    </Workspace2>
   );
 }
--- packages/esm-ward-app/src/ward-workspace/patient-transfer-request-workspace/patient-transfer-request.workspace.tsx
+++ packages/esm-ward-app/src/ward-workspace/patient-transfer-request-workspace/patient-transfer-request.workspace.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import { closeWorkspaceGroup2, Workspace2, type Workspace2DefinitionProps } from '@openmrs/esm-framework';
 import { type WardPatientWorkspaceProps } from '../../types';
 import WardPatientWorkspaceBanner from '../patient-banner/patient-banner.component';
 import PatientAdmitOrTransferForm from '../patient-transfer-bed-swap/patient-admit-or-transfer-request-form.component';
 import styles from './patient-transfer-request.scss';
-
-interface PatientTransferRequestWorkspaceProps extends WardPatientWorkspaceProps {}
+import { useTranslation } from 'react-i18next';
 
 /**
  * This workspace is launched when the "Transfer elsewhere" / "Admit elsewhere"
  * button on a pending request patient card is clicked on
  */
-const PatientTransferRequestWorkspace: React.FC<PatientTransferRequestWorkspaceProps> = (props) => {
-  const { wardPatient } = props;
+const PatientTransferRequestWorkspace: React.FC<Workspace2DefinitionProps<WardPatientWorkspaceProps>> = ({
+  workspaceProps: { wardPatient },
+  closeWorkspace,
+}) => {
+  const { t } = useTranslation();
+  const isTransfer = wardPatient.inpatientRequest.dispositionType == 'TRANSFER';
 
   return (
-    <div className={styles.patientTransferRequestWorkspace}>
-      <WardPatientWorkspaceBanner {...{ wardPatient }} />
-      <PatientAdmitOrTransferForm {...props} />
-    </div>
+    <Workspace2
+      title={isTransfer ? t('transferElsewhere', 'Transfer elsewhere') : t('admitElsewhere', 'Admit elsewhere')}>
+      <div className={styles.patientTransferRequestWorkspace}>
+        <WardPatientWorkspaceBanner {...{ wardPatient }} />
+        <PatientAdmitOrTransferForm
+          wardPatient={wardPatient}
+          onSuccess={async () => {
+            await closeWorkspace({ discardUnsavedChanges: true });
+            closeWorkspaceGroup2();
+          }}
+          onCancel={() => closeWorkspace()}
+        />
+      </div>
+    </Workspace2>
   );
 };
 
