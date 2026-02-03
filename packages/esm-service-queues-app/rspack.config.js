08a4f02ae8d6e6a95dbd08ae0d2359b9d179cc66
Shravanthi20 <satyanar061@gmail.com>
2026-02-03 22:58:59 +0530

    (O3-5393)Resolved issues

--- packages/esm-service-queues-app/rspack.config.js
+++ packages/esm-service-queues-app/rspack.config.js
@@ -1,1 +1,13 @@
-module.exports = require('openmrs/default-rspack-config');
+const config = require('openmrs/default-rspack-config');
+
+module.exports = (env, argv) => {
+    const defaultConfig = config(env, argv);
+
+    const tsCheckerPlugin = defaultConfig.plugins.find((plugin) => plugin.constructor.name === 'TsCheckerRspackPlugin');
+
+    if (tsCheckerPlugin) {
+        tsCheckerPlugin.options.memoryLimit = 4096;
+    }
+
+    return defaultConfig;
+};
