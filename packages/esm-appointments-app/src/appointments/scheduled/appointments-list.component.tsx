05e667bb959eb25fe0370b5ea21be1100bbf2afe
chibongho <cbho@pih.org>
2026-02-19 11:46:49 -0500

    (fix) O3-5368 Appointments - make appointment table always show rows of configured status (#2275)

--- packages/esm-appointments-app/src/appointments/scheduled/appointments-list.component.tsx
+++ packages/esm-appointments-app/src/appointments/scheduled/appointments-list.component.tsx
@@ -2,22 +2,27 @@ import React, { useMemo } from 'react';
 import { filterByServiceType } from '../utils';
 import { useAppointmentList } from '../../hooks/useAppointmentList';
 import AppointmentsTable from '../common-components/appointments-table.component';
+import { useConfig } from '@openmrs/esm-framework';
+import { useTranslation } from 'react-i18next';
+import { type AppointmentPanelConfig } from '../../scheduled-appointments-config-schema';
 
 interface AppointmentsListProps {
   appointmentServiceTypes?: Array<string>;
   date: string;
   excludeCancelledAppointments?: boolean;
-  status?: string;
-  title: string;
 }
 
+/**
+ * This component is both rendered as a regular componant and as an extension.
+ * As an extension, it can be configured to display appointments of certain status.
+ */
 const AppointmentsList: React.FC<AppointmentsListProps> = ({
   appointmentServiceTypes,
   date,
   excludeCancelledAppointments = false,
-  status,
-  title,
 }) => {
+  const { t } = useTranslation();
+  const { status, title = t('todays', "Today's") } = useConfig<AppointmentPanelConfig>();
   const { appointmentList, isLoading } = useAppointmentList(status, date);
 
   const appointmentsFilteredByServiceType = filterByServiceType(appointmentList, appointmentServiceTypes).map(
