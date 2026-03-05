ee74ed233a5cdff2744cb36349b82eaa2be90ac4
princegupta1234 <princegupta.ns153@gmail.com>
2026-03-06 00:28:27 +0530

    fix: resolve active-visits-app type error

--- packages/esm-active-visits-app/src/visits-summary/visit-detail.component.tsx
+++ packages/esm-active-visits-app/src/visits-summary/visit-detail.component.tsx
@@ -2,9 +2,9 @@ import React, { useMemo, useState } from 'react';
 import classNames from 'classnames';
 import { ContentSwitcher, DataTableSkeleton, Switch } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import { formatDatetime, formatTime, parseDate } from '@openmrs/esm-framework';
+import { formatDatetime, formatTime, Obs, parseDate } from '@openmrs/esm-framework';
 import { useVisit } from './visit.resource';
-import EncounterList from './visits-components/encounter-list.component';
+import EncounterList, { type EncounterListItem } from './visits-components/encounter-list.component';
 import VisitSummary from './visits-components/visit-summary.component';
 import styles from './visit-detail-overview.scss';
 
@@ -18,7 +18,7 @@ const VisitDetailComponent: React.FC<VisitDetailComponentProps> = ({ visitUuid, 
   const [contentSwitcherIndex, setContentSwitcherIndex] = useState(0);
   const { visit, isLoading } = useVisit(visitUuid);
 
-  const encounters = useMemo(
+  const encounters = useMemo<Array<EncounterListItem>>(
     () =>
       visit
         ? visit?.encounters?.map((encounter) => ({
