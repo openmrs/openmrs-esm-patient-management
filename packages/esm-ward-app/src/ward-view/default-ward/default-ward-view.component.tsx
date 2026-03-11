592f8460435781fd404c4078ef1e2f11e28f7b8d
Tanishq Sangwan <tanishqsangwan7@gmail.com>
2026-03-11 21:55:33 +0530

    O3-4673 Ward app to not allow admissions on locations without tag and show alert for the same

--- packages/esm-ward-app/src/ward-view/default-ward/default-ward-view.component.tsx
+++ packages/esm-ward-app/src/ward-view/default-ward/default-ward-view.component.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDefineAppContext } from '@openmrs/esm-framework';
+import { Loading, InlineNotification } from '@carbon/react';
 import { type WardViewContext } from '../../types';
+import useEmrConfiguration from '../../hooks/useEmrConfiguration';
+import useLocations from '../../hooks/useLocations';
+import useWardLocation from '../../hooks/useWardLocation';
 import { useWardPatientGrouping } from '../../hooks/useWardPatientGrouping';
 import DefaultWardBeds from './default-ward-beds.component';
 import DefaultWardPatientCardHeader from './default-ward-patient-card-header.component';
@@ -9,23 +14,61 @@ import DefaultWardUnassignedPatients from './default-ward-unassigned-patients.co
 import Ward from '../ward.component';
 import WardViewHeader from '../../ward-view-header/ward-view-header.component';
 import WardMetrics from '../../ward-view-header/ward-metrics.component';
+import styles from './default-ward-view.scss';
 
 const DefaultWardView = () => {
+  const { t } = useTranslation();
+
   const wardPatientGroupDetails = useWardPatientGrouping();
   useDefineAppContext<WardViewContext>('ward-view-context', {
     wardPatientGroupDetails,
     WardPatientHeader: DefaultWardPatientCardHeader,
   });
 
-  const wardBeds = <DefaultWardBeds />;
-  const wardMetrics = <WardMetrics />;
-  const wardUnassignedPatients = <DefaultWardUnassignedPatients />;
-  const wardPendingPatients = <DefaultWardPendingPatients />;
+  const { emrConfiguration, isLoadingEmrConfiguration } = useEmrConfiguration();
+  const { location } = useWardLocation();
+
+  const filterCriteria = useMemo(() => {
+    const tag = emrConfiguration?.supportsAdmissionLocationTag?.name;
+    return tag ? [['_tag', tag]] : [];
+  }, [emrConfiguration]);
+
+  const { data: admissionLocations, isLoading: isLoadingAdmissionLocations } = useLocations(
+    filterCriteria,
+    15,
+    !emrConfiguration,
+  );
+
+  const locationHasAllowAdmissionsTag = admissionLocations?.some((loc) => loc.id === location?.uuid) ?? false;
+
+  if (isLoadingEmrConfiguration || isLoadingAdmissionLocations) {
+    return (
+      <div className={styles.loaderContainer}>
+        <Loading withOverlay={false} />
+      </div>
+    );
+  }
 
   return (
     <>
-      <WardViewHeader {...{ wardPendingPatients, wardMetrics }} />
-      <Ward {...{ wardBeds, wardUnassignedPatients }} />
+      <WardViewHeader
+        wardPendingPatients={<DefaultWardPendingPatients />}
+        wardMetrics={<WardMetrics />}
+        locationAllowsAdmissions={locationHasAllowAdmissionsTag}
+      />
+      {!locationHasAllowAdmissionsTag && (
+        <InlineNotification
+          className={styles.noAdmissionsNotification}
+          kind="error"
+          hideCloseButton={true}
+          lowContrast={true}
+          title={t(
+            'locationDoesNotAllowAdmissions',
+            'This location does not allow admissions. If there are patients here, please discharge them.',
+          )}
+        />
+      )}
+      <Ward wardBeds={<DefaultWardBeds />} wardUnassignedPatients={<DefaultWardUnassignedPatients />} />
     </>
   );
 };
