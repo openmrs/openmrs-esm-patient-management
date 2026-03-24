ea0e7f4dcaa5168d306b633f5b38e2d568ed63a3
Shashank8521 <skwork8521@gmail.com>
2026-03-24 15:32:37 +0530

    (fix) O3-5545: Prev/Next navigation in appointments calendar view is not working as intenional

--- packages/esm-appointments-app/src/calendar/monthly/monthly-header.component.tsx
+++ packages/esm-appointments-app/src/calendar/monthly/monthly-header.component.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@carbon/react';
-import { formatDate } from '@openmrs/esm-framework';
+import { formatDate, navigate } from '@openmrs/esm-framework';
 import DaysOfWeekCard from './days-of-week.component';
 import styles from './monthly-header.scss';
+import { spaHomePage } from '../../constants';
 import { useSelectedDate } from '../../hooks/useSelectedDate';
 
 const DAYS_IN_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
@@ -12,16 +13,19 @@ const DAYS_IN_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
 const MonthlyHeader: React.FC = () => {
   const { t } = useTranslation();
   const selectedDate = useSelectedDate();
-
-  const [calendarSelectedDate, setCalendarSelectedDate] = useState(dayjs(selectedDate));
+  const calendarSelectedDate = dayjs(selectedDate);
 
   const handleSelectPrevMonth = useCallback(() => {
-    setCalendarSelectedDate(calendarSelectedDate.subtract(1, 'month'));
-  }, [calendarSelectedDate, setCalendarSelectedDate]);
+    navigate({
+      to: `${spaHomePage}/appointments/calendar/${calendarSelectedDate.subtract(1, 'month').format('YYYY-MM-DD')}`,
+    });
+  }, [calendarSelectedDate]);
 
   const handleSelectNextMonth = useCallback(() => {
-    setCalendarSelectedDate(calendarSelectedDate.add(1, 'month'));
-  }, [calendarSelectedDate, setCalendarSelectedDate]);
+    navigate({
+      to: `${spaHomePage}/appointments/calendar/${calendarSelectedDate.add(1, 'month').format('YYYY-MM-DD')}`,
+    });
+  }, [calendarSelectedDate]);
 
   return (
     <>
