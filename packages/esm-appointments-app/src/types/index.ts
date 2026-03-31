e96238d516d527cc04eaf0df0766c100aba940c5
Madhu Sharma <madhusharmamain7@gmail.com>
2026-03-31 20:04:10 +0545

    chore: improve type handling

--- packages/esm-appointments-app/src/types/index.ts
+++ packages/esm-appointments-app/src/types/index.ts
@@ -39,7 +39,7 @@ export interface Appointment {
   appointmentKind: AppointmentKind;
   appointmentNumber: string;
   comments: string;
-  endDateTime: Date | number | any;
+  endDateTime: Date | number | null;
   location: AppointmentLocation;
   // note: this is not a standard OpenMRS Patient object
   patient: {
@@ -53,8 +53,8 @@ export interface Appointment {
   providers: Array<OpenmrsResource>;
   recurring: boolean;
   service: AppointmentService;
-  startDateTime: string | any;
-  dateAppointmentScheduled: string | any;
+  startDateTime: string | number;
+  dateAppointmentScheduled: string | number;
   status: AppointmentStatus;
   uuid: string;
   additionalInfo?: string | null;
@@ -115,7 +115,7 @@ export interface Observation {
       display: string;
     };
   }>;
-  value: any;
+  value: string | number | boolean | null;
   obsDatetime: string;
 }
 
