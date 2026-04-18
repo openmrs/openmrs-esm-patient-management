6906a0d7f5419e7a77aa5b520953d1b3f2bde06b
Shashank8521 <skwork8521@gmail.com>
2026-04-18 18:35:53 +0530

    fix: use local state for calendar navigation

--- packages/esm-appointments-app/src/calendar/monthly/monthly-calendar-view.component.tsx
+++ packages/esm-appointments-app/src/calendar/monthly/monthly-calendar-view.component.tsx
@@ -1,19 +1,13 @@
 import React, { useCallback } from 'react';
 import isBetween from 'dayjs/plugin/isBetween';
 import dayjs, { type Dayjs } from 'dayjs';
-import { Button } from '@carbon/react';
-import { formatDate } from '@openmrs/esm-framework';
-import { useTranslation } from 'react-i18next';
 import { type DailyAppointmentsCountByService } from '../../types';
 import { monthDays } from '../../helpers';
-import DaysOfWeekCard from './days-of-week.component';
+import MonthlyHeader from './monthly-header.component';
 import MonthlyViewWorkload from './monthly-workload-view.component';
 import styles from '../appointments-calendar-view-view.scss';
-import headerStyles from './monthly-header.scss';
 
 dayjs.extend(isBetween);
-
-const DAYS_IN_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
 
 interface MonthlyCalendarViewProps {
   events: Array<DailyAppointmentsCountByService>;
@@ -26,8 +20,6 @@ const MonthlyCalendarView: React.FC<MonthlyCalendarViewProps> = ({
   calendarSelectedDate,
   setCalendarSelectedDate,
 }) => {
-  const { t } = useTranslation();
-
   const handleSelectPrevMonth = useCallback(() => {
     setCalendarSelectedDate((prev) => prev.subtract(1, 'month'));
   }, [setCalendarSelectedDate]);
@@ -38,26 +30,11 @@ const MonthlyCalendarView: React.FC<MonthlyCalendarViewProps> = ({
 
   return (
     <div className={styles.calendarViewContainer}>
-      <>
-        <div className={headerStyles.container}>
-          <Button
-            aria-label={t('previousMonth', 'Previous month')}
-            kind="tertiary"
-            onClick={handleSelectPrevMonth}
-            size="sm">
-            {t('prev', 'Prev')}
-          </Button>
-          <span>{formatDate(calendarSelectedDate.toDate(), { day: false, time: false, noToday: true })}</span>
-          <Button aria-label={t('nextMonth', 'Next month')} kind="tertiary" onClick={handleSelectNextMonth} size="sm">
-            {t('next', 'Next')}
-          </Button>
-        </div>
-        <div className={headerStyles.workLoadCard}>
-          {DAYS_IN_WEEK.map((day) => (
-            <DaysOfWeekCard key={day} dayOfWeek={day} />
-          ))}
-        </div>
-      </>
+      <MonthlyHeader
+        calendarSelectedDate={calendarSelectedDate}
+        onSelectPrevMonth={handleSelectPrevMonth}
+        onSelectNextMonth={handleSelectNextMonth}
+      />
       <div className={styles.wrapper}>
         <div className={styles.monthlyCalendar}>
           {monthDays(calendarSelectedDate).map((dateTime, i) => (
