5cc1fd4b006488850adaca4a3aec4f14eaf30330
Chi Bong Ho <cbho@pih.org>
2026-04-21 14:34:10 -0400

    (feat) O3-5624: Add ward app configuration to only show discharge button in certain locations

--- packages/esm-ward-app/src/action-menu-buttons/discharge-workspace-siderail.component.tsx
+++ packages/esm-ward-app/src/action-menu-buttons/discharge-workspace-siderail.component.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { ActionMenuButton2 } from '@openmrs/esm-framework';
+import { ActionMenuButton2, useConfig, useSession } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
 import { Exit } from '@carbon/react/icons';
+import { type DischargeWorkspaceSiderailConfig } from './discharge-workspace-siderail-config-schema';
 
 export default function PatientDischargeSideRailIcon() {
   const { t } = useTranslation();
-  return (
-    <ActionMenuButton2
-      icon={(props) => <Exit {...props} />}
-      label={t('discharge', 'Discharge')}
-      workspaceToLaunch={{
-        workspaceName: 'patient-discharge-workspace',
-      }}
-    />
-  );
+  const { sessionLocation } = useSession();
+  const { onlyShowDischargeForLocations } = useConfig<DischargeWorkspaceSiderailConfig>();
+
+  if (onlyShowDischargeForLocations.length === 0 || onlyShowDischargeForLocations.includes(sessionLocation?.uuid)) {
+    return (
+      <ActionMenuButton2
+        icon={(props) => <Exit {...props} />}
+        label={t('discharge', 'Discharge')}
+        workspaceToLaunch={{
+          workspaceName: 'patient-discharge-workspace',
+        }}
+      />
+    );
+  } else {
+    return null;
+  }
 }
