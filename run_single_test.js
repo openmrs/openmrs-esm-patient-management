9cca58667d82a35ccda1ab96e04ee83614e9a431
sourav-jyoti <souravjyoti64@gmail.com>
2026-05-18 23:11:19 +0530

    en

--- run_single_test.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('yarn turbo run test --filter=@openmrs/esm-ward-app -- patient-discharge', { stdio: 'inherit' });
-} catch (e) {
-  process.exit(1);
-}
