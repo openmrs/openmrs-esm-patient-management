2a49880d7785058acbf78eda78690b19ddeb3028
Chi Bong Ho <cbho@pih.org>
2026-09-24 16:51:16 -0400

    also remove last old workspace

--- packages/esm-patient-search-app/src/index.ts
+++ packages/esm-patient-search-app/src/index.ts
@@ -25,11 +25,6 @@ export const patientSearchIcon = getAsyncLifecycle(() => import('./patient-searc
 // This extension is not compatible with the tablet view.
 export const patientSearchBar = getAsyncLifecycle(() => import('./compact-patient-search.extension'), options);
 
-export const patientSearchWorkspace = getAsyncLifecycle(
-  () => import('./patient-search-workspace/patient-search.workspace'),
-  options,
-);
-
 export const patientSearchWorkspace2 = getAsyncLifecycle(
   () => import('./patient-search-workspace/patient-search2.workspace'),
   options,
